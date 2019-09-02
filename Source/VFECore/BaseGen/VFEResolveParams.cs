26d3c49cd166cac54677d0ec42bce8b39305abb4
XeoNovaDan <26175895+XeoNovaDan@users.noreply.github.com>
2019-09-03 00:28:25 +0100

    ExtendedFactionUtility and moved VFEResolveParams over

---
+++ Source/VFECore/BaseGen/VFEResolveParams.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using Verse;
+using Verse.AI;
+using Verse.AI.Group;
+using RimWorld;
+using RimWorld.BaseGen;
+using Harmony;
+
+namespace VFECore
+{
+
+    public class VFEResolveParams
+    {
+
+        public const string Name = "VFEMResolveParams";
+
+        public ThingDef edgeWallDef;
+        public float? towerRadius;
+        public bool? symmetricalSandbags;
+        public bool? hasDoors;
+        public bool? outdoorLighting;
+        public bool? generatePawns;
+
+    }
+
+}
---
+++ Source/VFECore/Utilities/ExtendedFactionUtility.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using UnityEngine;
+using Verse;
+using RimWorld;
+using Harmony;
+
+namespace VFECore
+{
+
+    public static class ExtendedFactionUtility
+    {
+
+        public static Faction RandomFactionOfTechLevel(TechLevel level, bool allowHidden = false, bool allowDefeated = false, bool allowNonHumanlike = false)
+        {
+            if (Find.FactionManager.AllFactions.Where(f => !f.def.isPlayer && f.def.techLevel == level && (allowHidden || !f.def.hidden) && (allowDefeated || !f.defeated) && (allowNonHumanlike || f.def.humanlikeFaction)).TryRandomElement(out var fac))
+                return fac;
+            return null;
+        }
+
+    }
+
+}
