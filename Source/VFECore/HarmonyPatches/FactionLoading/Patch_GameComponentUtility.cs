ffe3891397c1117eadd65caa307366cf1304cc84
OrionFive <randomw@gmx.net>
2020-07-17 14:37:56 +0200

    Added hook for faction loading

---
+++ Source/VFECore/HarmonyPatches/FactionLoading/Patch_GameComponentUtility.cs
@@ -0,0 +1,18 @@
+﻿using HarmonyLib;
+using RimWorld;
+using Verse;
+
+namespace VFECore.FactionLoading
+{
+    public static class Patch_GameComponentUtility
+    {
+        [HarmonyPatch(typeof(GameComponentUtility), nameof(GameComponentUtility.LoadedGame))]
+        public static class LoadedGame
+        {
+            public static void Postfix()
+            {
+                LongEventHandler.ExecuteWhenFinished(delegate { Log.Message($"This is faction loading."); });
+            }
+        }
+    }
+}
