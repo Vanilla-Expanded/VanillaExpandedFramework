0f5772b68f04a768873409d97a869a811ff0e3eb
Taranchuk <nfhfyxer@gmail.com>
2020-08-29 05:41:33 +0300

    CompApparelHediffs

---
+++ Source/VFECore/Comps/ThingComps/CompProperties_ApparelHediffs.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using RimWorld;
+using Verse;
+
+namespace VFECore
+{
+	public class CompProperties_ApparelHediffs : CompProperties
+	{
+		public List<string> hediffDefnames;
+		public CompProperties_ApparelHediffs()
+		{
+			this.compClass = typeof(CompApparelHediffs);
+		}
+	}
+}
+
