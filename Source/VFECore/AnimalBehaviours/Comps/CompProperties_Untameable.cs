b7852eae9238142b9ef59aff8d958affc7255909
juanosarg <31105173+juanosarg@users.noreply.github.com>
2020-09-01 16:10:43 +0200

    Added framework comp to make some insects untameable

---
+++ Source/VFECore/AnimalBehaviours/Comps/CompProperties_Untameable.cs
@@ -0,0 +1,19 @@
+﻿using RimWorld;
+using Verse;
+
+namespace AnimalBehaviours
+{
+    public class CompProperties_Untameable : CompProperties
+    {
+
+        //A comp class to make animals not tameable. You can indicate what Faction to return them to. If null, they'll just go manhunter
+
+        public CompProperties_Untameable()
+        {
+            this.compClass = typeof(CompUntameable);
+        }
+
+        public string factionToReturnTo = "";
+
+    }
+}
---
+++ Source/VFECore/AnimalBehaviours/Comps/CompUntameable.cs
@@ -0,0 +1,62 @@
+﻿using RimWorld;
+using Verse;
+using System.Collections.Generic;
+
+namespace AnimalBehaviours
+{
+    public class CompUntameable : ThingComp
+    {
+
+        public CompProperties_Untameable Props
+        {
+            get
+            {
+                return (CompProperties_Untameable)this.props;
+            }
+        }
+
+
+        public override void CompTick()
+        {
+            base.CompTick();
+            if (this.parent.IsHashIntervalTick(500))
+            {
+                this.CheckFaction();
+            }
+        }
+
+        public void CheckFaction()
+        {
+            if (this.parent.Faction == Faction.OfPlayer)
+            {
+
+                Pawn pawn = parent as Pawn;
+                if (pawn != null)
+                {
+                    if (Props.factionToReturnTo == "")
+                    {
+                       
+                            pawn.mindState.mentalStateHandler.TryStartMentalState(MentalStateDefOf.ManhunterPermanent);
+                       
+
+
+                    }
+                    else
+                    {
+
+                       
+                            parent.SetFaction(Find.FactionManager.FirstFactionOfDef(FactionDef.Named(Props.factionToReturnTo)), null);
+                            pawn.mindState.mentalStateHandler.TryStartMentalState(MentalStateDefOf.ManhunterPermanent);
+                        
+
+                    };
+                }
+                
+
+            }
+
+        }
+
+
+    }
+}
