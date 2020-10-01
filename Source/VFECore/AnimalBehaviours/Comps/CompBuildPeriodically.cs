5ba80b286452777946dc1c3026e8299cc9382222
juanosarg <31105173+juanosarg@users.noreply.github.com>
2020-10-01 12:10:40 +0200

    Added CompBuildPeriodically for beavers

---
+++ Source/VFECore/AnimalBehaviours/Comps/CompBuildPeriodically.cs
@@ -0,0 +1,76 @@
+﻿using RimWorld;
+using Verse;
+using System.Collections.Generic;
+
+namespace AnimalBehaviours
+{
+    public class CompBuildPeriodically : ThingComp
+    {
+
+        private Effecter effecter;
+
+        public CompProperties_BuildPeriodically Props
+        {
+            get
+            {
+                return (CompProperties_BuildPeriodically)this.props;
+            }
+        }
+
+
+        public override void CompTick()
+        {
+            base.CompTick();
+            if (this.parent.IsHashIntervalTick(Props.ticksToBuild))
+            {
+                this.TryCreateBuilding();
+            }
+        }
+
+        public void TryCreateBuilding()
+        {
+            if (this.parent.Map.listerThings.ThingsOfDef(ThingDef.Named(Props.defOfBuilding)).Count < Props.maxBuildingsPerMap)
+            {
+                Pawn pawn = this.parent as Pawn;
+
+                if ((pawn.Map != null) && (pawn.Awake()) && (!pawn.Downed))
+                {
+                    if (Props.acceptedTerrains != null)
+                    {
+                        if (Props.acceptedTerrains.Contains(pawn.Position.GetTerrain(pawn.Map).defName))
+                        {
+                            ThingDef newThing = ThingDef.Named(this.Props.defOfBuilding);
+                            Thing newbuilding = GenSpawn.Spawn(newThing, pawn.Position, pawn.Map, WipeMode.Vanish);
+
+                            if (this.effecter == null)
+                            {
+                                this.effecter = EffecterDefOf.Mine.Spawn();
+                            }
+                            this.effecter.Trigger(pawn, newbuilding);
+                        }
+                    }
+                    else
+                    {
+                        ThingDef newThing = ThingDef.Named(this.Props.defOfBuilding);
+                        Thing newbuilding = GenSpawn.Spawn(newThing, pawn.Position, pawn.Map, WipeMode.Vanish);
+
+                        if (this.effecter == null)
+                        {
+                            this.effecter = EffecterDefOf.Mine.Spawn();
+                        }
+                        this.effecter.Trigger(pawn, newbuilding);
+
+                    }
+
+                }
+            }
+
+            
+        }
+
+
+
+
+
+    }
+}
---
+++ Source/VFECore/AnimalBehaviours/Comps/CompProperties/CompProperties_BuildPeriodically.cs
@@ -0,0 +1,24 @@
+﻿using RimWorld;
+using Verse;
+using System.Collections.Generic;
+
+
+namespace AnimalBehaviours
+{
+    public class CompProperties_BuildPeriodically : CompProperties
+    {
+
+        //A comp class to make animals periodically create buildings around them. Terrains where buildings are accepted can
+        //be specified
+
+        public CompProperties_BuildPeriodically()
+        {
+            this.compClass = typeof(CompBuildPeriodically);
+        }
+
+        public string defOfBuilding = "";
+        public int ticksToBuild = 1000;
+        public int maxBuildingsPerMap = 10;
+        public List<string> acceptedTerrains = null;
+    }
+}
