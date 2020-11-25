7387d9766ebec1cfdb4f2577bda51766800d0078
KentHaeger <kent.haeger@gmail.com>
2020-11-25 01:06:14 -0600

    Added code to support mechanoid buildings

---
+++ Source/VFECore/VFECore/Comps/ThingComps/CompProperties_PawnDependsOn.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace VFECore
+{
+    public class CompProperties_PawnDependsOn : CompProperties
+    {
+        public PawnKindDef pawnToSpawn;
+
+        public CompProperties_PawnDependsOn()
+        {
+            this.compClass = typeof(CompPawnDependsOn);
+        }
+    }
+}
