be3d54aa534e460fba2baac5fcc76ab6251d10c0
Kylian <kylian.balde@gmail.com>
2020-11-29 13:24:31 +0100

    CSG update

--- Source/VFECore/CustomStructureGeneration/Utils/DebugOption.cs
+++ Source/VFECore/CustomStructureGeneration/Utils/DebugOption.cs
@@ -6,7 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Verse;
 
-namespace VFECore.CustomStructureGeneration.Utils
+namespace KCSG
 {
     public static class DebugOption
     {
@@ -17,6 +17,31 @@ namespace VFECore.CustomStructureGeneration.Utils
             {
                 if (pawn.Faction != Faction.OfPlayer) pawn.Destroy(DestroyMode.KillFinalize);
             }
+        }
+
+        [DebugAction("Vanilla Framework Expanded", "Quickspawn structure", actionType = DebugActionType.Action, allowedGameStates = AllowedGameStates.PlayingOnMap)]
+        private static void QuickspawnStructure()
+        {
+            List<DebugMenuOption> list = new List<DebugMenuOption>();
+            foreach (StructureLayoutDef localDef2 in DefDatabase<StructureLayoutDef>.AllDefs)
+            {
+                StructureLayoutDef localDef = localDef2;
+                list.Add(new DebugMenuOption(localDef.defName, DebugMenuOptionMode.Tool, delegate ()
+                {
+                    if (UI.MouseCell().InBounds(Find.CurrentMap))
+                    {
+                        KCSG_Utilities.HeightWidthFromLayout(localDef, out int h, out int w);
+                        CellRect cellRect = CellRect.CenteredOn(UI.MouseCell(), w, h);
+
+                        int count = 0;
+                        foreach (List<string> item in localDef.layouts)
+                        {
+                            KCSG_Utilities.GenerateRoomFromLayout(item, cellRect, Find.CurrentMap, localDef);
+                        }
+                    }
+                }));
+            }
+            Find.WindowStack.Add(new Dialog_DebugOptionListLister(list));
         }
     }
 }
