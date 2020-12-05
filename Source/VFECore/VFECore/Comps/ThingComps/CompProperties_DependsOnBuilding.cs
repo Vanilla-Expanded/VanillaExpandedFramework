e0cc9c31e960fd76868d9aa9beece8b16564e5ff
KentHaeger <kent.haeger@gmail.com>
2020-12-05 10:46:15 -0600

    Added ThingComps for Mechanoids functionality

---
+++ Source/VFECore/VFECore/Comps/ThingComps/CompProperties_DependsOnBuilding.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace VFECore
+{
+    public class CompProperties_DependsOnBuilding : CompProperties
+    {
+
+        public CompProperties_DependsOnBuilding()
+        {
+            this.compClass = typeof(CompPawnDependsOn);
+        }
+    }
+}
