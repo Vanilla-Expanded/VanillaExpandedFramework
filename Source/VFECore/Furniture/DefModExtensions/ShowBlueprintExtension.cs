741827bed18335acef2b3f405c2e4fbb3b363e88
juanosarg <31105173+juanosarg@users.noreply.github.com>
2020-12-06 09:54:58 +0100

    Added a defmodextension to make buildings show their blueprints

---
+++ Source/VFECore/Furniture/DefModExtensions/ShowBlueprintExtension.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using Verse;
+using RimWorld;
+
+namespace VanillaFurnitureExpanded
+{
+
+    public class ShowBlueprintExtension : DefModExtension
+    {
+
+        //A simple mod extension accesed by a Harmont patch to show a building's
+        //blueprint when it is in ghost (placement) mode
+
+        private static readonly ShowBlueprintExtension DefaultValues = new ShowBlueprintExtension();
+        public static ShowBlueprintExtension Get(Def def) => def.GetModExtension<ShowBlueprintExtension>() ?? DefaultValues;
+
+        
+        public bool showBlueprintInGhostMode = true;
+
+       
+
+    }
+
+}
