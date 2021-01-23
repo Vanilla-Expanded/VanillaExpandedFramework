6d3b84a883a33eca436844c8f01712afcc156cf2
Joshua <legodude17@users.noreply.github.com>
2021-01-22 16:01:53 -0800

    MVCF: Add Verb_SmokePop

---
+++ Source/MVCF/Verbs/Verb_SmokePop.cs
@@ -0,0 +1,16 @@
+﻿using RimWorld;
+using Verse;
+
+namespace MVCF.Verbs
+{
+    public class Verb_SmokePop : RimWorld.Verb_SmokePop
+    {
+        protected override bool TryCastShot()
+        {
+            GenExplosion.DoExplosion(caster.Position, caster.Map,
+                caster.GetStatValue(StatDefOf.SmokepopBeltRadius), DamageDefOf.Smoke, null, -1, -1f, null, null,
+                null, null, ThingDefOf.Gas_Smoke, 1f, 1, false, null, 0f, 1, 0f, false, null);
+            return true;
+        }
+    }
+}
