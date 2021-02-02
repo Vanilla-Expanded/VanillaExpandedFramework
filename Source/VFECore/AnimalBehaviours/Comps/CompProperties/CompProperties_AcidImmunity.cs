a1cf951c2a85b955c1118be229414fe1b8e43b74
juanosarg <31105173+juanosarg@users.noreply.github.com>
2021-02-02 14:52:34 +0100

    Added acid attacks and acid immunity

---
+++ Source/VFECore/AnimalBehaviours/Comps/CompProperties/CompProperties_AcidImmunity.cs
@@ -0,0 +1,18 @@
+﻿
+using Verse;
+
+namespace AnimalBehaviours
+{
+    public class CompProperties_AcidImmunity : CompProperties
+    {
+
+        //This is just an empty Comp. The new Hediff_AcidBuildup checks if the creature has it, and doesn't apply damage if so
+
+        public CompProperties_AcidImmunity()
+        {
+            this.compClass = typeof(CompAcidImmunity);
+        }
+
+
+    }
+}
