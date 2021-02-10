63634a2e952076f464699d784ebf9a81cfd605ff
juanosarg <31105173+juanosarg@users.noreply.github.com>
2021-02-10 16:03:12 +0100

    Added CompRefueling

---
+++ Source/VFECore/AnimalBehaviours/Comps/CompProperties/CompProperties_Refueling.cs
@@ -0,0 +1,25 @@
+﻿using Verse;
+using System.Collections.Generic;
+
+
+namespace AnimalBehaviours
+{
+    public class CompProperties_Refueling : CompProperties
+    {
+
+        //Similar to CompProperties_Electrified, this comp makes the user
+        //refuel nearby specified buildings
+
+        public int fuelingRate = 0;
+        public int fuelingRadius = 0;
+        public List<string> buildingsToAffect = null;
+        public bool mustBeTamed = false;
+
+        public CompProperties_Refueling()
+        {
+            this.compClass = typeof(CompRefueling);
+        }
+
+
+    }
+}
