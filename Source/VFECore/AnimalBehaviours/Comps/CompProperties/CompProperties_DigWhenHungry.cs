b170e98ae51d4094b447559615e12357f4870815
juanosarg <31105173+juanosarg@users.noreply.github.com>
2021-02-10 11:00:56 +0100

    Added couple options to CompDigWhenHungry

--- Source/VFECore/AnimalBehaviours/Comps/CompProperties/CompProperties_DigWhenHungry.cs
+++ Source/VFECore/AnimalBehaviours/Comps/CompProperties/CompProperties_DigWhenHungry.cs
@@ -11,9 +11,17 @@ namespace AnimalBehaviours
 
         public string customThingToDig = "";
         public int customAmountToDig = 1;
+
+        //timeToDig has a misleading name. It is a minimum counter. The user will not dig if less than timeToDig ticks have passed.
+        //This is done to avoid an animal digging again if it's still hungry.
         public int timeToDig = 40000;
+        //A list of acceptable terrains can be specified
         public List<string> acceptedTerrains = null;
+        //Should items be spawned forbidden?
         public bool spawnForbidden = false;
+        //Should the animal dig for items even if it's not hungry, every timeToDigForced ticks?
+        public bool digAnywayEveryXTicks = true;
+        public int timeToDigForced = 120000;
 
         public CompProperties_DigWhenHungry()
         {
