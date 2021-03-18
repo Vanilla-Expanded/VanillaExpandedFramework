2beca70b4402eef1b2f41443eb75f490d72d8fc2
juanosarg <31105173+juanosarg@users.noreply.github.com>
2021-03-18 15:41:48 +0100

    Added CompFilthProducer and CompDieAfterPeriod

---
+++ Source/VFECore/AnimalBehaviours/Comps/CompDieAfterPeriod.cs
@@ -0,0 +1,61 @@
+﻿using RimWorld;
+using Verse;
+using Verse.Sound;
+
+namespace AnimalBehaviours
+
+{
+    public class CompDieAfterPeriod : ThingComp
+    {
+        public int tickCounter = 0;
+
+        public CompProperties_DieAfterPeriod Props
+        {
+            get
+            {
+                return (CompProperties_DieAfterPeriod)this.props;
+            }
+        }
+
+        public override void CompTick()
+        {
+            base.CompTick();
+            tickCounter++;
+
+            if (tickCounter >= Props.timeToDieInTicks)
+            {
+                Pawn pawn = this.parent as Pawn;
+
+                if (pawn != null && pawn.Map != null)
+                {
+
+                    if (Props.effect)
+                    {
+                        for (int i = 0; i < 20; i++)
+                        {
+                            IntVec3 c;
+                            CellFinder.TryFindRandomReachableCellNear(this.parent.Position, this.parent.Map, 2, TraverseParms.For(TraverseMode.NoPassClosedDoors, Danger.Deadly, false), null, null, out c);
+                            FilthMaker.TryMakeFilth(c, this.parent.Map, ThingDef.Named(Props.effectFilth));
+                        }
+                        SoundDefOf.Hive_Spawn.PlayOneShot(new TargetInfo(this.parent.Position, this.parent.Map, false));
+                    }
+                    pawn.Kill(null);
+                }
+                tickCounter = 0;
+            }
+        }
+
+        public override string CompInspectStringExtra()
+        {
+
+
+            string text = base.CompInspectStringExtra();
+            string timeToLive = "VEF_TimeToDie".Translate((Props.timeToDieInTicks - tickCounter).ToStringTicksToPeriod(true, false, true, true));
+
+
+
+            return text + timeToLive;
+        }
+
+    }
+}
---
+++ Source/VFECore/AnimalBehaviours/Comps/CompProperties/CompProperties_FilthProducer.cs
@@ -0,0 +1,25 @@
+﻿
+using Verse;
+
+namespace AnimalBehaviours
+{
+    public class CompProperties_FilthProducer : CompProperties
+    {
+
+        //A class to make a creature produce filth around it. A new piece of filth of "filthType" is created
+        //every "ticksToCreateFilth", but ONLY "rate" percent of the time
+
+        public string filthType = "";
+        public float rate = 0f;
+        public int radius = 0;
+        public int ticksToCreateFilth = 600;
+
+
+
+        public CompProperties_FilthProducer()
+        {
+
+            this.compClass = typeof(CompFilthProducer);
+        }
+    }
+}
