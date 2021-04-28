3b16cb71eee0438131e083c96d4ca0ee35eaa71e
Kylian <kylian.balde@gmail.com>
2021-04-28 12:38:20 +0200

    Added ExcludeFromQuestsExtension - For faction def

---
+++ Source/VFECore/VFECore/DefModExtensions/ExcludeFromQuestsExtension.cs
@@ -0,0 +1,11 @@
+﻿using Verse;
+
+namespace VFECore
+{
+    internal class ExcludeFromQuestsExtension : DefModExtension
+    {
+        private static readonly ExcludeFromQuestsExtension DefaultValues = new ExcludeFromQuestsExtension();
+
+        public static ExcludeFromQuestsExtension Get(Def def) => def.GetModExtension<ExcludeFromQuestsExtension>() ?? DefaultValues;
+    }
+}
