f3b9a7944a9ce1f1952fc23a7834a12daefe6e3b
juanosarg <31105173+juanosarg@users.noreply.github.com>
2021-06-03 10:19:32 +0200

    Added CompDiseaseImmunity

---
+++ Source/VFECore/AnimalBehaviours/Comps/CompDiseaseImmunity.cs
@@ -0,0 +1,53 @@
+﻿using RimWorld;
+using Verse;
+using System.Collections.Generic;
+
+namespace AnimalBehaviours
+{
+    public class CompDiseaseImmunity : ThingComp
+    {
+
+      
+        public CompProperties_DiseaseImmunity Props
+        {
+            get
+            {
+                return (CompProperties_DiseaseImmunity)this.props;
+            }
+        }
+
+
+        public override void CompTick()
+        {
+            base.CompTick();
+            if (this.parent.IsHashIntervalTick(Props.tickInterval))
+            {
+                this.TryRemoveDiseases();
+            }
+        }
+
+        public void TryRemoveDiseases()
+        {
+            Pawn pawn = this.parent as Pawn;
+            if (pawn.health != null && pawn.health.hediffSet != null)
+            {
+                foreach (string hediff in Props.hediffsToRemove)
+                {
+                    Hediff hediffToRemove = pawn.health.hediffSet.GetFirstHediffOfDef(DefDatabase<HediffDef>.GetNamed(hediff, false));
+                    if (hediffToRemove != null)
+                    {
+                        pawn.health.RemoveHediff(hediffToRemove);
+                    }
+
+                }
+            }
+
+
+        }
+
+
+
+
+
+    }
+}
--- Source/VFECore/AnimalBehaviours/Harmony/Dialog_InfoCard_FillCard_Patch.cs
+++ Source/VFECore/AnimalBehaviours/Harmony/Dialog_InfoCard_FillCard_Patch.cs
@@ -15,7 +15,7 @@ namespace AnimalBehaviours
 		public static AnimalStatExtension extension;
 		public static Rect rect;
 
-		public static bool Prefix(Rect cardRect, Dialog_InfoCard __instance, Thing ___thing)
+		public static bool Prefix(Rect cardRect, Dialog_InfoCard __instance, Thing ___thing, Dialog_InfoCard.InfoCardTab ___tab)
 		{
 			Pawn pawn = ___thing as Pawn;
 			rect = cardRect;
@@ -35,10 +35,8 @@ namespace AnimalBehaviours
 					extension = pawn.def.GetModExtension<AnimalStatExtension>();
 					if (extension.showImageInInfoCard)
 					{
-						Type typ = typeof(Dialog_InfoCard);
-						FieldInfo type = typ.GetField("tab", BindingFlags.Instance | BindingFlags.NonPublic);
-						Dialog_InfoCard.InfoCardTab tab = (Dialog_InfoCard.InfoCardTab)type.GetValue(__instance);
-						if (tab == Dialog_InfoCard.InfoCardTab.Stats) {
+						
+						if (___tab == Dialog_InfoCard.InfoCardTab.Stats) {
 							Texture2D texture = ContentFinder<Texture2D>.Get(extension.ImageToShowInInfoCard, false);
 							Rect position = rect.AtZero();
 							position.width = 384f;
