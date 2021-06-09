dd4b825a66ddd5ebbeb8a64ecff798a4c283d385
juanosarg <31105173+juanosarg@users.noreply.github.com>
2021-06-09 10:47:22 +0200

    Fixed CompDropOnDeath

--- Source/VFECore/AnimalBehaviours/Comps/CompDropOnDeath.cs
+++ Source/VFECore/AnimalBehaviours/Comps/CompDropOnDeath.cs
@@ -9,7 +9,7 @@ namespace AnimalBehaviours
     public class CompDropOnDeath : ThingComp
     {
 
-        System.Random rand = new System.Random();
+       
 
         
 
@@ -22,47 +22,9 @@ namespace AnimalBehaviours
             }
         }
 
-        
-
-        public override void PostDestroy(DestroyMode mode, Map previousMap) {
-            Pawn pawn = this.parent as Pawn;
-            if (previousMap != null && pawn.health.Dead) {
-                if (rand.NextDouble() <= Props.dropChance)
-                {
-                    ThingDef thingDef;
-                    if (Props.isRandom)
-                    {
-
-                        thingDef = DefDatabase<ThingDef>.GetNamedSilentFail(Props.randomItems.RandomElement());
-                        if (thingDef != null)
-                        {
-                            Thing thing = ThingMaker.MakeThing(thingDef, null);
-                            thing.stackCount = Props.resourceAmount;
-                            GenPlace.TryPlaceThing(thing, this.parent.Position, this.parent.Map, ThingPlaceMode.Near, null, null, default(Rot4));
-                        }
-
-                    }
-                    else
-                    {
-                        thingDef = DefDatabase<ThingDef>.GetNamedSilentFail(Props.resourceDef);
-                        if (thingDef != null)
-                        {
-                            Thing thing = ThingMaker.MakeThing(thingDef, null);
-                            thing.stackCount = Props.resourceAmount;
-                            GenPlace.TryPlaceThing(thing, this.parent.Position, this.parent.Map, ThingPlaceMode.Near, null, null, default(Rot4));
-                        }
-                    }
-
-                }
 
 
-            }
-            
-
-            
-        
-        
-        }
+       
 
 
     }
---
+++ Source/VFECore/AnimalBehaviours/DeathActionWorkers/DeathActionWorker_DropOnDeath.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Verse;
+
+namespace AnimalBehaviours
+{
+	public class DeathActionWorker_DropOnDeath : DeathActionWorker
+	{
+
+		System.Random rand = new System.Random();
+
+		public override void PawnDied(Corpse corpse)
+		{
+			CompDropOnDeath comp = corpse.InnerPawn.TryGetComp<CompDropOnDeath>();
+			if (comp != null)
+            {
+
+
+
+                if (corpse.Map != null)
+                {
+                    if (rand.NextDouble() <= comp.Props.dropChance)
+                    {
+                        ThingDef thingDef;
+                        if (comp.Props.isRandom)
+                        {
+
+                            thingDef = DefDatabase<ThingDef>.GetNamedSilentFail(comp.Props.randomItems.RandomElement());
+                            if (thingDef != null)
+                            {
+                                Thing thing = ThingMaker.MakeThing(thingDef, null);
+                                thing.stackCount = comp.Props.resourceAmount;
+                                GenPlace.TryPlaceThing(thing, corpse.Position, corpse.Map, ThingPlaceMode.Near, null, null, default(Rot4));
+                            }
+
+                        }
+                        else
+                        {
+                            thingDef = DefDatabase<ThingDef>.GetNamedSilentFail(comp.Props.resourceDef);
+                            if (thingDef != null)
+                            {
+                                Thing thing = ThingMaker.MakeThing(thingDef, null);
+                                thing.stackCount = comp.Props.resourceAmount;
+                                GenPlace.TryPlaceThing(thing, corpse.Position, corpse.Map, ThingPlaceMode.Near, null, null, default(Rot4));
+                            }
+                        }
+
+                    }
+
+
+                }
+
+
+
+
+            }
+
+		}
+	}
+}
