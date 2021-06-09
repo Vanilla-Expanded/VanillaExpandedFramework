9970983fa93e4c40ca109badcefba813d02d4811
juanosarg <31105173+juanosarg@users.noreply.github.com>
2021-06-09 08:41:07 +0200

    Added CompDropOnDeath

---
+++ Source/VFECore/AnimalBehaviours/Comps/CompProperties/CompProperties_DropOnDeath.cs
@@ -0,0 +1,30 @@
+﻿using Verse;
+using System.Collections.Generic;
+
+
+namespace AnimalBehaviours
+{
+    public class CompProperties_DropOnDeath : CompProperties
+    {
+
+        //CompDropOnDeath makes an animal drop a resource when killed
+
+        public int resourceAmount = 1;
+        public string resourceDef = null;
+        public float dropChance = 1f;
+
+        //CompProperties_DropOnDeath allows an animal to produce random items
+
+        public bool isRandom = false;
+        public List<string> randomItems = null;
+
+
+
+        public CompProperties_DropOnDeath()
+        {
+            this.compClass = typeof(CompDropOnDeath);
+        }
+
+
+    }
+}
