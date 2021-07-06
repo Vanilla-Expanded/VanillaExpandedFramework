7b89f627438faf0cc1e942e8ad41d7d9ecb253e5
juanosarg <31105173+juanosarg@users.noreply.github.com>
2021-07-06 08:04:36 +0200

    Adding WildAnimalSpawner transpiler

---
+++ Source/VFECore/AnimalBehaviours/Defs/GenericToggleableAnimalDef.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RimWorld;
+using Verse;
+
+namespace AnimalBehaviours
+{
+    public class GenericToggleableAnimalDef : Def
+    {
+
+        //A list of pawnkind defNames
+        public List<string> toggleablePawns;
+    }
+}
---
+++ Source/VFECore/AnimalBehaviours/Options/VanillaAnimalsExpanded_Settings.cs
@@ -0,0 +1,81 @@
+﻿using RimWorld;
+using UnityEngine;
+using Verse;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+
+
+namespace AnimalBehaviours
+{
+
+
+    public class VanillaAnimalsExpanded_Settings : ModSettings
+
+    {
+
+        private static Vector2 scrollPosition = Vector2.zero;
+        public Dictionary<string, bool> pawnSpawnStates = new Dictionary<string, bool>();
+       
+        public override void ExposeData()
+        {
+            base.ExposeData();
+            Scribe_Collections.Look(ref pawnSpawnStates, "pawnSpawnStates", LookMode.Value, LookMode.Value, ref pawnKeys, ref boolValues);
+           
+
+
+        }
+        private List<string> pawnKeys;
+        private List<bool> boolValues;
+
+
+
+        public void DoWindowContents(Rect inRect)
+        {
+
+            List<string> keys = pawnSpawnStates.Keys.ToList().OrderByDescending(x => x).ToList();
+            Listing_Standard ls = new Listing_Standard();
+            Rect rect = new Rect(inRect.x, inRect.y, inRect.width, inRect.height);
+            Rect rect2 = new Rect(0f, 0f, inRect.width - 30f, keys.Count  * 24);
+            Widgets.BeginScrollView(rect, ref scrollPosition, rect2, true);
+            //ls.ColumnWidth = rect2.width / 2.2f;
+            ls.Begin(rect2);
+           
+            for (int num = keys.Count - 1; num >= 0; num--)
+            {
+               // if (num == keys.Count / 2) { ls.NewColumn(); }
+                bool test = pawnSpawnStates[keys[num]];
+
+                if (DefDatabase<PawnKindDef>.GetNamedSilentFail(keys[num]) == null)
+                {
+                    pawnSpawnStates.Remove(keys[num]);
+                }
+                else {
+                    ls.CheckboxLabeled("VEF_DisableAnimal".Translate(PawnKindDef.Named(keys[num]).LabelCap), ref test);
+                    pawnSpawnStates[keys[num]] = test;
+                }
+
+                
+            }
+
+            ls.End();
+            Widgets.EndScrollView();
+            base.Write();
+
+
+        }
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+}
