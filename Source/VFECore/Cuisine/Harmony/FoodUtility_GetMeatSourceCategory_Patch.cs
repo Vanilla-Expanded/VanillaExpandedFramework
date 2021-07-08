1a4d9cb45c1521a4db81c446d823f748c47c6fe1
juanosarg <31105173+juanosarg@users.noreply.github.com>
2021-07-08 10:08:43 +0200

    Added a patch to avoid errors on nutritionless ingredients

---
+++ Source/VFECore/Cuisine/Harmony/FoodUtility_GetMeatSourceCategory_Patch.cs
@@ -0,0 +1,35 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System.Reflection;
+using Verse;
+using System.Reflection.Emit;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using System;
+using Verse.AI;
+
+namespace VanillaCookingExpanded
+{
+    /*This Harmony Prefix avoids the creature trying to eat normal food
+    */
+    [HarmonyPatch(typeof(FoodUtility))]
+    [HarmonyPatch("GetMeatSourceCategory")]
+    public static class VanillaExpandedFramework_FoodUtility_GetMeatSourceCategory_Patch
+    {
+        [HarmonyPrefix]
+        public static bool DontCrapTheBedWithIngredientsWithoutNutrition(ThingDef source)
+
+        {
+           
+            if (source.ingestible == null)
+            {
+
+                return false;
+            }
+            else return true;
+
+
+        }
+    }
+}
