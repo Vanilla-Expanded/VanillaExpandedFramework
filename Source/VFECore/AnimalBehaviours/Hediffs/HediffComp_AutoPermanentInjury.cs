5f267347c0aa945cf6acf17c2373a904b9597813
juanosarg <31105173+juanosarg@users.noreply.github.com>
2021-07-11 14:23:57 +0200

    Added HediffComp_AutoPermanentInjury

---
+++ Source/VFECore/AnimalBehaviours/Hediffs/HediffComp_AutoPermanentInjury.cs
@@ -0,0 +1,22 @@
+﻿using RimWorld;
+using RimWorld.Planet;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using Verse;
+
+namespace AnimalBehaviours
+{
+    public class HediffComp_AutoPermanentInjury : HediffComp
+    {
+
+        public override void CompPostMake()
+        {
+            HediffComp_GetsPermanent permanentcomp = this.parent.TryGetComp<HediffComp_GetsPermanent>();
+            permanentcomp.IsPermanent = true;
+
+        }
+
+    }
+}
