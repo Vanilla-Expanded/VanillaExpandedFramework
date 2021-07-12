9710534346039bc7b74c27b587f1a3f7e68e3011
Kylian <kylian.balde@gmail.com>
2021-07-12 18:21:03 +0200

    Added Recipe Inheritance code

---
+++ Source/RecipeInheritance/RecipeInheritance/StaticConstructorClass.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using Verse;
+
+namespace RecipeInheritance
+{
+    [StaticConstructorOnStartup]
+    public static class StaticConstructorClass
+    {
+        static StaticConstructorClass()
+        {
+            for (int i = 0; i < DefDatabase<ThingDef>.AllDefsListForReading.Count; i++)
+            {
+                ThingDef thingDef = DefDatabase<ThingDef>.AllDefsListForReading[i];
+                if (thingDef.IsWorkTable)
+                {
+                    ThingDefExtension thingDefExtension = ThingDefExtension.Get(thingDef);
+                    if (thingDefExtension.inheritRecipesFrom != null)
+                    {
+                        List<RecipeDef> list = new List<RecipeDef>(thingDef.AllRecipes);
+                        NonPublicFields.ThingDef_allRecipesCached.SetValue(thingDef, null);
+
+                        for (int j = 0; j < thingDefExtension.inheritRecipesFrom.Count; j++)
+                        {
+                            ThingDef thingDef2 = thingDefExtension.inheritRecipesFrom[j];
+                            for (int k = 0; k < thingDef2.AllRecipes.Count; k++)
+                            {
+                                RecipeDef recipeDef = thingDef2.AllRecipes[k];
+                                if (thingDefExtension.Allows(recipeDef))
+                                {
+                                    if (thingDef.recipes == null)
+                                    {
+                                        thingDef.recipes = new List<RecipeDef>();
+                                    }
+                                    if (!list.Contains(recipeDef))
+                                    {
+                                        thingDef.recipes.Add(recipeDef);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Source/RecipeInheritance/RecipeInheritance/ThingDefExtension.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections.Generic;
+using Verse;
+
+namespace RecipeInheritance
+{
+    public class ThingDefExtension : DefModExtension
+    {
+        public ThingFilter allowedProductFilter;
+
+        public List<RecipeDef> allowedRecipes;
+
+        public ThingFilter disallowedProductFilter;
+
+        public List<RecipeDef> disallowedRecipes;
+
+        public List<ThingDef> inheritRecipesFrom;
+
+        private static readonly ThingDefExtension defaultValues = new ThingDefExtension();
+
+        public static ThingDefExtension Get(Def def)
+        {
+            return def.GetModExtension<ThingDefExtension>() ?? ThingDefExtension.defaultValues;
+        }
+
+        public bool Allows(RecipeDef recipe)
+        {
+            ThingDef producedThingDef = recipe.ProducedThingDef;
+            return (producedThingDef == null || ((this.allowedProductFilter == null || this.allowedProductFilter.Allows(producedThingDef)) && (this.disallowedProductFilter == null || !this.disallowedProductFilter.Allows(producedThingDef)))) && (this.allowedRecipes == null || this.allowedRecipes.Contains(recipe)) && (this.disallowedRecipes == null || !this.disallowedRecipes.Contains(recipe));
+        }
+
+        public override IEnumerable<string> ConfigErrors()
+        {
+            if (this.inheritRecipesFrom == null)
+            {
+                yield return "inheritRecipesFrom is null.";
+            }
+            else
+            {
+                List<string> nonWorkbenchDefNames = new List<string>();
+                List<string> recipelessThingDefNames = new List<string>();
+                int num;
+                for (int i = 0; i < this.inheritRecipesFrom.Count; i = num + 1)
+                {
+                    ThingDef thing = this.inheritRecipesFrom[i];
+                    if (!thing.IsWorkTable)
+                    {
+                        nonWorkbenchDefNames.Add(thing.defName);
+                    }
+                    else
+                    {
+                        if (thing.AllRecipes.NullOrEmpty())
+                        {
+                            recipelessThingDefNames.Add(thing.defName);
+                        }
+                    }
+                    num = i;
+                }
+                if (nonWorkbenchDefNames.Any())
+                {
+                    yield return "the following ThingDefs in inheritRecipesFrom are not workbenches: " + GenText.ToCommaList(nonWorkbenchDefNames, false);
+                }
+                if (recipelessThingDefNames.Any())
+                {
+                    yield return "the following workbenches in inheritRecipesFrom do not have any recipes: " + GenText.ToCommaList(recipelessThingDefNames, false);
+                }
+            }
+            yield break;
+        }
+    }
+}
