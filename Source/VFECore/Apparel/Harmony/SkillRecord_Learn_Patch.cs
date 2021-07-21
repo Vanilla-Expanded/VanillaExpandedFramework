51061384658a7a3a93ddb672707591350eedb1c8
Taranchuk <nfhfyxer@gmail.com>
2021-07-22 00:54:26 +0300

    added ApparelExtension

---
+++ Source/VFECore/Apparel/Harmony/SkillRecord_Learn_Patch.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using HarmonyLib;
+using RimWorld;
+using Verse;
+
+namespace VanillaApparelExpanded
+{
+	[HarmonyPatch(typeof(SkillRecord), "Learn")]
+	public static class SkillRecord_Learn_Patch
+	{
+		public static void Prefix(SkillRecord __instance, Pawn ___pawn, ref float xp, bool direct = false)
+		{
+			if (___pawn.apparel?.WornApparel != null)
+            {
+				foreach (var apparel in ___pawn.apparel.WornApparel)
+                {
+					var extension = apparel.def.GetModExtension<ApparelExtension>();
+					if (extension != null && extension.skillGainModifier.HasValue)
+                    {
+						xp *= extension.skillGainModifier.Value;
+					}
+                }
+			}
+		}
+	}
+}
---
+++ Source/VFECore/Apparel/ModExtensions/ApparelExtension.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using HarmonyLib;
+using RimWorld;
+using Verse;
+
+namespace VanillaApparelExpanded
+{
+    public class ApparelExtension : DefModExtension
+    {
+        public float? skillGainModifier;
+        public List<WorkTags> workDisables;
+    }
+}
