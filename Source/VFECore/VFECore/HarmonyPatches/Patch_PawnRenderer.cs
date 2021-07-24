e89583db1696e2cf2e35b96718cac4206ff317be
Joshua <legodude17@users.noreply.github.com>
2021-07-24 11:00:52 -0700

    VFECore: Proper tabard fix

--- Source/VFECore/VFECore/HarmonyPatches/Patch_PawnRenderer.cs
+++ Source/VFECore/VFECore/HarmonyPatches/Patch_PawnRenderer.cs
@@ -2,6 +2,7 @@
 using System.Linq;
 using System.Reflection.Emit;
 using HarmonyLib;
+using RimWorld;
 using Verse;
 
 // ReSharper disable once CheckNamespace
@@ -11,20 +12,44 @@ namespace VFECore
     {
         public static bool IsShell(ApparelLayerDef def)
         {
-            return def == RimWorld.ApparelLayerDefOf.Shell || def == ApparelLayerDefOf.VFEC_OuterShell;
+            return def == RimWorld.ApparelLayerDefOf.Shell || IsOuterShell(def);
+        }
+
+        public static bool IsOuterShell(ApparelLayerDef def)
+        {
+            return def == ApparelLayerDefOf.VFEC_OuterShell;
         }
 
         [HarmonyPatch(typeof(PawnRenderer), "DrawBodyApparel")]
         public static class DrawBodyApparel
         {
             [HarmonyTranspiler]
-            public static IEnumerable<CodeInstruction> Transpile(IEnumerable<CodeInstruction> instructions)
+            [HarmonyPriority(Priority.First)]
+            public static IEnumerable<CodeInstruction> Transpile(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
             {
                 var list = instructions.ToList();
                 var info1 = AccessTools.Field(typeof(RimWorld.ApparelLayerDefOf), nameof(RimWorld.ApparelLayerDefOf.Shell));
                 var idx1 = list.FindIndex(ins => ins.LoadsField(info1));
                 list[idx1] = new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(Patch_PawnRenderer), nameof(IsShell)));
                 list[idx1 + 1].opcode = OpCodes.Brfalse;
+                var idx2 = list.FindIndex(ins => ins.opcode == OpCodes.Stloc_S && ins.operand is LocalBuilder lb && lb.LocalIndex == 5);
+                var idx3 = list.FindIndex(idx2, ins => ins.opcode == OpCodes.Ldloca_S && ins.operand is LocalBuilder lb && lb.LocalIndex == 5);
+                var idx4 = list.FindIndex(idx3, ins => ins.opcode == OpCodes.Stind_R4);
+                var list2 = list.GetRange(idx3, idx4 - idx3 + 1).Select(ins => ins.Clone()).ToList();
+                var ins1 = list2.Find(ins => ins.opcode == OpCodes.Ldc_R4);
+                ins1.operand = (float) ins1.operand / 2f;
+                var label1 = generator.DefineLabel();
+                list[idx2 + 1].labels.Add(label1);
+                list.InsertRange(idx2 + 1, new[]
+                {
+                    new CodeInstruction(OpCodes.Ldloc_3),
+                    new CodeInstruction(OpCodes.Ldfld, AccessTools.Field(typeof(ApparelGraphicRecord), nameof(ApparelGraphicRecord.sourceApparel))),
+                    new CodeInstruction(OpCodes.Ldfld, AccessTools.Field(typeof(Thing), nameof(Thing.def))),
+                    new CodeInstruction(OpCodes.Ldfld, AccessTools.Field(typeof(ThingDef), nameof(ThingDef.apparel))),
+                    new CodeInstruction(OpCodes.Callvirt, AccessTools.PropertyGetter(typeof(ApparelProperties), nameof(ApparelProperties.LastLayer))),
+                    new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(Patch_PawnRenderer), nameof(IsOuterShell))),
+                    new CodeInstruction(OpCodes.Brfalse_S, label1)
+                }.Concat(list2));
                 return list;
             }
         }
@@ -33,6 +58,7 @@ namespace VFECore
         public static class MatsBodyBaseAt
         {
             [HarmonyTranspiler]
+            [HarmonyPriority(Priority.First)]
             public static IEnumerable<CodeInstruction> Transpile(IEnumerable<CodeInstruction> instructions)
             {
                 var list = instructions.ToList();
