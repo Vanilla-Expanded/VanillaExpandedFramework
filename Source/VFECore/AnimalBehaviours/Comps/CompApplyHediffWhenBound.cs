925f94b0bce5eaff357134fee4fc121fe27c5a7c
juanosarg <31105173+juanosarg@users.noreply.github.com>
2021-07-29 11:35:03 +0200

    Added CompApplyHediffWhenBound

---
+++ Source/VFECore/AnimalBehaviours/Comps/CompApplyHediffWhenBound.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Verse;
+using RimWorld;
+
+namespace AnimalBehaviours
+{
+    public class CompApplyHediffWhenBound : ThingComp
+    {
+
+
+        public CompProperties_ApplyHediffWhenBound Props
+        {
+            get
+            {
+                return (CompProperties_ApplyHediffWhenBound)this.props;
+            }
+        }
+
+        public override void CompTick()
+        {
+            base.CompTick();
+            if (this.parent.IsHashIntervalTick(Props.checkingInterval))
+            {
+                Pawn thisPawn = this.parent as Pawn;
+                foreach (Pawn pawn in PawnsFinder.AllMapsCaravansAndTravelingTransportPods_Alive_FreeColonists)
+                {
+
+                    bool flag = false;
+
+                    if (pawn.relations.DirectRelationExists(PawnRelationDefOf.Bond, thisPawn))
+                    {                        
+                        flag = true;
+                    }
+
+                    if (flag) {
+                        thisPawn.health.AddHediff(Props.hediffToApply);
+                    } else
+                    {
+                        Hediff hediff = thisPawn.health.hediffSet.GetFirstHediffOfDef(Props.hediffToApply);
+                        if (hediff != null)
+                        {
+                            thisPawn.health.RemoveHediff(hediff);
+                        }
+                    }
+
+
+
+
+                }
+
+            }
+        }
+
+
+
+
+
+    }
+}
+
---
+++ Source/VFECore/AnimalBehaviours/Comps/CompProperties/CompProperties_ApplyHediffWhenBound.cs
@@ -0,0 +1,19 @@
+﻿
+using Verse;
+
+namespace AnimalBehaviours
+{
+    public class CompProperties_ApplyHediffWhenBound : CompProperties
+    {
+
+        public int checkingInterval = 1000;
+        public HediffDef hediffToApply;
+
+        public CompProperties_ApplyHediffWhenBound()
+        {
+            this.compClass = typeof(CompApplyHediffWhenBound);
+        }
+
+
+    }
+}
