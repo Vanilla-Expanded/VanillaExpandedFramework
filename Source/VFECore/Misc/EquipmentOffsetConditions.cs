7c034d55f99990fae6add60544d6cad7e35926b3
Joshua <legodude17@users.noreply.github.com>
2021-08-02 16:17:35 -0700

    Add VEF_RangedCooldownFactor system

---
+++ Source/VFECore/Misc/EquipmentOffsetConditions.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using RimWorld;
+using Verse;
+
+namespace VFECore.Misc
+{
+    public class EquipmentOffsetConditions : DefModExtension
+    {
+        public List<TechLevel> techLevels;
+
+        public bool IsValid(Thing weapon, ThingDef apparelDef)
+        {
+            var weaponType = weapon.def.Verbs?.Any(v =>
+                v.verbClass == typeof(Verb_Shoot) || v.verbClass.IsSubclassOf(typeof(Verb_Shoot))) ?? false;
+            var techLevel = techLevels?.Contains(weapon.def.techLevel) ?? true;
+            return weaponType && techLevel;
+        }
+    }
+}
---
+++ Source/VFECore/Misc/StatPart_Ammo.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using RimWorld;
+using Verse;
+
+namespace VFECore.Misc
+{
+    [StaticConstructorOnStartup]
+    public class StatPart_Ammo : StatPart
+    {
+        private static StatDef statRangedCooldownFactor;
+
+        static StatPart_Ammo()
+        {
+            LongEventHandler.ExecuteWhenFinished(() =>
+                statRangedCooldownFactor = StatDef.Named("VEF_RangedCooldownFactor"));
+        }
+
+        public override void TransformValue(StatRequest req, ref float val)
+        {
+            if (!req.HasThing || !(req.Thing.ParentHolder is Pawn_EquipmentTracker eq)) return;
+            foreach (var thing in eq.AllEquipmentListForReading.Concat(eq.pawn.apparel.WornApparel))
+                if (!(thing.def.GetModExtension<EquipmentOffsetConditions>() is EquipmentOffsetConditions conds) || conds.IsValid(req.Thing, thing.def))
+                    val *= thing.GetStatValue(statRangedCooldownFactor);
+        }
+
+        public override string ExplanationPart(StatRequest req)
+        {
+            if (!req.HasThing || !(req.Thing.ParentHolder is Pawn_EquipmentTracker eq))
+                return "";
+            var builder = new StringBuilder();
+            foreach (var (thing, conds) in from apparel in eq.AllEquipmentListForReading.Concat(eq.pawn.apparel.WornApparel)
+                let conds = apparel.def
+                    .GetModExtension<EquipmentOffsetConditions>()
+                where conds != null || Math.Abs(apparel.GetStatValue(statRangedCooldownFactor) - 1f) > 0.001f
+                select (apparel, conds))
+                builder.AppendLine(
+                    conds.IsValid(req.Thing, thing.def)
+                        ? $"{thing.def.LabelCap}: {statRangedCooldownFactor.Worker.ValueToString(thing.GetStatValue(statRangedCooldownFactor), true, ToStringNumberSense.Factor)}"
+                        : $"{thing.def.LabelCap}: {req.Thing.def.LabelCap} {"VFECore.IsToo".Translate()} {(req.Thing.def.techLevel > conds.techLevels.Max() ? "VFECore.Advanced".Translate() : "VFECore.Primitive".Translate())}");
+            return builder.ToString();
+        }
+    }
+}
