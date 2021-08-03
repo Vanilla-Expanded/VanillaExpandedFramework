69cd3bc27390597357980531f3eeed030eee2052
juanosarg <31105173+juanosarg@users.noreply.github.com>
2021-08-03 15:16:36 +0200

    Added CompEnrageOtherPawns and CompChangeDefIfNotUnique

---
+++ Source/VFECore/AnimalBehaviours/Comps/CompProperties/CompProperties_ChangeDefIfNotUnique.cs
@@ -0,0 +1,19 @@
+﻿using Verse;
+
+namespace AnimalBehaviours
+{
+    public class CompProperties_ChangeDefIfNotUnique : CompProperties
+    {
+
+        //A comp class that will change this animal to a different one if it is not the only one in the map
+
+        public string defToChangeTo = "";
+
+        public CompProperties_ChangeDefIfNotUnique()
+        {
+            this.compClass = typeof(CompChangeDefIfNotUnique);
+        }
+
+
+    }
+}
---
+++ Source/VFECore/AnimalBehaviours/Comps/CompProperties/CompProperties_EnrageOtherPawns.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using Verse;
+
+namespace AnimalBehaviours
+{
+    public class CompProperties_EnrageOtherPawns : CompProperties
+    {
+
+        //A comp class that will make other animals on the map go manhunter if it goes manhunter
+
+        public List<PawnKindDef> pawnkinddefsToAffect;
+        public int checkingInterval = 200;
+
+        public CompProperties_EnrageOtherPawns()
+        {
+            this.compClass = typeof(CompEnrageOtherPawns);
+        }
+
+
+    }
+}
