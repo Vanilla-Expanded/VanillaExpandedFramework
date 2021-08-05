bc2c17239401aa386cf6d733a7f0363e31b6a18c
Taranchuk <nfhfyxer@gmail.com>
2021-08-05 20:56:31 +0300

    Bugfixes.
    Fixed the bug with smoothed walls destroying upon placing wall lights
    Fixed the collection changed bug with destroying walls with wall lights.

--- Source/VFECore/Furniture/Harmony/Building_Destroy.cs
+++ Source/VFECore/Furniture/Harmony/Building_Destroy.cs
@@ -17,15 +17,35 @@ namespace VanillaFurnitureExpanded
 	{
 		public static void Prefix(Building __instance)
 		{
+			Log.Message("Destroy: " + __instance);
 			if (__instance != null && __instance.def != null && __instance.def.passability == Traversability.Impassable && __instance.Map != null)
 			{
-				foreach (var t in __instance.Position.GetThingList(__instance.Map).Where(b => b != __instance))
+				foreach (var t in __instance.Position.GetThingList(__instance.Map).Where(b => b != __instance).ToList())
 				{
 					var mountableComp = t.TryGetComp<CompMountableOnWall>();
 					if (mountableComp != null)
                     {
 						t.Destroy(DestroyMode.Refund);
-                    }
+						Log.Message("2 Destroy: " + __instance + " - " + t);
+					}
+				}
+			}
+		}
+	}
+
+	[HarmonyPatch(typeof(GenConstruct), "BlocksConstruction")]
+	public static class Patch_BlocksConstruction
+	{
+		public static void Postfix(Thing constructible, Thing t, ref bool __result)
+		{
+			if (__result)
+			{
+				ThingDef thingDef = ((constructible is Blueprint) ? constructible.def : ((!(constructible is Frame)) 
+					? constructible.def.blueprintDef : constructible.def.entityDefToBuild.blueprintDef));
+				ThingDef thingDef2 = thingDef.entityDefToBuild as ThingDef;
+				if (t.def.IsSmoothed && thingDef2.HasComp(typeof(CompMountableOnWall)) && thingDef2.building != null && thingDef2.building.canPlaceOverWall)
+				{
+					__result = false;
 				}
 			}
 		}
--- Source/VFECore/Furniture/Placeworkers/PlaceWorker_OnWall.cs
+++ Source/VFECore/Furniture/Placeworkers/PlaceWorker_OnWall.cs
@@ -14,7 +14,7 @@ namespace VanillaFurnitureExpanded
 			if (adjacementCell.InBounds(map))
 			{
 				var adjacementEdifice = adjacementCell.GetEdifice(map);
-				if (adjacementEdifice != null && ((adjacementEdifice?.def.defName.ToLower().Contains("wall") ?? false) || adjacementEdifice.def.IsSmoothed))
+				if (adjacementEdifice != null)
 				{
 					return false;
 				}
