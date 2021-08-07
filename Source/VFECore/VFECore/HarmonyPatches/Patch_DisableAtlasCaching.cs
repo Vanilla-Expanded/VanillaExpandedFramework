061c04336688d34e630d4f4526e34e54a565f6cb
Simon Latusek <simon.latusek@gmail.com>
2021-08-07 06:23:01 +0200

    atlas compression

--- Source/VFECore/VFECore/HarmonyPatches/Patch_DisableAtlasCaching.cs
+++ Source/VFECore/VFECore/HarmonyPatches/Patch_DisableAtlasCaching.cs
@@ -8,12 +8,15 @@ namespace VFECore
 {
     using System.Reflection;
     using System.Reflection.Emit;
+    using System.Runtime.CompilerServices;
     using HarmonyLib;
+    using RimWorld;
+    using UnityEngine;
     using Verse;
 
 
     [HarmonyPatch(typeof(PawnRenderer), nameof(PawnRenderer.RenderPawnAt))]
-    public static class RenderPawnAt
+    public static class Patch_RenderPawnAt
     {
         [HarmonyTranspiler]
         public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
@@ -32,7 +35,7 @@ namespace VFECore
                     yield return new CodeInstruction(OpCodes.And);
 
                     yield return new CodeInstruction(OpCodes.Ldloc_1);
-                    yield return CodeInstruction.Call(typeof(RenderPawnAt), nameof(ChangeFlags));
+                    yield return CodeInstruction.Call(typeof(Patch_RenderPawnAt), nameof(ChangeFlags));
                     yield return new CodeInstruction(OpCodes.Stloc_1);
                 }
             }
@@ -58,6 +61,59 @@ namespace VFECore
         {
             __result = !VFEGlobal.settings.disableCaching;
             return __result;
+        }
+    }
+
+    [HarmonyPatch]
+    public static class Patch_BakeStaticAtlasCompiler
+    {
+        public static readonly AccessTools.FieldRef<StaticTextureAtlas, List<Texture2D>> textureAccess = AccessTools.FieldRefAccess<StaticTextureAtlas, List<Texture2D>>("textures");
+
+        [HarmonyTargetMethod]
+        public static MethodBase TargetMethod() => 
+            AccessTools.GetDeclaredMethods(typeof(GlobalTextureAtlasManager)).First(mi => mi.HasAttribute<CompilerGeneratedAttribute>());
+
+        [HarmonyTranspiler]
+        public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            MethodInfo bakeInfo = AccessTools.Method(typeof(StaticTextureAtlas), nameof(StaticTextureAtlas.Bake));
+
+            foreach (CodeInstruction instruction in instructions)
+            {
+                yield return instruction;
+
+                if (instruction.Calls(bakeInfo))
+                {
+                    yield return new CodeInstruction(OpCodes.Ldloc_0);
+                    yield return CodeInstruction.Call(typeof(Patch_BakeStaticAtlasCompiler), nameof(CompressTextureAtlas));
+                }
+            }
+        }
+
+        public static void CompressTextureAtlas(StaticTextureAtlas atlas)
+        {
+            atlas.ColorTexture.Compress(true);
+            atlas.MaskTexture?.Compress(true);
+        }
+    }
+
+    [HarmonyPatch(typeof(StaticTextureAtlas), nameof(StaticTextureAtlas.Bake))]
+    public static class Patch_BakeStaticAtlas
+    {
+        [HarmonyTranspiler]
+        public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            MethodInfo bakeInfo = AccessTools.Method(typeof(StaticTextureAtlas), nameof(StaticTextureAtlas.Bake));
+
+            foreach (CodeInstruction instruction in instructions)
+            {
+                if (instruction.opcode == OpCodes.Stloc_S && instruction.operand is LocalBuilder lb && lb.LocalIndex == 4)
+                {
+                    yield return new CodeInstruction(OpCodes.Pop);
+                    yield return new CodeInstruction(OpCodes.Ldc_I4_0);
+                }
+                yield return instruction;
+            }
         }
     }
 }
