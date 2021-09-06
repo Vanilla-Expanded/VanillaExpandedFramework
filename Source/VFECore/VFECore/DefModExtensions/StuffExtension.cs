0de4b61b61f6ab6b9eaa52abffc5101ba4b7acb2
Taranchuk <nfhfyxer@gmail.com>
2021-09-06 17:56:23 +0300

    added StuffExtension to manage stuff commonalities better

---
+++ Source/VFECore/VFECore/DefModExtensions/StuffExtension.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using Verse;
+using RimWorld;
+
+namespace VFECore
+{
+
+    public class StuffExtension : DefModExtension
+    {
+        public float? structureGenerationCommonalityFactor;
+        public float? weaponGenerationCommonalityFactor;
+        public float? apparelGenerationCommonalityFactor;
+
+        public float? structureGenerationCommonalityOffset;
+        public float? weaponGenerationCommonalityOffset;
+        public float? apparelGenerationCommonalityOffset;
+    }
+}
