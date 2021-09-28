730661f93869687d222c2b10b9d121ff85d71a28
juanosarg <31105173+juanosarg@users.noreply.github.com>
2021-09-28 15:12:21 +0200

    Added Plant_NarrowTemperatures

---
+++ Source/VFECore/Cuisine/Plants/Plant_NarrowTemperatures.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RimWorld;
+using UnityEngine;
+
+using Verse;
+
+namespace VanillaCookingExpanded
+{
+    public class Plant_NarrowTemperatures : Plant
+    {
+
+        public override float GrowthRate
+        {
+            get
+            {
+                if (this.Blighted)
+                {
+                    return 0f;
+                }
+                if (base.Spawned && !PlantUtility.GrowthSeasonNow(base.Position, base.Map, false))
+                {
+                    return 0.1f;
+                }
+                return this.GrowthRateFactor_Fertility * this.GrowthRateFactor_TemperatureNarrow * this.GrowthRateFactor_Light;
+            }
+        }
+
+        public float GrowthRateFactor_TemperatureNarrow
+        {
+            get
+            {
+                float num;
+                if (!GenTemperature.TryGetTemperatureForCell(base.Position, base.Map, out num))
+                {
+                    return 1f;
+                }
+                if (num < 20f)
+                {
+                    return Mathf.InverseLerp(0f, 20f, num);
+                }
+                if (num > 30f)
+                {
+                    return Mathf.InverseLerp(50f, 30f, num);
+                }
+                return 1f;
+            }
+        }
+
+        public override string GetInspectString()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            if (this.LifeStage == PlantLifeStage.Growing)
+            {
+                stringBuilder.AppendLine("PercentGrowth".Translate(this.GrowthPercentString));
+                stringBuilder.AppendLine("GrowthRate".Translate() + ": " + this.GrowthRate.ToStringPercent());
+                if (!this.Blighted)
+                {
+                    if (this.Resting)
+                    {
+                        stringBuilder.AppendLine("PlantResting".Translate());
+                    }
+                    if (!this.HasEnoughLightToGrow)
+                    {
+                        stringBuilder.AppendLine("PlantNeedsLightLevel".Translate() + ": " + this.def.plant.growMinGlow.ToStringPercent());
+                    }
+                    float growthRateFactor_Temperature = this.GrowthRateFactor_TemperatureNarrow;
+                    if (growthRateFactor_Temperature < 0.99f)
+                    {
+                        if (growthRateFactor_Temperature < 0.01f)
+                        {
+                            stringBuilder.AppendLine("OutOfIdealTemperatureRangeNotGrowing".Translate());
+                        }
+                        else
+                        {
+                            stringBuilder.AppendLine("OutOfIdealTemperatureRange".Translate(Mathf.RoundToInt(growthRateFactor_Temperature * 100f).ToString()));
+                        }
+                    }
+                }
+            }
+            else if (this.LifeStage == PlantLifeStage.Mature)
+            {
+                if (this.HarvestableNow)
+                {
+                    stringBuilder.AppendLine("ReadyToHarvest".Translate());
+                }
+                else
+                {
+                    stringBuilder.AppendLine("Mature".Translate());
+                }
+            }
+            if (this.DyingBecauseExposedToLight)
+            {
+                stringBuilder.AppendLine("DyingBecauseExposedToLight".Translate());
+            }
+            if (this.Blighted)
+            {
+                stringBuilder.AppendLine("Blighted".Translate() + " (" + this.Blight.Severity.ToStringPercent() + ")");
+            }
+            string text = base.InspectStringPartsFromComps();
+            if (!text.NullOrEmpty())
+            {
+                stringBuilder.Append(text);
+            }
+            return stringBuilder.ToString().TrimEndNewlines();
+        }
+    }
+}
