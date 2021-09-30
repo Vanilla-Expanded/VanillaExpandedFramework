2db2c2def5a7506f1f95e8493b06270e4eb4835d
juanosarg <31105173+juanosarg@users.noreply.github.com>
2021-09-30 08:34:41 +0200

    Draftability patches reduced in impact

--- Source/VFECore/AnimalBehaviours/Harmony/Draftability/Pawn_GetGizmos_Patch.cs
+++ Source/VFECore/AnimalBehaviours/Harmony/Draftability/Pawn_GetGizmos_Patch.cs
@@ -12,62 +12,59 @@ namespace AnimalBehaviours
 
 
 
-    /*This second Harmony postfix adds or removes gizmos from the pawn's gizmo list (which is actually IEnumerable)
+    /*This pass-through postfix adds or removes gizmos from the pawn's gizmo list (which is actually IEnumerable)
      * 
      */
-   /* [HarmonyPatch(typeof(Pawn))]
-    [HarmonyPatch("GetGizmos")]
+     [HarmonyPatch(typeof(Pawn))]
+     [HarmonyPatch("GetGizmos")]
 
-    static class VanillaExpandedFramework_Pawn_GetGizmos_Patch
-    {
-        [HarmonyPostfix]
+     static class VanillaExpandedFramework_Pawn_GetGizmos_Patch
+     {
+         [HarmonyPostfix]
 
-        public static void AddGizmo(Pawn __instance, ref IEnumerable<Gizmo> __result)
-        {
-         
+         public static IEnumerable<Gizmo> AddGizmo(IEnumerable<Gizmo> __result, Pawn __instance)
+         {
+            foreach (var gizmo in __result) yield return gizmo;
             var pawn = __instance;
-            var gizmos = __result.ToList();
-           
+            
 
             bool flagIsCreatureMine = pawn.Faction != null && pawn.Faction.IsPlayer;
-            bool flagIsCreatureDraftable = AnimalCollectionClass.draftable_animals.ContainsKey(pawn);
+             bool flagIsCreatureDraftable = AnimalCollectionClass.draftable_animals.ContainsKey(pawn);
 
 
-            if (flagIsCreatureDraftable && flagIsCreatureMine && pawn.drafter != null)
-            {
+             if (flagIsCreatureDraftable && flagIsCreatureMine && pawn.drafter != null)
+             {
 
 
-                Command_Toggle drafting_command = new Command_Toggle();
-                drafting_command.toggleAction = delegate
-                {
-                    pawn.drafter.Drafted = !pawn.drafter.Drafted;
-                };
-                drafting_command.isActive = (() => pawn.drafter.Drafted);
-                drafting_command.defaultLabel = (pawn.drafter.Drafted ? "CommandUndraftLabel" : "CommandDraftLabel").Translate();
-                drafting_command.hotKey = KeyBindingDefOf.Command_ColonistDraft;
-                drafting_command.defaultDesc = "CommandToggleDraftDesc".Translate();
-                drafting_command.icon = ContentFinder<Texture2D>.Get("ui/commands/Draft", true);
-                drafting_command.turnOnSound = SoundDefOf.DraftOn;
-                drafting_command.groupKey = 81729172;
-                drafting_command.turnOffSound = SoundDefOf.DraftOff;
-                gizmos.Insert(0, drafting_command);
+                 Command_Toggle drafting_command = new Command_Toggle();
+                 drafting_command.toggleAction = delegate
+                 {
+                     pawn.drafter.Drafted = !pawn.drafter.Drafted;
+                 };
+                 drafting_command.isActive = (() => pawn.drafter.Drafted);
+                 drafting_command.defaultLabel = (pawn.drafter.Drafted ? "CommandUndraftLabel" : "CommandDraftLabel").Translate();
+                 drafting_command.hotKey = KeyBindingDefOf.Command_ColonistDraft;
+                 drafting_command.defaultDesc = "CommandToggleDraftDesc".Translate();
+                 drafting_command.icon = ContentFinder<Texture2D>.Get("ui/commands/Draft", true);
+                 drafting_command.turnOnSound = SoundDefOf.DraftOn;
+                 drafting_command.groupKey = 81729172;
+                 drafting_command.turnOffSound = SoundDefOf.DraftOff;
+                 yield return drafting_command;
 
 
-            }
+             }
 
 
 
 
-
-            __result = gizmos;
-        }
+         }
 
 
 
 
 
 
-    }
+     }
 
-    */
+     
 }
--- Source/VFECore/AnimalBehaviours/Harmony/Draftability/Pawn_IsColonistPlayerControlled_Patch.cs
+++ Source/VFECore/AnimalBehaviours/Harmony/Draftability/Pawn_IsColonistPlayerControlled_Patch.cs
@@ -12,11 +12,7 @@ using RimWorld.Planet;
 namespace AnimalBehaviours
 {
 
-    /*This first Harmony postfix deals with adding a Pawn_DraftController if it detects the creature
-     * belongs to the player and to the custom class CompDraftable. It also adds a Pawn_EquipmentTracker
-     * because some ugly errors are produced otherwise, though it is basically unused
-     * 
-     */
+    
     [HarmonyPatch(typeof(Pawn))]
     [HarmonyPatch("IsColonistPlayerControlled", MethodType.Getter)]
     public static class VanillaExpandedFramework_Pawn_IsColonistPlayerControlled_Patch
@@ -25,10 +21,10 @@ namespace AnimalBehaviours
         static void AddAnimalAsColonist(Pawn __instance, ref bool __result)
         {
             bool flagIsCreatureDraftable = AnimalCollectionClass.draftable_animals.ContainsKey(__instance);
-            bool flagIsMindControlBuildingPresent = AnimalCollectionClass.numberOfAnimalControlHubsBuilt > 0;
-            if (flagIsCreatureDraftable && flagIsMindControlBuildingPresent)
+          
+            if (flagIsCreatureDraftable)
             {
-                __result = __instance.Spawned && __instance.HostFaction == null;
+                __result = __instance.Spawned && __instance.HostFaction == null && __instance.Faction == Faction.OfPlayer;
             }
         }
     }
