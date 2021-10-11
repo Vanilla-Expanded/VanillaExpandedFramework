443ed4755380e54dff0c2354d4e0fb85aa95ffef
juanosarg <31105173+juanosarg@users.noreply.github.com>
2021-10-11 09:29:02 +0200

    Added pairedMeme to ExtendedMemeProperties

--- Source/VFECore/Memes/DefModExtensions/ExtendedMemeProperties.cs
+++ Source/VFECore/Memes/DefModExtensions/ExtendedMemeProperties.cs
@@ -13,10 +13,19 @@ namespace VanillaMemesExpanded
 
     public class ExtendedMemeProperties : DefModExtension
     {
-
+        //Used to make a meme only be choosable for a certain structure
         public string neededMeme;
+
+        //Used to make a meme only be choosable if another different meme is chosen too
+        public string pairedMeme;
+
+        //Used to make all members of the Ideoligion acquire a given trait
         public TraitDef forcedTrait;
+
+        //Used to make the colony start with an opinion offset with all factions
         public int factionOpinionOffset;
+
+        //Used to make all members of the Ideoligion acquire a given ability
         public List<AbilityDef> abilitiesGiven;
     }
 
---
+++ Source/VFECore/Memes/Harmony/Dialog_ChooseMemes_TryAccept.cs
@@ -0,0 +1,47 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System.Reflection;
+using Verse;
+using System.Reflection.Emit;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using System;
+using Verse.AI;
+
+namespace VanillaMemesExpanded
+{
+
+    [HarmonyPatch(typeof(Dialog_ChooseMemes))]
+    [HarmonyPatch("TryAccept")]
+    public static class VanillaMemesExpanded_Dialog_ChooseMemes_TryAccept_Patch
+    {
+        [HarmonyPrefix]
+        public static bool DetectIfPairedMeme(ref List<MemeDef> ___newMemes)
+
+        {
+           
+            for (int i = 0; i < ___newMemes.Count; i++)
+            {
+                ExtendedMemeProperties extendedMemeProps = ___newMemes[i].GetModExtension<ExtendedMemeProperties>();
+                if (extendedMemeProps != null && extendedMemeProps.pairedMeme!=null)
+                {
+                    if (!___newMemes.Contains(DefDatabase<MemeDef>.GetNamedSilentFail(extendedMemeProps.pairedMeme)))
+                    {
+                        Messages.Message("VME_MessageNeedsThePairedMeme".Translate(___newMemes[i].label, DefDatabase<MemeDef>.GetNamedSilentFail(extendedMemeProps.pairedMeme)?.label), MessageTypeDefOf.RejectInput, false);
+                        return false;
+                    }
+                   
+                }
+
+
+
+
+
+            }
+            return true;
+
+
+        }
+    }
+}
