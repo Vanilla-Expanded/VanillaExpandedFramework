f71414f200675167799bba35616fe05dc907ce92
KylianB <kylian.balde@gmail.com>
2021-10-18 13:45:24 +0200

    KCSG - Item can use stuffDef symbol tag

--- Source/KCSG/Utils/GenUtils.cs
+++ Source/KCSG/Utils/GenUtils.cs
@@ -71,7 +71,7 @@ namespace KCSG
                         }
                         else if (temp.thingDef?.category == ThingCategory.Item && cell.Walkable(map))
                         {
-                            thing = ThingMaker.MakeThing(temp.thingDef, temp.thingDef.stuffCategories?.Count > 0 ? GenStuff.RandomStuffFor(temp.thingDef) : null);
+                            thing = ThingMaker.MakeThing(temp.thingDef, temp.stuffDef ?? (temp.thingDef.stuffCategories?.Count > 0 ? GenStuff.RandomStuffFor(temp.thingDef) : null));
                             thing.stackCount = Mathf.Clamp(Rand.RangeInclusive(1, temp.thingDef.stackLimit), 1, 75);
 
                             CompQuality quality = thing.TryGetComp<CompQuality>();
