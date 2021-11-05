585c1a886d957537f3686cc1fd1364e97db6a011
Simon Latusek <simon.latusek@gmail.com>
2021-11-05 14:08:37 +0100

    abilities are not forced but considered over weapons

--- Source/VFECore/Abilities/TryGetAttackVerb_Patch.cs
+++ Source/VFECore/Abilities/TryGetAttackVerb_Patch.cs
@@ -13,23 +13,22 @@ namespace VFECore.Abilities
     [HarmonyPatch(typeof(Pawn), nameof(Pawn.TryGetAttackVerb))]
     public static class TryGetAttackVerb_Patch
     {
-        [HarmonyPrefix]
-        public static bool Prefix(Pawn __instance, ref Verb __result, Thing target)
+        [HarmonyPostfix]
+        public static void Postfix(Pawn __instance, ref Verb __result, Thing target)
         {
             CompAbilities compAbilities = __instance.TryGetComp<CompAbilities>();
             if (compAbilities == null)
-                return true;
+                return;
 
             List<Verb_CastAbility> verbs = compAbilities.LearnedAbilities.Where(ab => ab.CanAutoCast && ab.IsEnabledForPawn(out string _) && ab.CanHitTarget(target)).Select(ab => ab.verb).ToList();
 
             if (verbs.NullOrEmpty())
-                return true;
+                return;
 
-            if (!verbs.TryRandomElementByWeight(ve => ve.ability.Chance, out Verb_CastAbility verb))
-                return true;
+            if (!verbs.Select(ve => new Tuple<Verb, float>(ve, ve.ability.Chance)).AddItem(new Tuple<Verb, float>(__result, 1f)).TryRandomElementByWeight(t => t.Item2, out Tuple<Verb, float> result))
+                return;
 
-            __result = verb;
-            return false;
+            __result = result.Item1;
         }
     }
 }
