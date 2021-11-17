9f2d9d01955057aff407181b90c6ac64dc2420e4
juanosarg <31105173+juanosarg@users.noreply.github.com>
2021-11-17 13:32:47 +0100

    Added compPawnOverlay

---
+++ Source/VFECore/AnimalBehaviours/Comps/CompPawnOverlay.cs
@@ -0,0 +1,25 @@
+﻿using RimWorld;
+using UnityEngine;
+using Verse;
+
+namespace AnimalBehaviours
+{
+    [StaticConstructorOnStartup]
+    public class CompPawnOverlay : CompFireOverlayBase
+    {
+        public new CompProperties_PawnOverlay Props => (CompProperties_PawnOverlay)props;
+
+        public override void PostDraw()
+        {
+            base.PostDraw();
+            CompProperties_PawnOverlay props = Props;
+            Vector3 drawPos = parent.DrawPos;
+            for (int i = 0; i < props.graphicElements.Count; i++)
+
+            {
+                drawPos.y += 0.04054054f;
+                props.graphicElements[i].Graphic.Draw(drawPos, parent.Rotation, (Thing)this.parent);
+            }
+        }
+    }
+}
---
+++ Source/VFECore/AnimalBehaviours/Comps/CompProperties/CompProperties_PawnOverlay.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using RimWorld;
+using UnityEngine;
+using Verse;
+
+namespace AnimalBehaviours
+{
+    public class CompProperties_PawnOverlay : CompProperties_FireOverlay
+    {
+        public List<GraphicData> graphicElements;
+
+        public CompProperties_PawnOverlay()
+        {
+            this.compClass = typeof(CompPawnOverlay);
+        }
+
+        public override void DrawGhost(IntVec3 center, Rot4 rot, ThingDef thingDef, Color ghostCol, AltitudeLayer drawAltitude, Thing thing = null)
+        {
+            Vector3 centerVec = center.ToVector3ShiftedWithAltitude(drawAltitude);
+            for (int i = 0; i < graphicElements.Count; i++)
+            {
+                GhostUtility.GhostGraphicFor(graphicElements[i].Graphic, thingDef, ghostCol).DrawFromDef(centerVec, rot, thingDef);
+                centerVec.y += 0.04054054f;
+            }
+        }
+    }
+}
