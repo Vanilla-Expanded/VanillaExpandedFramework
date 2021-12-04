c77ca3d2b980ce60df9f42a878a9fe04d8a0c201
Taranchuk <nfhfyxer@gmail.com>
2021-12-04 18:27:07 +0200

    added MatsBodyBaseAt and DrawPawnBody transpilers

--- Source/VFECore/VFECore/HarmonyPatches/Apparel_Draw_Patches.cs
+++ Source/VFECore/VFECore/HarmonyPatches/Apparel_Draw_Patches.cs
@@ -10,12 +10,14 @@ using System.Threading.Tasks;
 using UnityEngine;
 using Verse;
 using Verse.AI;
+using static RimWorld.FleshTypeDef;
 
 namespace VFECore
 {
     public class ApparelDrawPosExtension : DefModExtension
     {
         public DrawSettings headgearDrawSettings;
+        public DrawSettings apparelDrawSettings;
     }
     public class DrawSettings
     {
@@ -72,6 +74,10 @@ namespace VFECore
                     yield return new CodeInstruction(OpCodes.Stloc_3);
                 }
             }
+            if (!found)
+            {
+                Log.Error("[Vanilla Framework Expanded] Transpiler on PawnRenderer:DrawHeadHair+DrawApparel failed.");
+            }
         }
 
         public static Vector3 ModifyHeadGearLoc(Pawn pawn, Vector3 loc, ApparelGraphicRecord apparelRecord)
@@ -82,6 +88,97 @@ namespace VFECore
                 return extension.headgearDrawSettings.GetDrawPosOffset(pawn, loc);
             }
             return loc;
+        }
+    }
+
+    [HarmonyPatch(typeof(PawnGraphicSet), "MatsBodyBaseAt")]
+    public static class Patch_PawnGraphicSet_MatsBodyBaseAt_Transpiler
+    {
+        public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> codeInstructions)
+        {
+            var apparelGraphicsField = AccessTools.Field(typeof(PawnGraphicSet), "apparelGraphics");
+            var matsAtMethod = AccessTools.Method(typeof(Graphic), nameof(Graphic.MatAt));
+            var codes = codeInstructions.ToList();
+            bool foundApparelGraphicsField = false;
+            for (var i = 0; i < codes.Count; i++)
+            {
+                yield return codes[i];
+                if (codes[i].LoadsField(apparelGraphicsField))
+                {
+                    foundApparelGraphicsField = true;
+                }
+                if (foundApparelGraphicsField && codes[i].Calls(matsAtMethod))
+                {
+                    yield return new CodeInstruction(OpCodes.Dup);
+                    yield return new CodeInstruction(OpCodes.Ldarg_0);
+                    yield return new CodeInstruction(OpCodes.Ldfld, apparelGraphicsField);
+                    yield return new CodeInstruction(OpCodes.Ldloc_1);
+                    yield return new CodeInstruction(OpCodes.Callvirt, AccessTools.Method(typeof(List<ApparelGraphicRecord>), "get_Item"));
+                    yield return new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(Patch_PawnGraphicSet_MatsBodyBaseAt_Transpiler), nameof(MapValue)));
+                }
+            }
+        }
+
+        public static void MapValue(Material mat, ApparelGraphicRecord apparelGraphicRecord)
+        {
+            Patch_PawnRenderer_DrawPawnBody_Transpiler.mappedValues[mat] = apparelGraphicRecord;
+        }
+    }
+    
+    [HarmonyPatch(typeof(PawnRenderer), "DrawPawnBody")]
+    public static class Patch_PawnRenderer_DrawPawnBody_Transpiler
+    {
+        public static Vector3 oldVector;
+    
+        public static Dictionary<Material, ApparelGraphicRecord> mappedValues = new Dictionary<Material, ApparelGraphicRecord>();
+        public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> codeInstructions)
+        {
+            var codes = codeInstructions.ToList();
+            bool found1 = false;
+            bool found2 = false;
+            var drawMeshNowOrLaterMethod = AccessTools.Method(typeof(GenDraw), nameof(GenDraw.DrawMeshNowOrLater), new Type[] { typeof(Mesh), typeof(Vector3), typeof(Quaternion), typeof(Material), typeof(bool) });
+            for (var i = 0; i < codes.Count; i++)
+            {
+                if (!found1 && i > 3 && codes[i - 1].opcode == OpCodes.Stloc_S
+                    && codes[i - 1].operand is LocalBuilder lb && lb.LocalIndex == 5
+                    && codes[i].opcode == OpCodes.Ldarg_S && codes[i].OperandIs(6))
+                {
+                    found1 = true;
+                    yield return new CodeInstruction(OpCodes.Ldarg_0);
+                    yield return new CodeInstruction(OpCodes.Ldfld, AccessTools.Field(typeof(PawnRenderer), "pawn"));
+                    yield return new CodeInstruction(OpCodes.Ldloca_S, 1);
+                    yield return new CodeInstruction(OpCodes.Ldloc_S, 5);
+                    yield return new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(Patch_PawnRenderer_DrawPawnBody_Transpiler), nameof(ModifyApparelLoc)));
+                }
+                yield return codes[i];
+                if (found1 && !found2 && codes[i].Calls(drawMeshNowOrLaterMethod))
+                {
+                    found2 = true;
+                    yield return new CodeInstruction(OpCodes.Ldloca_S, 1);
+                    yield return new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(Patch_PawnRenderer_DrawPawnBody_Transpiler), nameof(ResetVector)));
+}
+            }
+            if (!found1 && !found2)
+            {
+                Log.Error("[Vanilla Framework Expanded] Transpiler on PawnRenderer:DrawPawnBody failed.");
+            }
+        }
+        public static void ModifyApparelLoc(Pawn pawn, ref Vector3 vector, Material mat)
+        {
+            oldVector = vector;
+            if (mappedValues.TryGetValue(mat, out var apparelRecord))
+            {
+                var extension = apparelRecord.sourceApparel.def.GetModExtension<ApparelDrawPosExtension>();
+                if (extension != null)
+                {
+                    vector = extension.apparelDrawSettings.GetDrawPosOffset(pawn, vector);
+                }
+            }
+        }
+    
+        public static void ResetVector(ref Vector3 vector)
+        {
+            vector = oldVector;
         }
     }
 }
