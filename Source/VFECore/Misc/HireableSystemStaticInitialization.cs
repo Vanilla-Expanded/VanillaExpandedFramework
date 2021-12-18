990ab5e832a51f43ae8e3044c1054c4f54aea6d0
Simon Latusek <simon.latusek@gmail.com>
2021-12-18 20:35:41 +0100

    fix loading of hireables in save games

--- Source/VFECore/Misc/HireableSystemStaticInitialization.cs
+++ Source/VFECore/Misc/HireableSystemStaticInitialization.cs
@@ -1,12 +1,12 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using HarmonyLib;
-using RimWorld;
-using Verse;
-
-namespace VFECore.Misc
+﻿namespace VFECore.Misc
 {
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Linq;
+    using HarmonyLib;
+    using RimWorld;
+    using Verse;
+
     [StaticConstructorOnStartup]
     public static class HireableSystemStaticInitialization
     {
@@ -16,26 +16,42 @@ namespace VFECore.Misc
         {
             Hireables = DefDatabase<HireableFactionDef>.AllDefs.GroupBy(def => def.commTag).Select(group => new Hireable(group.Key, group.ToList())).ToList();
             if (Hireables.Any())
+            {
                 VFECore.harmonyInstance.Patch(AccessTools.Method(typeof(Building_CommsConsole), nameof(Building_CommsConsole.GetCommTargets)),
-                    postfix: new HarmonyMethod(typeof(HireableSystemStaticInitialization), nameof(GetCommTargets_Postfix)));
+                                              postfix: new HarmonyMethod(typeof(HireableSystemStaticInitialization), nameof(GetCommTargets_Postfix)));
+
+                VFECore.harmonyInstance.Patch(AccessTools.Method(typeof(LoadedObjectDirectory), "Clear"),
+                                              postfix: new HarmonyMethod(typeof(HireableSystemStaticInitialization), nameof(AddHireablesToLoadedObjectDirectory)));
+            }
         }
 
         public static IEnumerable<ICommunicable> GetCommTargets_Postfix(IEnumerable<ICommunicable> communicables) => communicables.Concat(Hireables);
+
+        public static void AddHireablesToLoadedObjectDirectory(LoadedObjectDirectory __instance)
+        {
+            foreach (Hireable hireable in Hireables)
+                __instance.RegisterLoaded(hireable);
+        }
     }
 
-    public class Hireable : IGrouping<string, HireableFactionDef>, ICommunicable
+    public class Hireable : IGrouping<string, HireableFactionDef>, ICommunicable, ILoadReferenceable
     {
+        private static readonly AccessTools.FieldRef<CrossRefHandler, LoadedObjectDirectory> loadedObjectInfo =
+            AccessTools.FieldRefAccess<CrossRefHandler, LoadedObjectDirectory>("loadedObjectDirectory");
+
         private readonly List<HireableFactionDef> factions;
 
         public Hireable(string label, List<HireableFactionDef> list)
         {
-            Key = label;
-            factions = list;
+            this.Key      = label;
+            this.factions = list;
+
+            loadedObjectInfo(Scribe.loader.crossRefs).RegisterLoaded(this);
         }
 
-        public string GetCallLabel() => "VEF.Hire".Translate(Key.CapitalizeFirst());
+        public string GetCallLabel() => "VEF.Hire".Translate(this.Key.CapitalizeFirst());
 
-        public string GetInfoText() => "VEF.HireDesc".Translate(Key.CapitalizeFirst());
+        public string GetInfoText() => "VEF.HireDesc".Translate(this.Key.CapitalizeFirst());
 
         public void TryOpenComms(Pawn negotiator)
         {
@@ -45,12 +61,13 @@ namespace VFECore.Misc
         public Faction GetFaction() => null;
 
         public FloatMenuOption CommFloatMenuOption(Building_CommsConsole console, Pawn negotiator) => FloatMenuUtility.DecoratePrioritizedTask(
-            new FloatMenuOption(GetCallLabel(), () => console.GiveUseCommsJob(negotiator, this), MenuOptionPriority.InitiateSocial), negotiator, console);
+         new FloatMenuOption(this.GetCallLabel(), () => console.GiveUseCommsJob(negotiator, this), MenuOptionPriority.InitiateSocial), negotiator, console);
 
-        public IEnumerator<HireableFactionDef> GetEnumerator() => factions.GetEnumerator();
+        public IEnumerator<HireableFactionDef> GetEnumerator() => this.factions.GetEnumerator();
 
-        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+        IEnumerator IEnumerable.GetEnumerator() => this.GetEnumerator();
 
-        public string Key { get; }
+        public string Key               { get; }
+        public string GetUniqueLoadID() => $"{nameof(Hireable)}_{this.Key}";
     }
 }
