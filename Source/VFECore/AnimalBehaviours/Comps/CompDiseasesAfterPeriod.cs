fab3696ae0d7130d5421f8472d32b41bf32e5584
juanosarg <31105173+juanosarg@users.noreply.github.com>
2022-01-12 15:09:26 +0100

    Added CompDiseasesAfterPeriod

---
+++ Source/VFECore/AnimalBehaviours/Comps/CompDiseasesAfterPeriod.cs
@@ -0,0 +1,47 @@
+﻿using RimWorld;
+using Verse;
+using Verse.Sound;
+
+namespace AnimalBehaviours
+
+{
+    public class CompDiseasesAfterPeriod : ThingComp
+    {
+        public int tickCounter = 0;
+
+        public CompProperties_DiseasesAfterPeriod Props
+        {
+            get
+            {
+                return (CompProperties_DiseasesAfterPeriod)this.props;
+            }
+        }
+
+        public override void CompTick()
+        {
+            base.CompTick();
+            tickCounter++;
+
+            if (tickCounter >= Props.timeToApplyInTicks)
+            {
+                Pawn pawn = this.parent as Pawn;
+
+                if (pawn != null && pawn.Map != null)
+                {
+                    HediffDef randomHediff = Props.hediffsToApply.RandomElement();
+                    Hediff hediff = pawn.health.hediffSet.GetFirstHediffOfDef(randomHediff);
+                    if (hediff == null)
+                    {
+                        pawn.health.AddHediff(hediff);
+                    }
+                    
+                    
+                }
+                tickCounter = (int)(Props.timeToApplyInTicks*0.8);
+            }
+        }
+
+        
+
+    }
+}
