d63124745893321e769d69e035b546bce2a8f567
KylianB <kylian.balde@gmail.com>
2022-02-24 19:46:43 +0100

    Add ThoughtExtension

---
+++ Source/VFECore/VFECore/DefModExtensions/ThoughtExtension.cs
@@ -0,0 +1,11 @@
+﻿using RimWorld;
+using System.Collections.Generic;
+using Verse;
+
+namespace VFECore
+{
+    public class ThoughtExtensions : DefModExtension
+    {
+        public List<ThoughtDef> removeThoughtsWhenAdded;
+    }
+}
