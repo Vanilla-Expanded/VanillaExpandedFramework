26e1bc51880dbfcb2a9a6987510a2cc4d2673aed
Taranchuk <nfhfyxer@gmail.com>
2022-03-03 21:55:37 +0200

    WeatherOverlay_Effects improvements

--- Source/VFECore/VFECore/MultiplayerSupport.cs
+++ Source/VFECore/VFECore/MultiplayerSupport.cs
@@ -1,5 +1,6 @@
 ﻿using HarmonyLib;
 using Multiplayer.API;
+using RimWorld;
 using Verse;
 
 namespace VFECore
@@ -16,6 +17,10 @@ namespace VFECore
             MP.RegisterSyncMethod(typeof(CompAutumnLeavesSpawner), "TryFindSpawnCell", null);
             var tryFindSpawnCell = AccessTools.Method(typeof(CompAutumnLeavesSpawner), "TryFindSpawnCell", null, null);
             harmony.Patch(tryFindSpawnCell, new HarmonyMethod(typeof(MultiplayerSupport), "FixRNGPre", null), new HarmonyMethod(typeof(MultiplayerSupport), "FixRNGPos", null), null, null);
+
+            MP.RegisterSyncMethod(typeof(WeatherOverlay_Effects), "DoDamage", null);
+            var doDamage = AccessTools.Method(typeof(WeatherOverlay_Effects), "DoDamage", null, null);
+            harmony.Patch(doDamage, new HarmonyMethod(typeof(MultiplayerSupport), "FixRNGPre", null), new HarmonyMethod(typeof(MultiplayerSupport), "FixRNGPos", null), null, null);
         }
 
         private static void FixRNGPre()
