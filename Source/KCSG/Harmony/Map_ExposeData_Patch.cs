9d4583b62240d8aed95bda4f0a92f37d331a5fa1
KylianB <kylian.balde@gmail.com>
2022-04-03 16:51:38 +0200

    KCSG: faster Map_ExposeData_Patch

--- Source/KCSG/Harmony/Map_ExposeData_Patch.cs
+++ Source/KCSG/Harmony/Map_ExposeData_Patch.cs
@@ -12,11 +12,21 @@ namespace KCSG
         {
             if (Scribe.mode == LoadSaveMode.Saving)
             {
-                List<Thing> kcsgSkyfallers = __instance.listerThings.AllThings.FindAll(t => t is KCSG_Skyfaller);
+                var kcsgSkyfallers = new List<Thing>();
+                var allThings = __instance.listerThings.AllThings;
+                for (int i = 0; i < allThings.Count; i++)
+                {
+                    var thing = allThings[i];
+                    if (thing is KCSG_Skyfaller)
+                        kcsgSkyfallers.Add(thing);
+                }
+
+
                 if (kcsgSkyfallers.Count > 0)
                 {
-                    foreach (Thing faller in kcsgSkyfallers)
+                    for (int i = 0; i < kcsgSkyfallers.Count; i++)
                     {
+                        Thing faller = kcsgSkyfallers[i];
                         if (faller is KCSG_Skyfaller skyfaller && skyfaller != null)
                         {
                             skyfaller.SaveImpact();
