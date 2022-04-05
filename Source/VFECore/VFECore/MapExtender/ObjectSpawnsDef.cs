3f00936206333c922040f662aea8fcb94b94d75f
Taranchuk <nfhfyxer@gmail.com>
2022-04-05 14:48:12 +0300

    added logging to ConfigErrors so it will be shown on startup

--- Source/VFECore/VFECore/MapExtender/ObjectSpawnsDef.cs
+++ Source/VFECore/VFECore/MapExtender/ObjectSpawnsDef.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using RimWorld;
 using UnityEngine;
 using Verse;
@@ -20,5 +21,17 @@ namespace VFECore
         public bool findCellsOutsideColony = false;
         public bool spawnOnlyInPlayerMaps = false;
         public bool randomRotation;
+
+        public override IEnumerable<string> ConfigErrors()
+        {
+            foreach (var error in base.ConfigErrors())
+            {
+                yield return error;
+            }
+            if (thingDef is null)
+            {
+                yield return "[VEF] ObjectSpawnsDef " + this.defName + " contain null thing def. It will not work.";
+            }
+        }
     }
 }
