6a9371d0310552bb31f1a2c20cb48979abe1203a
KylianB <kylian.balde@gmail.com>
2022-04-07 12:23:15 +0200

    PipeSystem: Better CompResourceProcessor
    Allow multiple result and the payer to choose one of them

--- Source/PipeSystem/PipeSystem/Comps/CompProperties/CompProperties_ResourceProcessor.cs
+++ Source/PipeSystem/PipeSystem/Comps/CompProperties/CompProperties_ResourceProcessor.cs
@@ -1,4 +1,5 @@
-﻿using Verse;
+﻿using System.Collections.Generic;
+using Verse;
 
 namespace PipeSystem
 {
@@ -8,21 +9,41 @@ namespace PipeSystem
         {
             compClass = typeof(CompResourceProcessor);
         }
-
+        // Show inspect string?
         public bool showBufferInfo = true;
-        public float bufferSize;
-
+        // Translation key:
         public string notWorkingKey;
+        // All possible results of the process
+        public List<Result> results;
 
-        public int eachTicks;
-        public Result result;
+        /// <summary>
+        /// Config errors handling. Empty results? Null translation key?
+        /// </summary>
+        public override IEnumerable<string> ConfigErrors(ThingDef parentDef)
+        {
+            foreach (var error in base.ConfigErrors(parentDef))
+                yield return error;
+
+            if (results.NullOrEmpty())
+                yield return $"CompProperties_ResourceProcessor of {parentDef.defName} cannot have empty or null <results>";
+            if (notWorkingKey == null)
+                yield return $"CompProperties_ResourceProcessor of {parentDef.defName} cannot have null <notWorkingKey>";
+        }
     }
 
+    /// <summary>
+    /// Result class, contain every information needed to produce stuff
+    /// </summary>
     public class Result
     {
+        // Produce each ticks: Default to 600 ticks (10 sec)
+        public int eachTicks = 600;
+        // Amount needed to produce result
+        public int countNeeded;
+        // Result as a thing
         public ThingDef thing;
         public int thingCount;
-
+        // Result as a piped resource
         public PipeNetDef net;
         public int netCount;
     }
--- Source/PipeSystem/PipeSystem/Comps/CompResourceProcessor.cs
+++ Source/PipeSystem/PipeSystem/Comps/CompResourceProcessor.cs
@@ -7,77 +7,155 @@ using Verse;
 
 namespace PipeSystem
 {
+    /// <summary>
+    /// Allow the player to choose from a list of result. Produce the choosen result.
+    /// Either spawn thing, or fill another net.
+    /// </summary>
+    [StaticConstructorOnStartup]
     public class CompResourceProcessor : CompResource
     {
         public new CompProperties_ResourceProcessor Props => (CompProperties_ResourceProcessor)props;
 
         public float Storage { get => storage; }
 
-        private float storage;
-
+        private static readonly Texture2D emptyIcon = ContentFinder<Texture2D>.Get("UI/EmptyImage");
+        // Other comps we run check on
         private CompResource otherComp;
         private CompFlickable flickable;
         private CompPowerTrader compPower;
-
+        // Stuff saved:
+        // - Amount stored
+        private float storage;
+        // - Next produce tick
         private int nextProcessTick;
+        // - Enough resource to process
+        private bool enoughResource;
+        // - Storages are full?
+        private bool cantProcess;
+        // - Choosed result
+        private int resultIndex;
 
+        // Initialized post spawn:
         private List<IntVec3> adjCells;
+        private Vector3 trueCenter;
         private bool canPushToNet;
         private bool canCreateItems;
-        private Vector3 trueCenter;
-        private bool cantRefine;
-        private bool enoughResource;
+        private Command_Action chooseOuputGizmo;
 
+        /// <summary>
+        /// Should work? We check flickable comp, power comp, make sure we can process and there is enough resources
+        /// </summary>
         public bool Working
         {
             get
             {
-                return (flickable == null || flickable.SwitchIsOn) && (compPower == null || compPower.PowerOn) && enoughResource && !cantRefine;
+                return (flickable == null || flickable.SwitchIsOn) && (compPower == null || compPower.PowerOn) && enoughResource && !cantProcess;
             }
         }
 
+        /// <summary>
+        /// The current choosed processing result
+        /// </summary>
+        public Result ChoosedResult { get => Props.results[resultIndex]; }
+
+        /// <summary>
+        /// Set-up vars
+        /// </summary>
         public override void PostPostMake()
         {
             base.PostPostMake();
-            nextProcessTick = Find.TickManager.TicksGame + Props.eachTicks;
+            nextProcessTick = Find.TickManager.TicksGame + ChoosedResult.eachTicks;
             storage = 0;
-            cantRefine = false;
+            resultIndex = 0;
+            cantProcess = false;
             enoughResource = false;
         }
 
+        /// <summary>
+        /// Set up everything that ins't saved
+        /// </summary>
         public override void PostSpawnSetup(bool respawningAfterLoad)
         {
             base.PostSpawnSetup(respawningAfterLoad);
-            if (Props.result.net != null)
+            // Get comps
+            flickable = parent.GetComp<CompFlickable>();
+            compPower = parent.GetComp<CompPowerTrader>();
+            // Get adjacent cells for spawning
+            adjCells = GenAdj.CellsAdjacent8Way(parent).ToList();
+            // Get center for pusling icon
+            trueCenter = parent.TrueCenter();
+
+            if (Props.results.Count > 1)
+            {
+                chooseOuputGizmo = new Command_Action()
+                {
+                    action = delegate
+                    {
+                        var floatMenuOptions = new List<FloatMenuOption>();
+                        for (int rIndex = 0; rIndex < Props.results.Count; rIndex++)
+                        {
+                            var res = Props.results[rIndex];
+                            var label = res.net != null ? res.net.resource.name : res.thing.label;
+                            var count = res.net != null ? res.netCount : res.thingCount;
+                            floatMenuOptions.Add(new FloatMenuOption(
+                                "PipeSystem_Produce".Translate(count, label),
+                                () =>
+                                {
+                                    resultIndex = Props.results.IndexOf(res);
+                                    SetupForChoice();
+                                    nextProcessTick = Find.TickManager.TicksGame + ChoosedResult.eachTicks;
+                                }));
+                        }
+                        Find.WindowStack.Add(new FloatMenu(floatMenuOptions));
+                    },
+                    defaultLabel = "PipeSystem_ChooseResult".Translate(),
+                    defaultDesc = "PipeSystem_ChooseResultDesc".Translate(),
+                    icon = ChoosedResult.thing != null ? ChoosedResult.thing.uiIcon : emptyIcon,
+                };
+            }
+
+            SetupForChoice();
+        }
+
+        /// <summary>
+        /// Get the comp that match result, change some variables and change gizmo icon
+        /// </summary>
+        private void SetupForChoice()
+        {
+            // Get the needed compResource
+            if (ChoosedResult.net != null)
             {
                 var comps = parent.GetComps<CompResource>();
                 for (int i = 0; i < comps.Count(); i++)
                 {
                     var comp = comps.ElementAt(i);
-                    if (comp.Props.pipeNet == Props.result.net)
+                    if (comp.Props.pipeNet == ChoosedResult.net)
                     {
                         otherComp = comp;
                         break;
                     }
                 }
             }
-
-            canPushToNet = Props.result.net != null && otherComp != null;
-            canCreateItems = Props.result.thing != null;
-
-            flickable = parent.GetComp<CompFlickable>();
-            compPower = parent.GetComp<CompPowerTrader>();
-
-            adjCells = GenAdj.CellsAdjacent8Way(parent).ToList();
-            trueCenter = parent.TrueCenter();
+            // Initiate variable then used in SpawnOrPushToNet
+            canPushToNet = ChoosedResult.net != null && otherComp != null;
+            canCreateItems = ChoosedResult.thing != null;
+            // Change gizmo icon
+            if (chooseOuputGizmo != null)
+            {
+                chooseOuputGizmo.icon = ChoosedResult.thing != null ? ChoosedResult.thing.uiIcon : emptyIcon;
+            }
         }
 
+        /// <summary>
+        /// Each tick, update/maintain sound if needed.
+        /// Each process tick, try to finish process if possible. 
+        /// </summary>
         public override void CompTick()
         {
             int tick = Find.TickManager.TicksGame;
             if (tick >= nextProcessTick)
             {
-                if (storage == Props.bufferSize
+                if (storage >= ChoosedResult.countNeeded
                     && (flickable == null || flickable.SwitchIsOn)
                     && (compPower == null || compPower.PowerOn))
                 {
@@ -88,38 +166,55 @@ namespace PipeSystem
                 {
                     enoughResource = false;
                 }
-                nextProcessTick = tick + Props.eachTicks;
+                nextProcessTick = tick + ChoosedResult.eachTicks;
             }
             // Sound
             UpdateSustainer(Working);
         }
 
+        /// <summary>
+        /// Save storage, nextProcessTick, cantProcess, enoughResource and resultIndex
+        /// </summary>
         public override void PostExposeData()
         {
             base.PostExposeData();
             Scribe_Values.Look(ref storage, "storage");
             Scribe_Values.Look(ref nextProcessTick, "nextProcessTick");
-            Scribe_Values.Look(ref cantRefine, "cantRefine", false, true);
-            Scribe_Values.Look(ref enoughResource, "enoughResource", false, true);
+            Scribe_Values.Look(ref resultIndex, "resultIndex");
+            Scribe_Values.Look(ref cantProcess, "cantProcess", false);
+            Scribe_Values.Look(ref enoughResource, "enoughResource", false);
         }
 
+        /// <summary>
+        /// Print more info regarding current process
+        /// </summary>
         public override string CompInspectStringExtra()
         {
             StringBuilder sb = new StringBuilder();
             sb.AppendInNewLine(base.CompInspectStringExtra());
-
+            // Show storage percentage
             if (Props.showBufferInfo)
-                sb.AppendInNewLine("PipeSystem_ProcessorBuffer".Translate((storage / Props.bufferSize).ToStringPercent()));
-
-            if (cantRefine && Props.notWorkingKey != null)
+            {
+                float percent = storage / ChoosedResult.countNeeded;
+                percent = percent > 1f ? 1f : percent;
+                sb.AppendInNewLine("PipeSystem_ProcessorBuffer".Translate(percent.ToStringPercent()));
+            }
+            // If can't process anymore, show given key
+            if (cantProcess && Props.notWorkingKey != null)
                 sb.AppendInNewLine(Props.notWorkingKey.Translate());
-
-            if (Working)
-                sb.AppendInNewLine("PipeSystem_Producing".Translate(Props.result.thingCount, Props.result.thing.LabelCap, (1f - ((nextProcessTick - Find.TickManager.TicksGame) / (float)Props.eachTicks)).ToStringPercent()));
+            // If working show the thing that will be produced, the amount, and the progress
+            if (Working && storage >= ChoosedResult.countNeeded)
+                sb.AppendInNewLine("PipeSystem_Producing".Translate(
+                    ChoosedResult.thingCount,
+                    ChoosedResult.thing.LabelCap,
+                    (1f - ((nextProcessTick - Find.TickManager.TicksGame) / (float)ChoosedResult.eachTicks)).ToStringPercent()));
 
             return sb.ToString().Trim();
         }
 
+        /// <summary>
+        /// If we don't have enough resource, show the pusling off icon
+        /// </summary>
         public override void PostDraw()
         {
             base.PostDraw();
@@ -128,32 +223,49 @@ namespace PipeSystem
         }
 
         /// <summary>
+        /// Add the choose output gizmo
+        /// </summary>
+        public override IEnumerable<Gizmo> CompGetGizmosExtra()
+        {
+            var gizmos = base.CompGetGizmosExtra();
+            for (int i = 0; i < gizmos.Count(); i++)
+                yield return gizmos.ElementAt(i);
+
+            if (chooseOuputGizmo != null)
+                yield return chooseOuputGizmo;
+        }
+
+        /// <summary>
         /// Push resource to processor. Return amount used.
         /// </summary>
         public float PushTo(float amount)
         {
             var used = 0f;
-            var sub = Props.bufferSize - storage;
+            var sub = ChoosedResult.countNeeded - storage;
             if (sub > 0f)
             {
                 var toStore = sub > amount ? amount : sub;
                 storage += toStore;
                 used += toStore;
+                // We reached count needed, start process
+                if (storage == ChoosedResult.countNeeded)
+                    nextProcessTick = Find.TickManager.TicksGame + ChoosedResult.eachTicks;
             }
 
             return used;
         }
 
+        /// <summary>
+        /// Create and spawn thing, or increase existing thing stacksize, or push to net
+        /// </summary>
         private void SpawnOrPushToNet()
         {
             // If it can directly go into the net
-            if (canPushToNet && otherComp.PipeNet is PipeNet net
-                && (net.AvailableCapacity is float a && a > 0
-                    || net.converters.Count > 0))
+            if (canPushToNet && otherComp.PipeNet is PipeNet net && net.connectors.Count > 1)
             {
-                var count = Props.result.netCount;
+                var count = ChoosedResult.netCount;
                 // Available storage?
-                if (a > count)
+                if (net.AvailableCapacity > count)
                 {
                     // Store it
                     net.DistributeAmongStorage(count);
@@ -165,12 +277,13 @@ namespace PipeSystem
                     // Convert it, if some left keep it inside here
                     storage -= net.DistributeAmongConverter(count);
                 }
+                // No storage/converter, try refuel connected things
                 else if (net.refuelables.Count > 0)
                 {
                     storage -= net.DistributeAmongRefuelables(count);
                 }
                 // We shouldn't have anymore resource, if we do -> storage full or converter full
-                cantRefine = storage > 0;
+                cantProcess = storage > 0;
             }
             // If can't go into net
             else if (canCreateItems)
@@ -183,19 +296,19 @@ namespace PipeSystem
                     if (cell.Walkable(map))
                     {
                         // Try find thing of the same def
-                        var thing = cell.GetFirstThing(map, Props.result.thing);
+                        var thing = cell.GetFirstThing(map, ChoosedResult.thing);
                         if (thing != null)
                         {
-                            if ((thing.stackCount + Props.result.thingCount) > thing.def.stackLimit)
+                            if ((thing.stackCount + ChoosedResult.thingCount) > thing.def.stackLimit)
                                 continue;
                             // We found some, modifying stack size
-                            thing.stackCount += Props.result.thingCount;
+                            thing.stackCount += ChoosedResult.thingCount;
                         }
                         else
                         {
                             // We didn't find any, creating thing
-                            thing = ThingMaker.MakeThing(Props.result.thing);
-                            thing.stackCount = Props.result.thingCount;
+                            thing = ThingMaker.MakeThing(ChoosedResult.thing);
+                            thing.stackCount = ChoosedResult.thingCount;
                             if (!GenPlace.TryPlaceThing(thing, cell, map, ThingPlaceMode.Near))
                                 continue;
                         }
@@ -204,7 +317,7 @@ namespace PipeSystem
                 }
                 // Reset buffer
                 storage = 0;
-                cantRefine = false;
+                cantProcess = false;
             }
         }
     }
