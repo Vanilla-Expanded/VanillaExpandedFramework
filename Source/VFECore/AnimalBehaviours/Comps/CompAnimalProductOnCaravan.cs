fd3ed4591a069de1b0386ce9073df8dfded88a13
juanosarg <31105173+juanosarg@users.noreply.github.com>
2022-04-08 13:19:54 +0200

    Added CompGiveThoughtsOnCaravan

--- Source/VFECore/AnimalBehaviours/Comps/CompAnimalProductOnCaravan.cs
+++ Source/VFECore/AnimalBehaviours/Comps/CompAnimalProductOnCaravan.cs
@@ -41,6 +41,8 @@ namespace AnimalBehaviours
 
 						}
 
+						
+
 					}
 				}
 				
---
+++ Source/VFECore/AnimalBehaviours/Comps/CompGiveThoughtsOnCaravan.cs
@@ -0,0 +1,84 @@
+﻿
+using System.Collections.Generic;
+using RimWorld;
+using Verse;
+using RimWorld.Planet;
+using UnityEngine;
+
+namespace AnimalBehaviours
+{
+
+    public class CompGiveThoughtsOnCaravan : ThingComp
+    {
+
+        public CompProperties_GiveThoughtsOnCaravan Props => (CompProperties_GiveThoughtsOnCaravan)props;
+
+
+
+
+
+        public override void CompTick()
+        {
+            if (parent.IsHashIntervalTick(Props.intervalTicks))
+            {
+
+
+
+                Pawn pawn = parent as Pawn;
+
+                if (CaravanUtility.IsCaravanMember(pawn))
+                {
+
+                    Caravan caravan = CaravanUtility.GetCaravan(pawn);
+                    foreach (Pawn pawnMember in caravan.PawnsListForReading)
+                    {
+
+                        if (!Props.causeNegativeAtRandom) {
+
+                            pawnMember.needs?.mood?.thoughts?.memories?.TryGainMemory(Props.thought, pawn);
+
+                        }
+                        else
+                        {
+                            if (Rand.Chance(Props.randomNegativeChance))
+                            {
+                                if (pawnMember.needs?.mood?.thoughts?.memories?.GetFirstMemoryOfDef(Props.thought) == null)
+                                {
+                                    pawnMember.needs?.mood?.thoughts?.memories?.TryGainMemory(Props.negativeThought, pawn);
+                                }
+                            }
+                            else {
+                                if (pawnMember.needs?.mood?.thoughts?.memories?.GetFirstMemoryOfDef(Props.negativeThought)==null)
+                                {
+                                    pawnMember.needs?.mood?.thoughts?.memories?.TryGainMemory(Props.thought, pawn);
+                                }
+
+                            }
+
+
+
+                        }
+                        
+                        
+                        
+                        
+
+                    }
+
+
+
+                }
+
+
+            }
+        }
+
+
+
+
+
+
+
+
+    }
+}
