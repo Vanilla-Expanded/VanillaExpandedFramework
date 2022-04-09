35012817cadf7ef366f93e1908a7d16db1bb67fd
KylianB <kylian.balde@gmail.com>
2022-04-09 16:14:48 +0200

    PipeSystem: Building_PipeValve

---
+++ Source/PipeSystem/PipeSystem/Buildings/Building_PipeValve.cs
@@ -0,0 +1,18 @@
+﻿using RimWorld;
+using Verse;
+
+namespace PipeSystem
+{
+    [StaticConstructorOnStartup]
+    public class Building_PipeValve : Building
+    {
+        private CompFlickable flickableComp;
+        public override Graphic Graphic => flickableComp.CurrentGraphic;
+
+        public override void SpawnSetup(Map map, bool respawningAfterLoad)
+        {
+            base.SpawnSetup(map, respawningAfterLoad);
+            flickableComp = GetComp<CompFlickable>();
+        }
+    }
+}
