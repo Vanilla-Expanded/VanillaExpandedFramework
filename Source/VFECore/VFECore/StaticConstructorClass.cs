a6dde4ff1e7cd3a3ca844980c71932192a86ba2e
Joshua <legodude17@users.noreply.github.com>
2022-04-12 16:49:49 -0700

    Fix cache issue in ScenPart_ForcedFactionGoodwill

--- Source/VFECore/VFECore/StaticConstructorClass.cs
+++ Source/VFECore/VFECore/StaticConstructorClass.cs
@@ -1,25 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using UnityEngine;
+﻿using UnityEngine;
 using Verse;
-using RimWorld;
 
 namespace VFECore
 {
-
     [StaticConstructorOnStartup]
     public static class StaticConstructorClass
     {
+        public static Mesh plane20Flip = MeshMakerPlanes.NewPlaneMesh(2f, true);
 
         static StaticConstructorClass()
         {
             // Cache setters
             PawnShieldGenerator.Reset();
-           // ScenPartUtility.SetCache();
+            ScenPartUtility.SetCache();
         }
-        public static Mesh plane20Flip = MeshMakerPlanes.NewPlaneMesh(2f, flipped: true);
     }
-
 }
