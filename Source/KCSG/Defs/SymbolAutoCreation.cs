c6c5370deebf6658400972cb7b006c1299fdc930
KylianB <kylian.balde@gmail.com>
2022-04-19 18:32:58 +0200

    KCSG: Make SymbolAutoCreation def obsolete

--- Source/KCSG/Defs/SymbolAutoCreation.cs
+++ Source/KCSG/Defs/SymbolAutoCreation.cs
@@ -1,9 +1,20 @@
-﻿using Verse;
+﻿using System;
+using System.Collections.Generic;
+using Verse;
 
 namespace KCSG
 {
+    [Obsolete]
     class SymbolAutoCreation : Def
     {
         public bool autoSymbolsCreation = false;
+
+        public override IEnumerable<string> ConfigErrors()
+        {
+            foreach (var msg in base.ConfigErrors())
+                yield return msg;
+
+            yield return $"SymbolAutoCreation def will soon be deprecated. {modContentPack.Name} need to swith to regular SymbolDefs.";
+        }
     }
 }
