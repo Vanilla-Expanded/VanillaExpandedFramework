a39614e5ddc93386d091460b6f8c6d4025b860ca
Joshua <legodude17@users.noreply.github.com>
2022-04-20 19:14:37 -0700

    Abilities: Improve Ability_Explode

--- Source/VFECore/Abilities/Abilities/Ability_Explode.cs
+++ Source/VFECore/Abilities/Abilities/Ability_Explode.cs
@@ -10,16 +10,30 @@ namespace VFECore.Abilities
             base.Cast(target);
             if (def.GetModExtension<AbilityExtension_Explosion>() is AbilityExtension_Explosion ext)
                 GenExplosion.DoExplosion(ext.onCaster ? pawn.Position : target.Cell, pawn.Map, ext.explosionRadius, ext.explosionDamageDef, pawn,
-                    ext.explosionDamageAmount,
-                    ignoredThings: new List<Thing> {pawn});
+                    ext.explosionDamageAmount, ext.explosionArmorPenetration, ext.explosionSound, null, null, null, ext.postExplosionSpawnThingDef,
+                    ext.postExplosionSpawnChance, ext.postExplosionSpawnThingCount, ext.applyDamageToExplosionCellsNeighbors, ext.preExplosionSpawnThingDef,
+                    ext.preExplosionSpawnChance, ext.preExplosionSpawnThingCount, ext.chanceToStartFire, ext.damageFalloff, ext.explosionDirection,
+                    new List<Thing> {pawn});
         }
     }
 
     public class AbilityExtension_Explosion : DefModExtension
     {
-        public int       explosionDamageAmount;
+        public bool      applyDamageToExplosionCellsNeighbors;
+        public float     chanceToStartFire;
+        public bool      damageFalloff;
+        public float     explosionArmorPenetration = -1f;
+        public int       explosionDamageAmount = -1;
         public DamageDef explosionDamageDef;
+        public float?    explosionDirection;
         public float     explosionRadius;
+        public SoundDef  explosionSound;
         public bool      onCaster;
+        public float     postExplosionSpawnChance;
+        public int       postExplosionSpawnThingCount = 1;
+        public ThingDef  postExplosionSpawnThingDef;
+        public float     preExplosionSpawnChance;
+        public int       preExplosionSpawnThingCount = 1;
+        public ThingDef  preExplosionSpawnThingDef;
     }
 }
