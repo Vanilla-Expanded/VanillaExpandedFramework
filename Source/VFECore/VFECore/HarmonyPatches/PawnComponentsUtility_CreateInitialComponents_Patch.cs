14e2517e36ef12591f795a74112cc49951e288d6
Taranchuk <nfhfyxer@gmail.com>
2022-04-22 16:08:10 +0300

    fixes a bug with non-humanlike pawns generating with skills

---
+++ Source/VFECore/VFECore/HarmonyPatches/PawnComponentsUtility_CreateInitialComponents_Patch.cs
@@ -0,0 +1,20 @@
+﻿using Verse;
+using RimWorld;
+using HarmonyLib;
+
+namespace VFECore
+{
+    [HarmonyPatch(typeof(PawnComponentsUtility), "CreateInitialComponents")]
+    public static class PawnComponentsUtility_CreateInitialComponents_Patch
+    {
+        [HarmonyPostfix]
+        public static void Postfix(Pawn pawn)
+        {
+            if (pawn.skills is null && pawn.kindDef.skills != null)
+            {
+                pawn.skills = new Pawn_SkillTracker(pawn);
+            }
+        }
+    }
+
+}
