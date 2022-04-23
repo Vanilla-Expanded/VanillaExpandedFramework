a4922fb4d4d1bcd7a46800a20fbead83078386b0
Joshua <legodude17@users.noreply.github.com>
2022-04-22 18:58:55 -0700

    VFECore: Move Hire to own folder, add Shields

---
+++ Source/VFECore/Abilities/Abilities/Ability_HediffDuration.cs
@@ -0,0 +1,29 @@
+﻿using Verse;
+
+namespace VFECore.Abilities
+{
+    public class Ability_HediffDuration : Ability
+    {
+        public override void ApplyHediffs(LocalTargetInfo targetInfo)
+        {
+            var hediffExtension = def.GetModExtension<AbilityExtension_Hediff>();
+            if (targetInfo.Pawn == null || !(hediffExtension?.applyAuto ?? false)) return;
+            var localHediff = HediffMaker.MakeHediff(hediffExtension.hediff, targetInfo.Pawn);
+            if (hediffExtension.severity > float.Epsilon)
+                localHediff.Severity = hediffExtension.severity;
+            if (localHediff is HediffWithComps hwc)
+                foreach (var hediffComp in hwc.comps)
+                    switch (hediffComp)
+                    {
+                        case HediffComp_Ability hca:
+                            hca.ability = this;
+                            break;
+                        case HediffComp_Disappears hcd:
+                            hcd.ticksToDisappear = GetDurationForPawn();
+                            break;
+                    }
+
+            targetInfo.Pawn.health.AddHediff(localHediff);
+        }
+    }
+}
---
+++ Source/VFECore/Misc/Shields/ShieldsSystem.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using HarmonyLib;
+using UnityEngine;
+using Verse;
+
+namespace VFECore.Shields
+{
+    public static class ShieldsSystem
+    {
+        public static Dictionary<Pawn, List<HediffComp_Draw>> HediffDrawsByPawn = new Dictionary<Pawn, List<HediffComp_Draw>>();
+
+        private static bool          drawPatchesApplied;
+        private static bool          shieldPatchesApplied;
+        private static HarmonyMethod MyMethod(this string name) => new HarmonyMethod(typeof(ShieldsSystem), name);
+
+        public static void ApplyDrawPatches()
+        {
+            if (drawPatchesApplied) return;
+            drawPatchesApplied = true;
+            VFECore.harmonyInstance.Patch(AccessTools.Method(typeof(Pawn), nameof(Pawn.SpawnSetup)), postfix: nameof(OnPawnSpawn).MyMethod());
+            VFECore.harmonyInstance.Patch(AccessTools.Method(typeof(Pawn), nameof(Pawn.DeSpawn)),    postfix: nameof(OnPawnDespawn).MyMethod());
+            VFECore.harmonyInstance.Patch(AccessTools.Method(typeof(Pawn), nameof(Pawn.DrawAt)),     postfix: nameof(PawnPostDrawAt).MyMethod());
+        }
+
+        public static void ApplyShieldPatches()
+        {
+            if (shieldPatchesApplied) return;
+            shieldPatchesApplied = true;
+            VFECore.harmonyInstance.Patch(AccessTools.Method(typeof(ThingWithComps), nameof(ThingWithComps.PreApplyDamage)),
+                postfix: nameof(PostPreApplyDamage).MyMethod());
+        }
+
+        public static void OnPawnSpawn(Pawn __instance)
+        {
+            HediffDrawsByPawn.Add(__instance,
+                __instance.health.hediffSet.hediffs.OfType<HediffWithComps>().SelectMany(hediff => hediff.comps).OfType<HediffComp_Draw>().ToList());
+        }
+
+        public static void OnPawnDespawn(Pawn __instance)
+        {
+            HediffDrawsByPawn.Remove(__instance);
+        }
+
+        public static void PawnPostDrawAt(Pawn __instance, Vector3 drawLoc)
+        {
+            if (HediffDrawsByPawn.TryGetValue(__instance, out var list))
+                for (var i = 0; i < list.Count; i++)
+                    list[i].DrawAt(drawLoc);
+        }
+
+        public static void PostPreApplyDamage(ThingWithComps __instance, ref DamageInfo dinfo, ref bool absorbed)
+        {
+            if (absorbed || !(__instance is Pawn pawn)) return;
+            foreach (var shield in pawn.health.hediffSet.hediffs.OfType<HediffWithComps>().SelectMany(hediff => hediff.comps).OfType<HediffComp_Shield>())
+            {
+                shield.PreApplyDamage(ref dinfo, ref absorbed);
+                if (absorbed) break;
+            }
+        }
+    }
+}
