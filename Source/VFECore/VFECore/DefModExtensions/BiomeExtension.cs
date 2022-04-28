ac4ff4feb0154dacadec1913d853588209d22310
Taranchuk <nfhfyxer@gmail.com>
2022-04-28 15:40:42 +0300

    Added a biome extension with terrain swap feature from Anima Forest mod

---
+++ Source/VFECore/VFECore/DefModExtensions/BiomeExtension.cs
@@ -0,0 +1,16 @@
+﻿using RimWorld;
+using System.Collections.Generic;
+using Verse;
+
+namespace VFECore
+{
+    public class SwapTerrainDef
+    {
+        public TerrainDef from;
+        public TerrainDef to;
+    }
+    public class BiomeExtension : DefModExtension
+    {
+        public List<SwapTerrainDef> terrainsToSwap;
+    }
+}
