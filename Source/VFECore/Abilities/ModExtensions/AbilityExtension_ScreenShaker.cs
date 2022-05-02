71f268a4fe8f3f886a0819dc9b04ce31982dc22f
Taranchuk <nfhfyxer@gmail.com>
2022-05-02 21:48:59 +0300

    aadded screen shaker extension

---
+++ Source/VFECore/Abilities/ModExtensions/AbilityExtension_ScreenShaker.cs
@@ -0,0 +1,14 @@
+﻿namespace VFECore.Abilities
+{
+    using RimWorld;
+    using Verse;
+
+    public class AbilityExtension_ScreenShaker : AbilityExtension_AbilityMod
+	{
+		public float intensity;
+		public override void Cast(LocalTargetInfo target, Ability ability)
+		{
+			Find.CameraDriver.shaker.DoShake(intensity);
+		}
+	}
+}
