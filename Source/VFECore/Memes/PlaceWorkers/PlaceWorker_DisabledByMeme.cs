82386ada41e82fb9ac15a7449aa5d905c134f621
juanosarg <31105173+juanosarg@users.noreply.github.com>
2022-05-14 13:15:45 +0200

    Added PlaceWorker_DisabledByMeme

---
+++ Source/VFECore/Memes/PlaceWorkers/PlaceWorker_DisabledByMeme.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using RimWorld;
+using Verse;
+namespace VanillaMemesExpanded
+{
+    public class PlaceWorker_DisabledByMeme : PlaceWorker
+    {
+
+        public override bool IsBuildDesignatorVisible(BuildableDef def)
+        {
+            List<MemeDef> list = Current.Game?.World?.factionManager?.OfPlayer?.ideos?.PrimaryIdeo?.memes;
+            if (list != null)
+            {
+                foreach (MemeDef meme in list)
+                {
+                    ExtendedMemeProperties extendedMemeProps = meme.GetModExtension<ExtendedMemeProperties>();
+                    if (extendedMemeProps != null)
+                    {
+                        if (extendedMemeProps.removedDesignators != null)
+                        {
+                            foreach (ThingDef thing in extendedMemeProps.removedDesignators)
+                            {
+                                if (thing == def)
+                                {
+                                    return false;
+                                }
+                            }
+
+                         }
+                    }
+
+
+                }
+
+            }
+            return true;
+        }
+
+    }
+}
