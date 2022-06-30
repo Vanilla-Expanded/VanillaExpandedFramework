893fd19d4953d63a832af5597503a696504ef945
KylianB <kylian.balde@gmail.com>
2022-06-30 12:03:21 +0200

    KCSG: Ideology buildings

--- Source/KCSG/SymbolResolvers/SymbolResolver_ScatterPropsAround.cs
+++ Source/KCSG/SymbolResolvers/SymbolResolver_ScatterPropsAround.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using RimWorld;
 using RimWorld.BaseGen;
 using Verse;
@@ -15,15 +16,31 @@ namespace KCSG
         {
             Map map = BaseGen.globalSettings.map;
             var propsOpt = GenOption.PropsOptions;
+            var faction = rp.faction;
 
             if (propsOpt.scatterProps && propsOpt.scatterPropsDefs.Count > 0)
             {
                 usedSpots = new List<IntVec3>();
+                var usables = propsOpt.scatterPropsDefs.ListFullCopy();
 
+                if (ModsConfig.IdeologyActive && faction != null && faction.ideos != null && faction.ideos.PrimaryIdeo is Ideo p && p.IdeoApprovesOfSlavery())
+                {
+                    var tBuildings = DefDatabase<ThingDef>.AllDefs.Where(def => def.StatBaseDefined(StatDefOf.TerrorSource)
+                                                                                && (typeof(Building_Casket).IsAssignableFrom(def.thingClass)
+                                                                                    || p.cachedPossibleBuildings.Any(b => b.ThingDef == def)));
+                    usables.AddRange(tBuildings);
+                }
+
+                var thingUsed = new List<ThingDef>();
                 var propsStart = DateTime.Now;
                 for (int i = 0; i < propsOpt.scatterMaxAmount; i++)
                 {
-                    var prop = propsOpt.scatterPropsDefs.RandomElement();
+                    if (thingUsed.Count == usables.Count)
+                        thingUsed.Clear();
+
+                    var prop = usables.FindAll(t => !thingUsed.Contains(t)).RandomElement();
+                    thingUsed.Add(prop);
+
                     Rot4 rot = Rot4.North;
 
                     if (prop.rotatable && Rand.Bool)
@@ -52,8 +69,16 @@ namespace KCSG
                         return true;
                     }, map, out IntVec3 cell))
                     {
-                        Thing thing = ThingMaker.MakeThing(prop, GenStuff.DefaultStuffFor(prop));
+                        Thing thing = ThingMaker.MakeThing(prop, faction != null ? BaseGenUtility.CheapStuffFor(prop, faction) : GenStuff.DefaultStuffFor(prop));
                         thing.SetFactionDirect(map.ParentFaction);
+
+                        if (thing is Building_Casket casket)
+                        {
+                            var otherFac = Find.FactionManager.GetFactions(allowNonHumanlike: false).Where(f => !f.IsPlayer && f.HostileTo(rp.faction)).RandomElementWithFallback(rp.faction);
+                            var pawn = PawnGenerator.GeneratePawn(new PawnGenerationRequest(PawnKindDefOf.Slave, otherFac, allowFood: false));
+                            pawn.Kill(null);
+                            casket.TryAcceptThing(pawn.Corpse);
+                        }
 
                         if (prop.rotatable)
                             GenSpawn.Spawn(thing, cell, map, rot);
