d6eba26f7bc7913e5ff59ae8ec835f23616f27da
juanosarg <31105173+juanosarg@users.noreply.github.com>
2022-08-23 17:45:00 +0200

    Added CompHediffWhenFleeing

---
+++ Source/VFECore/AnimalBehaviours/Comps/CompProperties/CompProperties_HediffWhenFleeing.cs
@@ -0,0 +1,23 @@
+﻿
+using Verse;
+
+namespace AnimalBehaviours
+{
+    public class CompProperties_HediffWhenFleeing : CompProperties
+    {
+
+        public int tickInterval = 60;
+        public HediffDef hediffToCause;
+        public bool graphicAndSoundEffect = false;
+        public bool hediffOnRadius = false;
+        public float radius = 3;
+
+
+        public CompProperties_HediffWhenFleeing()
+        {
+            this.compClass = typeof(CompHediffWhenFleeing);
+        }
+
+
+    }
+}
