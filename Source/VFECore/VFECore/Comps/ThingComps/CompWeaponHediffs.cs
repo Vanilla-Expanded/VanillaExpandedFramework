6ef2254fe6a9d198d6f0b56c58fcd63b56eae3a8
Taranchuk <nfhfyxer@gmail.com>
2022-08-31 20:45:56 +0300

    fixes to CompWeaponHediffs

--- Source/VFECore/VFECore/Comps/ThingComps/CompWeaponHediffs.cs
+++ Source/VFECore/VFECore/Comps/ThingComps/CompWeaponHediffs.cs
@@ -16,38 +16,46 @@ namespace VFECore
         public Pawn wearer = null;
         public List<Hediff> wearerHediffs = new List<Hediff>();
         public CompProperties_WeaponHediffs Props => base.props as CompProperties_WeaponHediffs;
+
+        public override void PostSpawnSetup(bool respawningAfterLoad)
+        {
+            base.PostSpawnSetup(respawningAfterLoad);
+            AssignHediffs();
+        }
         public override void CompTick()
         {
             base.CompTick();
-            if (this.parent is ThingWithComps weapon && 
-                (weapon.ParentHolder is not Pawn_EquipmentTracker tracker || tracker.pawn != wearer))
+            AssignHediffs();
+        }
+
+        public void AssignHediffs()
+        {
+            if (this.parent is ThingWithComps weapon)
             {
                 var hediffs = this.Props.hediffs;
-                if (wearer != null)
+                if (weapon.ParentHolder is not Pawn_EquipmentTracker tracker || tracker.pawn != wearer)
                 {
-                    foreach (var hediff in wearerHediffs)
+                    if (wearer != null)
                     {
-                        wearer.health.hediffSet.hediffs.Remove(hediff);
+                        foreach (var hediff in wearerHediffs)
+                        {
+                            wearer.health.hediffSet.hediffs.Remove(hediff);
+                        }
+                        wearerHediffs.Clear();
+                        wearer = null;
                     }
                 }
-
-                if (weapon.ParentHolder is Pawn_EquipmentTracker tracker2 && tracker2.pawn != null)
+                if (weapon.ParentHolder is Pawn_EquipmentTracker tracker2 && tracker2.pawn != null 
+                    && (tracker2.pawn != wearer || (wearerHediffs?.Any(x => x?.pawn == tracker2.pawn) is false)))
                 {
-                    wearerHediffs.Clear();
-                    foreach (var hediffDef in hediffs) //it adds hediffs to the new owner
+                    wearerHediffs = new List<Hediff>();
+                    foreach (var hediffDef in hediffs)
                     {
                         var hediff = HediffMaker.MakeHediff(hediffDef, tracker2.pawn);
-                        if (hediff != null)
-                        {
-                            tracker2.pawn.health.AddHediff(hediff);
-                            wearerHediffs.Add(hediff);
-                        }
+                        tracker2.pawn.health.AddHediff(hediff);
+                        wearerHediffs.Add(hediff);
                     }
                     wearer = tracker2.pawn;
-                }
-                else
-                {
-                    wearer = null;
                 }
             }
         }
@@ -57,6 +65,10 @@ namespace VFECore
             base.PostExposeData();
             Scribe_References.Look(ref wearer, this.GetType() + "_wearer");
             Scribe_Collections.Look(ref wearerHediffs, this.GetType() + "_wearerHediffs", LookMode.Reference);
+            if (Scribe.mode == LoadSaveMode.PostLoadInit)
+            {
+                AssignHediffs();
+            }
         }
     }
 }
