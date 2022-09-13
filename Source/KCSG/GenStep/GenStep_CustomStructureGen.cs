677d08392b70c7813ebd12c863836f3d1044f6e0
KylianB <kylian.balde@gmail.com>
2022-09-13 15:11:59 +0200

    kcsg: remove obsolete fields from GenStep_CustomStructureGen

--- Source/KCSG/GenStep/GenStep_CustomStructureGen.cs
+++ Source/KCSG/GenStep/GenStep_CustomStructureGen.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using RimWorld;
 using RimWorld.BaseGen;
 using Verse;
@@ -20,21 +19,10 @@ namespace KCSG
         public List<ThingDef> filthTypes = new List<ThingDef>();
         public float scatterChance = 0.4f;
 
-        // TODO Obsolete - To remove in next rimworld version
-        [Obsolete]
-        public bool shouldRuin = false;
-
-        [Obsolete]
-        public List<string> ruinSymbolResolvers = new List<string>();
-
         public override int SeedPart => 916595355;
 
         public override void Generate(Map map, GenStepParams parms)
         {
-            // TODO Compat - To remove in next rimworld version
-            if (!ruinSymbolResolvers.NullOrEmpty())
-                symbolResolvers = ruinSymbolResolvers.ListFullCopy();
-
             GenOption.ext = new CustomGenOption
             {
                 symbolResolvers = symbolResolvers,
