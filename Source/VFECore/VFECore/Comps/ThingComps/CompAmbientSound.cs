0bbd8035447475bd85d7bf209da4f4f52d313dde
Taranchuk <nfhfyxer@gmail.com>
2022-09-19 16:02:09 +0300

    added CompAmbientSound

---
+++ Source/VFECore/VFECore/Comps/ThingComps/CompAmbientSound.cs
@@ -0,0 +1,42 @@
+﻿using Verse;
+using Verse.Sound;
+
+namespace VFECore
+{
+    public class CompProperties_AmbientSound : CompProperties
+    {
+        public SoundDef ambientSound;
+        public CompProperties_AmbientSound()
+        {
+            this.compClass = typeof(CompAmbientSound);
+        }
+    }
+    public class CompAmbientSound : ThingComp
+    {
+        private Sustainer sustainerAmbient;
+        public CompProperties_AmbientSound Props => base.props as CompProperties_AmbientSound;
+        public override void PostSpawnSetup(bool respawningAfterLoad)
+        {
+            base.PostSpawnSetup(respawningAfterLoad);
+            LongEventHandler.ExecuteWhenFinished(delegate
+            {
+                SoundInfo info = SoundInfo.InMap(this.parent);
+                if (this.parent is Pawn pawn)
+                {
+                    if (pawn.pather is null) pawn.pather = new Pawn_PathFollower(pawn);
+                    if (pawn.stances is null) pawn.stances = new Pawn_StanceTracker(pawn);
+                }
+                sustainerAmbient = Props.ambientSound.TrySpawnSustainer(info);
+            });
+        }
+        public override void PostDeSpawn(Map map)
+        {
+            base.PostDeSpawn(map);
+            if (sustainerAmbient != null)
+            {
+                sustainerAmbient.End();
+            }
+        }
+    }
+}
+
