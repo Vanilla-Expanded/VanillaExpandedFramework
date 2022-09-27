d8f81fd74ae34e0a699e8a0bcbb68aee880de7a6
Joshua <legodude17@users.noreply.github.com>
2022-09-26 22:24:01 -0700

    VSE: Fix tech limiter on factions

--- Source/VanillaStorytellersExpanded/VanillaStorytellersExpanded/XeoNovaDanCode/Patch_FactionGenerator.cs
+++ Source/VanillaStorytellersExpanded/VanillaStorytellersExpanded/XeoNovaDanCode/Patch_FactionGenerator.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Reflection;
 using System.Reflection.Emit;
 using HarmonyLib;
 using RimWorld;
@@ -9,43 +7,25 @@ using Verse;
 
 namespace VanillaStorytellersExpanded
 {
-	// Token: 0x0200000A RID: 10
-	public static class Patch_FactionGenerator
-	{
-		// Token: 0x0200000B RID: 11
-		[HarmonyPatch(typeof(FactionGenerator), "GenerateFactionsIntoWorld")]
-		public static class GenerateFactionsIntoWorld
-		{
-			// Token: 0x0600000E RID: 14 RVA: 0x000020AA File Offset: 0x000002AA
-			public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
-			{
-				List<CodeInstruction> instructionList = instructions.ToList<CodeInstruction>();
-				MethodInfo allDefsInfo = AccessTools.Property(typeof(DefDatabase<FactionDef>), "AllDefs").GetGetMethod();
-				MethodInfo allowedFactionDefsInfo = AccessTools.Method(typeof(Patch_FactionGenerator.GenerateFactionsIntoWorld), "AllowedFactionDefs", null, null);
-				int num;
-				for (int i = 0; i < instructionList.Count; i = num + 1)
-				{
-					CodeInstruction instruction = instructionList[i];
-					bool flag = instruction.opcode == OpCodes.Call && instruction.OperandIs(allDefsInfo);
-					if (flag)
-					{
-						yield return instruction;
-						instruction = new CodeInstruction(OpCodes.Call, allowedFactionDefsInfo);
-					}
-					yield return instruction;
-					instruction = null;
-					num = i;
-				}
-				yield break;
-			}
+    public static class Patch_FactionGenerator
+    {
+        [HarmonyPatch(typeof(FactionGenerator), "GenerateFactionsIntoWorld")]
+        public static class GenerateFactionsIntoWorld
+        {
+            public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+            {
+                var allDefsInfo = AccessTools.PropertyGetter(typeof(DefDatabase<FactionDef>), "AllDefs");
+                var allowedFactionDefsInfo = AccessTools.Method(typeof(GenerateFactionsIntoWorld), nameof(AllowedFactionDefs));
+                foreach (var instruction in instructions)
+                {
+                    yield return instruction;
+                    if (instruction.Calls(allDefsInfo) || instruction.opcode == OpCodes.Ldarg_0)
+                        yield return new CodeInstruction(OpCodes.Call, allowedFactionDefsInfo);
+                }
+            }
 
-			// Token: 0x0600000F RID: 15 RVA: 0x000024A8 File Offset: 0x000006A8
-			private static IEnumerable<FactionDef> AllowedFactionDefs(IEnumerable<FactionDef> original)
-			{
-				return from f in original
-				where CustomStorytellerUtility.FactionAllowed(f)
-				select f;
-			}
-		}
-	}
+            private static IEnumerable<FactionDef> AllowedFactionDefs(IEnumerable<FactionDef> original) =>
+                original?.Where(f => CustomStorytellerUtility.FactionAllowed(f));
+        }
+    }
 }
