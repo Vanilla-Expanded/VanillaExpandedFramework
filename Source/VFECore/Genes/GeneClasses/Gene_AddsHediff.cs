d4dd66e86a585c73d6131a6449c73f7c4490019b
juanosarg <31105173+juanosarg@users.noreply.github.com>
2022-09-27 18:21:23 +0200

    Added Gene_AddsHediff

---
+++ Source/VFECore/Genes/GeneClasses/Gene_AddsHediff.cs
@@ -0,0 +1,17 @@
+﻿
+using RimWorld;
+using Verse;
+
+namespace VanillaGenesExpanded
+{
+
+	public class Gene_AddsHediff : Gene
+	{
+		public override void PostAdd()
+		{
+			base.PostAdd();
+			pawn.health.AddHediff(this.def.hediff);
+
+		}
+	}
+}
