07bfe85860794177a23636fe954f88ff624b4c1a
juanosarg <31105173+juanosarg@users.noreply.github.com>
2022-09-27 12:13:37 +0200

    First things for GeneExtension

---
+++ Source/VFECore/Genes/GraphicsCache/GraphicsCache.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+using Verse;
+
+namespace VanillaGenesExpanded
+{
+    [StaticConstructorOnStartup]
+    public static class GraphicsCache
+    {
+
+        public static readonly CachedTexture GeneBackground_Xenogene = new CachedTexture("UI/Icons/Genes/GeneBackground_Xenogene");
+        public static readonly CachedTexture GeneBackground_Endogene = new CachedTexture("UI/Icons/Genes/GeneBackground_Endogene");
+
+    }
+}
