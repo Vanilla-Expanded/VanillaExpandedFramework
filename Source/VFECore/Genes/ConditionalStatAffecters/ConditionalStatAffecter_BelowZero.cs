a0fc0878a434e238be63ce93e57f1a9b6fe942da
juanosarg <31105173+juanosarg@users.noreply.github.com>
2022-10-03 12:58:19 +0200

    Added ConditionalStatAffecters

---
+++ Source/VFECore/Genes/ConditionalStatAffecters/ConditionalStatAffecter_BelowZero.cs
@@ -0,0 +1,25 @@
+﻿
+using RimWorld;
+using Verse;
+
+namespace VanillaGenesExpanded
+{
+	public class ConditionalStatAffecter_BelowZero : ConditionalStatAffecter
+	{
+		public override string Label => "VGE_StatsReport_Below0".Translate();
+
+		public override bool Applies(StatRequest req)
+		{
+			if (!ModsConfig.BiotechActive)
+			{
+				return false;
+			}
+			Pawn pawn;
+			if (req.HasThing && (pawn = (req.Thing as Pawn)) != null && pawn.Map != null && pawn.Position.GetTemperature(pawn.Map) <0)
+			{
+				return true;
+			}
+			return false;
+		}
+	}
+}
---
+++ Source/VFECore/Genes/ConditionalStatAffecters/ConditionalStatAffecter_OverFortyDegrees.cs
@@ -0,0 +1,25 @@
+﻿
+using RimWorld;
+using Verse;
+
+namespace VanillaGenesExpanded
+{
+	public class ConditionalStatAffecter_OverFortyDegrees : ConditionalStatAffecter
+	{
+		public override string Label => "VGE_StatsReport_Over40".Translate();
+
+		public override bool Applies(StatRequest req)
+		{
+			if (!ModsConfig.BiotechActive)
+			{
+				return false;
+			}
+			Pawn pawn;
+			if (req.HasThing && (pawn = (req.Thing as Pawn)) != null && pawn.Map != null && pawn.Position.GetTemperature(pawn.Map) > 40)
+			{
+				return true;
+			}
+			return false;
+		}
+	}
+}
