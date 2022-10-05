a5c6462b0050b3874f2ac0c2789e57e0a40e2caa
juanosarg <31105173+juanosarg@users.noreply.github.com>
2022-10-05 13:38:50 +0200

    Tweaks to HediffComp_DieAfterPeriod

--- Source/VFECore/AnimalBehaviours/Hediffs/Properties/HediffCompProperties_DieAfterPeriod.cs
+++ Source/VFECore/AnimalBehaviours/Hediffs/Properties/HediffCompProperties_DieAfterPeriod.cs
@@ -10,6 +10,7 @@ namespace AnimalBehaviours
         public bool justVanish = false;
         public bool effect = false;
         public string effectFilth = "Filth_Blood";
+        public string DescriptionLabel = "VEF_TimeToDie";
 
         public HediffCompProperties_DieAfterPeriod()
         {
