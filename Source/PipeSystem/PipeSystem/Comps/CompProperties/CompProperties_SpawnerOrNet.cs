d616ae34ef3aead77245f9f4fa56263e7824a8d1
KylianB <kylian.balde@gmail.com>
2022-10-05 08:56:09 +0200

    PipeSystem: CompProperties_SpawnerOrNet

---
+++ Source/PipeSystem/PipeSystem/Comps/CompProperties/CompProperties_SpawnerOrNet.cs
@@ -0,0 +1,20 @@
+﻿using Verse;
+
+namespace PipeSystem
+{
+    public class CompProperties_SpawnerOrNet : CompProperties_Resource
+    {
+        public bool inheritFaction;
+        public bool requiresPower;
+        public string saveKeysPrefix;
+        public bool showMessageIfOwned;
+        public int spawnCount = 1;
+        public bool spawnForbidden;
+        public IntRange spawnIntervalRange = new IntRange(100, 100);
+        public int spawnMaxAdjacent = -1;
+        public ThingDef thingToSpawn;
+        public bool writeTimeLeftToSpawn;
+
+        public CompProperties_SpawnerOrNet() => compClass = typeof(CompSpawnerOrNet);
+    }
+}
