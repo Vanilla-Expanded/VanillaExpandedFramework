848733d5fcdb19c694fc1b5df964785bf1cfe840
Taranchuk <nfhfyxer@gmail.com>
2022-10-06 10:48:02 +0300

    added shield settings

--- Source/VFECore/VFECore/PawnShieldGenerator.cs
+++ Source/VFECore/VFECore/PawnShieldGenerator.cs
@@ -1,45 +1,43 @@
-﻿using System;
+﻿using RimWorld;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using UnityEngine;
 using Verse;
-using RimWorld;
 
 namespace VFECore
 {
-
     public static class PawnShieldGenerator
     {
 
         private static List<ThingStuffPair> allShieldPairs;
-        private static List<ThingStuffPair> workingShields = new List<ThingStuffPair>();
+        private static List<ThingStuffPair> workingShields = new();
 
         public static void Reset()
         {
-            Predicate<ThingDef> isShield = (ThingDef td) => td.GetCompProperties<CompProperties_Shield>() is CompProperties_Shield shieldProps && !shieldProps.shieldTags.NullOrEmpty();
-            allShieldPairs = ThingStuffPair.AllWith(isShield);
-            using (IEnumerator<ThingDef> enumerator = (from td in DefDatabase<ThingDef>.AllDefs where isShield(td) select td).GetEnumerator())
+            static bool isShield(ThingDef td)
             {
-                while (enumerator.MoveNext())
+                return td.GetCompProperties<CompProperties_Shield>() is CompProperties_Shield shieldProps && !shieldProps.shieldTags.NullOrEmpty();
+            }
+
+            allShieldPairs = ThingStuffPair.AllWith(isShield);
+            using var enumerator = (from td in DefDatabase<ThingDef>.AllDefs where isShield(td) select td).GetEnumerator();
+            while (enumerator.MoveNext())
+            {
+                var thingDef = enumerator.Current;
+                float num = (from pa in allShieldPairs where pa.thing == thingDef select pa).Sum((ThingStuffPair pa) => pa.Commonality);
+                float num2 = thingDef.generateCommonality / num;
+                if (num2 != 1f)
                 {
-                    ThingDef thingDef = enumerator.Current;
-                    float num = (from pa in allShieldPairs where pa.thing == thingDef select pa).Sum((ThingStuffPair pa) => pa.Commonality);
-                    float num2 = thingDef.generateCommonality / num;
-                    if (num2 != 1f)
+                    for (int i = 0; i < allShieldPairs.Count; i++)
                     {
-                        for (int i = 0; i < allShieldPairs.Count; i++)
+                        var thingStuffPair = allShieldPairs[i];
+                        if (thingStuffPair.thing == thingDef)
                         {
-                            ThingStuffPair thingStuffPair = allShieldPairs[i];
-                            if (thingStuffPair.thing == thingDef)
-                            {
-                                allShieldPairs[i] = new ThingStuffPair(thingStuffPair.thing, thingStuffPair.stuff, thingStuffPair.commonalityMultiplier * num2);
-                            }
+                            allShieldPairs[i] = new ThingStuffPair(thingStuffPair.thing, thingStuffPair.stuff, thingStuffPair.commonalityMultiplier * num2);
                         }
                     }
                 }
-                enumerator.Dispose();
             }
+            enumerator.Dispose();
         }
 
         public static void TryGenerateShieldFor(Pawn pawn)
@@ -49,38 +47,50 @@ namespace VFECore
 
             // No shieldTags
             if (kindDefExtension.shieldTags.NullOrEmpty())
+            {
                 return;
+            }
 
             // Not a tool user
             if (!pawn.RaceProps.ToolUser)
+            {
                 return;
+            }
 
             // Primary unusable with shields
             if (pawn.equipment.Primary is ThingWithComps primary && !primary.def.UsableWithShields())
+            {
                 return;
+            }
 
             // Has multiple weapons
             if (pawn.equipment.AllEquipmentListForReading.Count(t => t.def.equipmentType == EquipmentType.Primary) > 1)
+            {
                 return;
+            }
 
             // Not enough manipulation
             if (!pawn.health.capacities.CapableOf(PawnCapacityDefOf.Manipulation) || !pawn.CanUseShields())
+            {
                 return;
+            }
 
             // Pacifist
             if (pawn.story != null && pawn.WorkTagIsDisabled(WorkTags.Violent))
+            {
                 return;
+            }
 
             float randomInRange = kindDefExtension.shieldMoney.RandomInRange;
             for (int i = 0; i < allShieldPairs.Count; i++)
             {
-                ThingStuffPair w = allShieldPairs[i];
+                var w = allShieldPairs[i];
                 if (w.Price <= randomInRange)
                 {
                     var shieldProps = w.thing.GetCompProperties<CompProperties_Shield>();
                     if (kindDefExtension.shieldTags == null || kindDefExtension.shieldTags.Any((string tag) => shieldProps.shieldTags.Contains(tag)))
                     {
-                        if (w.thing.generateAllowChance >= 1f || Rand.ChanceSeeded(w.thing.generateAllowChance, pawn.thingIDNumber ^ (int)w.thing.shortHash ^ 28554824))
+                        if (w.thing.generateAllowChance >= 1f || Rand.ChanceSeeded(w.thing.generateAllowChance, pawn.thingIDNumber ^ w.thing.shortHash ^ 28554824))
                         {
                             workingShields.Add(w);
                         }
@@ -92,8 +102,7 @@ namespace VFECore
                 return;
             }
             //pawn.equipment.DestroyAllEquipment(DestroyMode.Vanish);
-            ThingStuffPair thingStuffPair;
-            if (workingShields.TryRandomElementByWeight((ThingStuffPair w) => w.Commonality * w.Price, out thingStuffPair))
+            if (workingShields.TryRandomElementByWeight((ThingStuffPair w) => w.Commonality * w.Price, out var thingStuffPair))
             {
                 var shield = (Apparel)ThingMaker.MakeThing(thingStuffPair.thing, thingStuffPair.stuff);
                 PawnGenerator.PostProcessGeneratedGear(shield, pawn);
@@ -103,7 +112,9 @@ namespace VFECore
                 {
                     var thingDefExtension = ThingDefExtension.Get(thingStuffPair.thing);
                     if (!thingDefExtension.useFactionColourForPawnKinds.NullOrEmpty() && thingDefExtension.useFactionColourForPawnKinds.Contains(pawn.kindDef))
+                    {
                         shield.SetColor(pawn.Faction.Color);
+                    }
                 }
                 pawn.AddShield(shield);
             }
---
+++ Source/VFECore/VFECore/VanillaShieldsExpandedPatcher.cs
@@ -0,0 +1,177 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using Verse;
+
+namespace VFECore
+{
+    [StaticConstructorOnStartup]
+    public static class VanillaShieldsExpandedStartup
+    {
+        public static bool anyShieldItemPresent;
+        static VanillaShieldsExpandedStartup()
+        {
+            anyShieldItemPresent = DefDatabase<ThingDef>.AllDefs.Any(x => x.GetCompProperties<CompProperties_Shield>() != null);
+            VanillaShieldsExpandedSettings.usableWithShieldsWeapons ??= new Dictionary<string, bool>();
+            VanillaShieldsExpandedSettings.allWeapons = DefDatabase<ThingDef>.AllDefs.Where(x => x.IsWeapon && !x.destroyOnDrop).ToList();
+            SetValues();
+        }
+
+        public static void SetValues()
+        {
+            foreach (var thingDef in VanillaShieldsExpandedSettings.allWeapons)
+            {
+                var extension = thingDef.GetModExtension<ThingDefExtension>();
+                if (extension != null)
+                {
+                    if (!VanillaShieldsExpandedSettings.usableWithShieldsWeapons.TryGetValue(thingDef.defName, out _))
+                    {
+                        VanillaShieldsExpandedSettings.usableWithShieldsWeapons[thingDef.defName] = _ = extension.usableWithShields;
+                    }
+                }
+                else
+                {
+                    if (thingDef.IsRangedWeapon)
+                    {
+                        if (!VanillaShieldsExpandedSettings.usableWithShieldsWeapons.ContainsKey(thingDef.defName))
+                        {
+                            VanillaShieldsExpandedSettings.usableWithShieldsWeapons[thingDef.defName] = false;
+                        }
+                    }
+                    else if (thingDef.IsMeleeWeapon)
+                    {
+                        if (!VanillaShieldsExpandedSettings.usableWithShieldsWeapons.ContainsKey(thingDef.defName))
+                        {
+                            VanillaShieldsExpandedSettings.usableWithShieldsWeapons[thingDef.defName] = true;
+                        }
+                    }
+                }
+
+                if (VanillaShieldsExpandedSettings.usableWithShieldsWeapons.TryGetValue(thingDef.defName, out bool usableWithShields))
+                {
+                    if (usableWithShields)
+                    {
+                        if (extension is null)
+                        {
+                            thingDef.modExtensions ??= new List<DefModExtension>();
+                            extension = new ThingDefExtension();
+                            thingDef.modExtensions.Add(extension);
+                        }
+                    }
+                    if (extension != null)
+                    {
+                        extension.usableWithShields = usableWithShields;
+                    }
+                }
+            }
+        }
+    }
+    public class VanillaShieldsExpandedMod : Mod
+    {
+        public static VanillaShieldsExpandedSettings settings;
+        public VanillaShieldsExpandedMod(ModContentPack pack) : base(pack)
+        {
+            settings = GetSettings<VanillaShieldsExpandedSettings>();
+        }
+        public override void DoSettingsWindowContents(Rect inRect)
+        {
+            base.DoSettingsWindowContents(inRect);
+            settings.DoSettingsWindowContents(inRect);
+        }
+
+        public override string SettingsCategory()
+        {
+            return VanillaShieldsExpandedStartup.anyShieldItemPresent ? "Vanilla Shields Expanded" : "";
+        }
+
+        public override void WriteSettings()
+        {
+            base.WriteSettings();
+            VanillaShieldsExpandedStartup.SetValues();
+        }
+    }
+
+    public class VanillaShieldsExpandedSettings : ModSettings
+    {
+        public static Dictionary<string, bool> usableWithShieldsWeapons = new();
+        public static List<ThingDef> allWeapons = new();
+        public override void ExposeData()
+        {
+            base.ExposeData();
+            Scribe_Collections.Look(ref usableWithShieldsWeapons, "usableWithShieldsWeapons", LookMode.Value, LookMode.Value);
+        }
+
+        private string searchKey;
+        public bool showMeleeWeapons = true;
+        public bool showRangedWeapons = true;
+        public void DoSettingsWindowContents(Rect inRect)
+        {
+            var rect = new Rect(inRect.x, inRect.y, inRect.width, inRect.height);
+            Text.Anchor = TextAnchor.MiddleLeft;
+            var searchLabel = new Rect(rect.x + 5, rect.y, 60, 24);
+            Widgets.Label(searchLabel, "VEF.Shields.Search".Translate());
+            var searchRect = new Rect(searchLabel.xMax + 5, searchLabel.y, 200, 24f);
+            searchKey = Widgets.TextField(searchRect, searchKey);
+            Text.Anchor = TextAnchor.UpperLeft;
+
+            var showMeleeWeaponsRect = new Rect(searchRect.xMax + 15, searchRect.y, 180, 24);
+            Widgets.CheckboxLabeled(showMeleeWeaponsRect, "VEF.Shields.ShowMeleeWeapons".Translate(), ref showMeleeWeapons);
+
+            var showRangeWeaponsRect = new Rect(showMeleeWeaponsRect.xMax + 30, searchRect.y, 180, 24);
+            Widgets.CheckboxLabeled(showRangeWeaponsRect, "VEF.Shields.ShowRangeWeapons".Translate(), ref showRangedWeapons);
+
+            var thingDefs = (searchKey.NullOrEmpty() ? allWeapons : allWeapons.Where(x => x.label.ToLower().Contains(searchKey.ToLower())))
+                .Where(x => (x.IsRangedWeapon && showRangedWeapons) || (x.IsMeleeWeapon && showMeleeWeapons)).OrderBy(x => x.label).ToList();
+
+            var resetRect = new Rect(searchLabel.x, searchLabel.yMax + 5, 265, 24f);
+            if (Widgets.ButtonText(resetRect, "VEF.Shields.ResetModSettingsToDefault".Translate()))
+            {
+                usableWithShieldsWeapons.Clear();
+                VanillaShieldsExpandedStartup.SetValues();
+            }
+
+            var explanationTitleRect = new Rect(resetRect.xMax + 15, resetRect.y, inRect.width - (resetRect.width + 35), 24f);
+            Widgets.Label(explanationTitleRect, "VEF.Shields.ExplanationTitle".Translate());
+
+            float height = GetScrollHeight(thingDefs);
+            var outerRect = new Rect(rect.x, searchRect.yMax + 35, rect.width, rect.height - 70);
+            var viewArea = new Rect(rect.x, outerRect.y, rect.width - 16, height);
+            Widgets.BeginScrollView(outerRect, ref scrollPosition, viewArea, true);
+            var outerPos = new Vector2(rect.x + 5, outerRect.y);
+            float num = 0;
+            int entryHeight = 200;
+            foreach (var def in thingDefs)
+            {
+                bool canDrawGroup = num >= scrollPosition.y - entryHeight && num <= (scrollPosition.y + outerRect.height);
+                float curNum = outerPos.y;
+                if (canDrawGroup)
+                {
+                    var iconRect = new Rect(outerPos.x + 5, outerPos.y + 5, 24, 24);
+                    Widgets.ThingIcon(iconRect, def);
+                    var labelRect = new Rect(iconRect.xMax + 15, outerPos.y + 5, viewArea.width - 80, 24f);
+                    Widgets.Label(labelRect, def.LabelCap);
+                    var pos = new Vector2(viewArea.width - 40, labelRect.y);
+                    bool value = usableWithShieldsWeapons[def.defName];
+                    Widgets.Checkbox(pos, ref value);
+                    usableWithShieldsWeapons[def.defName] = value;
+                }
+                var innerPos = new Vector2(outerPos.x + 10, outerPos.y);
+                outerPos.y += 24;
+                num += outerPos.y - curNum;
+            }
+            Widgets.EndScrollView();
+        }
+
+        private float GetScrollHeight(List<ThingDef> defs)
+        {
+            float num = 0;
+            foreach (var def in defs)
+            {
+                num += 24;
+            }
+            return num + 5;
+        }
+        private static Vector2 scrollPosition = Vector2.zero;
+    }
+
+}
