26c2323b04e99d7ba2da86d2e7a7c38e6ab0ee39
juanosarg <31105173+juanosarg@users.noreply.github.com>
2022-10-14 13:39:48 +0200

    Removed obsolete WildAnimalSpawner transpiler

---
+++ Source/VFECore/AnimalBehaviours/Harmony/MapTemperature_SeasonAcceptableFor_Patch.cs
@@ -0,0 +1,44 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System.Reflection;
+using Verse;
+using System.Reflection.Emit;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using System;
+using Verse.AI;
+
+namespace AnimalBehaviours
+{
+
+    [HarmonyPatch(typeof(MapTemperature))]
+    [HarmonyPatch("SeasonAcceptableFor")]
+    public static class VanillaExpandedFramework_MapTemperature_SeasonAcceptableFor_Patch
+    {
+        [HarmonyPostfix]
+        public static void AllowAnimalSpawns(ThingDef animalRace, ref bool __result)
+
+        {
+
+           
+
+            if (VanillaAnimalsExpanded_Mod.settings.pawnSpawnStates != null && VanillaAnimalsExpanded_Mod.settings.pawnSpawnStates.Keys.Contains(animalRace.defName))
+            {
+                if (VanillaAnimalsExpanded_Mod.settings.pawnSpawnStates[animalRace.defName])
+                {
+
+                    __result = false;
+                }
+
+            }
+
+
+        }
+    }
+
+
+
+
+
+}
