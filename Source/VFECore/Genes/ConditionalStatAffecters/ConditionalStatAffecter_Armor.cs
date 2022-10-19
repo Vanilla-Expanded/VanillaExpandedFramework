3955b2a3eafa3f90b68a548c82e2438e7e05b00c
juanosarg <31105173+juanosarg@users.noreply.github.com>
2022-10-19 12:09:32 +0200

    Recompiled and added ConditionalStatAffecters

---
+++ Source/VFECore/Genes/ConditionalStatAffecters/ConditionalStatAffecter_Armor.cs
@@ -0,0 +1,53 @@
+﻿using RimWorld;
+using Verse;
+using System.Collections.Generic;
+
+namespace VanillaGenesExpanded
+
+{
+	public class ConditionalStatAffecter_Armor : ConditionalStatAffecter
+	{
+		public override string Label => "VGE_StatsReport_Armoured".Translate();
+
+		public override bool Applies(StatRequest req)
+		{
+			if (!ModsConfig.BiotechActive)
+			{
+				return false;
+			}
+			Pawn pawn;
+			bool flag = false;
+			if (req.HasThing && (pawn = req.Thing as Pawn) != null && pawn.apparel != null)
+			{
+
+				List<Apparel> wornApparel = pawn.apparel.WornApparel;
+				for (int i = 0; i < wornApparel.Count; i++)
+				{
+					if (wornApparel[i].Stuff?.stuffProps?.categories?.Contains(StuffCategoryDefOf.Metallic) == true || wornApparel[i].def?.thingCategories?.Contains(ThingCategoryDefOf.ApparelArmor) == true)
+					{
+						flag = true;
+					}
+
+					if (wornApparel[i].def?.thingSetMakerTags?.Contains("Warcasket") ?? false)
+					{
+						flag = true;
+					}
+
+				}
+
+				if (flag)
+				{
+
+					return true;
+				}
+
+
+
+			}
+			return false;
+
+
+
+		}
+	}
+}
---
+++ Source/VFECore/Genes/ConditionalStatAffecters/ConditionalStatAffecter_NoSunlight.cs
@@ -0,0 +1,23 @@
+﻿
+using RimWorld;
+using Verse;
+namespace VanillaGenesExpanded
+{
+	public class ConditionalStatAffecter_NoSunlight : ConditionalStatAffecter
+	{
+		public override string Label => "VGE_StatsReport_NoSunlight".Translate();
+
+		public override bool Applies(StatRequest req)
+		{
+			if (!ModsConfig.BiotechActive)
+			{
+				return false;
+			}
+			if (req.HasThing && req.Thing.Spawned)
+			{
+				return !req.Thing.Position.InSunlight(req.Thing.Map);
+			}
+			return false;
+		}
+	}
+}
