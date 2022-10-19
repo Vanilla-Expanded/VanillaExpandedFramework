f5c9f38b4dfcb2474159957ea9373757ed8859db
juanosarg <31105173+juanosarg@users.noreply.github.com>
2022-10-19 12:26:05 +0200

    Added ConditionalStatAffecters

---
+++ Source/VFECore/Genes/ConditionalStatAffecters/ConditionalStatAffecter_DryLand.cs
@@ -0,0 +1,36 @@
+﻿using RimWorld;
+using Verse;
+using System.Collections.Generic;
+
+namespace VanillaGenesExpanded
+
+{
+	public class ConditionalStatAffecter_DryLand : ConditionalStatAffecter
+	{
+		public override string Label => "VGE_StatsReport_NotWater".Translate();
+
+		public override bool Applies(StatRequest req)
+		{
+			if (!ModsConfig.BiotechActive)
+			{
+				return false;
+			}
+			Pawn pawn;
+
+			if (req.HasThing && (pawn = req.Thing as Pawn) != null && pawn.Map != null)
+			{
+
+				if (!pawn.Position.GetTerrain(pawn.Map).IsWater)
+				{
+					return true;
+				}
+
+
+			}
+			return false;
+
+
+
+		}
+	}
+}
