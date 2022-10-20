201a2a55bc3e01ee21ea49ca9372c5184ab58fc6
KylianB <kylian.balde@gmail.com>
2022-10-20 10:25:51 +0200

    Update VWEMakeshift to 1.4

--- Source/VWEMakeshift/GunsGoPewPew/Verb_MakeshiftShoot.cs
+++ Source/VWEMakeshift/GunsGoPewPew/Verb_MakeshiftShoot.cs
@@ -2,43 +2,43 @@
 
 namespace VWEMakeshift
 {
-  public class Verb_MakeshiftShoot : Verb_ShootWithSmoke
-  {
-    protected override int ShotsPerBurst
+    public class Verb_MakeshiftShoot : Verb_ShootWithSmoke
     {
-      get
-      {
-        var projectile   = this.GetProjectile();
-        var equipmentDef = EquipmentSource?.def;
-        if (equipmentDef is null)
+        protected override int ShotsPerBurst
         {
-          Log.Error($"Unable to retrieve weapon def from <color=teal>{GetType()}</color>. Please report to Oskar or Smash Phil.");
-          return base.ShotsPerBurst;
+            get
+            {
+                var projectile = this.GetProjectile();
+                var equipmentDef = EquipmentSource?.def;
+                if (equipmentDef is null)
+                {
+                    Log.Error($"Unable to retrieve weapon def from <color=teal>{GetType()}</color>. Please report to Oskar or Smash Phil.");
+                    return base.ShotsPerBurst;
+                }
+
+                var makeshiftProps = equipmentDef.GetModExtension<MakeshiftProperties>();
+                if (makeshiftProps is null)
+                {
+                    Log.ErrorOnce($"<color=teal>{GetType()}</color> cannot be used without <color=teal>MakeshiftProperties</color> DefModExtension. Motes will not be thrown.", Gen.HashCombine(projectile.GetHashCode(), "MakeshiftProperties".GetHashCode()));
+                    return base.ShotsPerBurst;
+                }
+
+                var shots = makeshiftProps.shots.RandomInRange;
+                return shots;
+            }
         }
 
-        var makeshiftProps = equipmentDef.GetModExtension<MakeshiftProperties>();
-        if (makeshiftProps is null)
+        public override bool TryStartCastOn(LocalTargetInfo castTarg, LocalTargetInfo destTarg, bool surpriseAttack = false, bool canHitNonTargetPawns = true, bool preventFriendlyFire = false, bool nonInterruptingSelfCast = false)
         {
-          Log.ErrorOnce($"<color=teal>{GetType()}</color> cannot be used without <color=teal>MakeshiftProperties</color> DefModExtension. Motes will not be thrown.", Gen.HashCombine(projectile.GetHashCode(), "MakeshiftProperties".GetHashCode()));
-          return base.ShotsPerBurst;
+            if (ShotsPerBurst <= 0)
+            {
+                ticksToNextBurstShot = verbProps.ticksBetweenBurstShots;
+                if (CasterIsPawn && !verbProps.nonInterruptingSelfCast) CasterPawn.stances.SetStance(new Stance_Cooldown(verbProps.ticksBetweenBurstShots + 1, currentTarget, this));
+
+                return false;
+            }
+
+            return base.TryStartCastOn(castTarg, destTarg, surpriseAttack, canHitNonTargetPawns, preventFriendlyFire, nonInterruptingSelfCast);
         }
-
-        var shots = makeshiftProps.shots.RandomInRange;
-        return shots;
-      }
     }
-
-    public override bool TryStartCastOn(LocalTargetInfo castTarg, LocalTargetInfo destTarg, bool surpriseAttack = false, bool canHitNonTargetPawns = true, bool preventFriendlyFire = false)
-    {
-      if (ShotsPerBurst <= 0)
-      {
-        ticksToNextBurstShot = verbProps.ticksBetweenBurstShots;
-        if (CasterIsPawn && !verbProps.nonInterruptingSelfCast) CasterPawn.stances.SetStance(new Stance_Cooldown(verbProps.ticksBetweenBurstShots + 1, currentTarget, this));
-
-        return false;
-      }
-
-      return base.TryStartCastOn(castTarg, destTarg, surpriseAttack, canHitNonTargetPawns);
-    }
-  }
 }
