736a3e4ab91f322113407eb54b028154dc9546d2
juanosarg <31105173+juanosarg@users.noreply.github.com>
2022-10-23 16:49:13 +0200

    Added HediffComp_RemoveFromMechs

---
+++ Source/VFECore/AnimalBehaviours/Hediffs/HediffComp_RemoveFromMechs.cs
@@ -0,0 +1,37 @@
+﻿
+using RimWorld;
+using System.Collections.Generic;
+using Verse;
+using System.Linq;
+using Verse.Sound;
+using UnityEngine;
+using AnimalBehaviours;
+
+namespace AnimalBehaviours
+{
+    class HediffComp_RemoveFromMechs : HediffComp
+    {
+        public HediffCompProperties_RemoveFromMechs Props
+        {
+            get
+            {
+                return (HediffCompProperties_RemoveFromMechs)this.props;
+            }
+        }
+
+
+
+        public override void CompPostMake()
+        {
+            base.CompPostMake();
+            if (this.parent.pawn.RaceProps.IsMechanoid)
+            {
+                this.parent.pawn.health.RemoveHediff(this.parent);
+            }
+        }
+
+
+
+
+    }
+}
---
+++ Source/VFECore/AnimalBehaviours/Hediffs/Properties/HediffCompProperties_RemoveFromMechs.cs
@@ -0,0 +1,17 @@
+﻿
+﻿using System;
+using Verse;
+using System.Collections.Generic;
+
+namespace AnimalBehaviours
+{
+    public class HediffCompProperties_RemoveFromMechs : HediffCompProperties
+    {
+
+
+        public HediffCompProperties_RemoveFromMechs()
+        {
+            this.compClass = typeof(HediffComp_RemoveFromMechs);
+        }
+    }
+}
