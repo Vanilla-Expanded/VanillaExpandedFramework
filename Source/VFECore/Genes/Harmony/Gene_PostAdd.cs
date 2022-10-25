1df4eb6d14196cd3c8b2bf7b5d4a8d2ada0aa9c7
Taranchuk <nfhfyxer@gmail.com>
2022-10-25 07:40:59 +0300

    apply gene effects only after gene generator or after pawn generator

--- Source/VFECore/Genes/Harmony/Gene_PostAdd.cs
+++ Source/VFECore/Genes/Harmony/Gene_PostAdd.cs
@@ -1,33 +1,52 @@
 using HarmonyLib;
+using System.Collections.Generic;
 using Verse;
 
 namespace VanillaGenesExpanded
 {
-    [HarmonyPatch(typeof(Gene), "PostAdd")]
-    public static class VanillaGenesExpanded_Gene_PostAdd_Patch
+    [HarmonyPatch(typeof(PawnGenerator), "GenerateGenes")]
+    public static class PawnGenerator_GenerateGenes_Patch
     {
-        [HarmonyPostfix]
-        public static void PostFix(Gene __instance)
+        public static void Postfix(Pawn pawn)
         {
-            if (__instance.Active)
+            List<Gene> genes = pawn.genes.GenesListForReading;
+            foreach (Gene gene in genes)
             {
-                GeneExtension extension = __instance.def.GetModExtension<GeneExtension>();
-                if (extension != null)
+                if (gene.Active)
                 {
-                    if (extension.forceFemale == true)
-                    {
-                        __instance.pawn.gender = Gender.Female;
-                    }
-                    if (extension.forceMale == true)
-                    {
-                        __instance.pawn.gender = Gender.Male;
-                    }
+                    Gene_PostAdd_Patch.ApplyGeneEffects(gene);
+                }
+            }
+        }
+    }
+    [HarmonyPatch(typeof(Gene), "PostAdd")]
+    public static class Gene_PostAdd_Patch
+    {
+        public static void Postfix(Gene __instance)
+        {
+            if (PawnGenerator.IsBeingGenerated(__instance.pawn) is false && __instance.Active)
+            {
+                ApplyGeneEffects(__instance);
+            }
+        }
+        public static void ApplyGeneEffects(Gene __instance)
+        {
+            GeneExtension extension = __instance.def.GetModExtension<GeneExtension>();
+            if (extension != null)
+            {
+                if (extension.forceFemale == true)
+                {
+                    __instance.pawn.gender = Gender.Female;
+                }
+                if (extension.forceMale == true)
+                {
+                    __instance.pawn.gender = Gender.Male;
+                }
 
-                    if (extension.forcedBodyType != null)
-                    {
-                        __instance.pawn.story.bodyType = extension.forcedBodyType;
-                        __instance.pawn.Drawer.renderer.graphics.SetAllGraphicsDirty();
-                    }
+                if (extension.forcedBodyType != null)
+                {
+                    __instance.pawn.story.bodyType = extension.forcedBodyType;
+                    __instance.pawn.Drawer.renderer.graphics.SetAllGraphicsDirty();
                 }
             }
         }
