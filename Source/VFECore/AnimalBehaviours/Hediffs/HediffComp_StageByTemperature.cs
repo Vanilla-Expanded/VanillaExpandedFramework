6f7f0556320085defff9d3b98aa9c272c3cf467f
juanosarg <31105173+juanosarg@users.noreply.github.com>
2022-10-27 19:10:39 +0200

    Added HediffCompProperties_StageByTemperature

---
+++ Source/VFECore/AnimalBehaviours/Hediffs/HediffComp_StageByTemperature.cs
@@ -0,0 +1,50 @@
+﻿
+
+using RimWorld;
+using System.Collections.Generic;
+using Verse;
+using UnityEngine;
+using Verse.Sound;
+using UnityEngine;
+using Verse.Noise;
+
+namespace AnimalBehaviours
+{
+    class HediffComp_StageByTemperature : HediffComp
+    {
+
+
+        public HediffCompProperties_StageByTemperature Props
+        {
+            get
+            {
+                return (HediffCompProperties_StageByTemperature)this.props;
+            }
+        }
+
+
+        public override void CompPostTick(ref float severityAdjustment)
+        {
+            base.CompPostTick(ref severityAdjustment);
+
+            if (this.parent.pawn.IsHashIntervalTick(500)&& this.parent.pawn.Map!=null)
+            {
+                float tempNow = parent.pawn.Position.GetTemperature(parent.pawn.Map);
+
+                float percentage = Mathf.Clamp((tempNow - Props.minTemp) / (Props.maxTemp - Props.minTemp),0,1);
+
+                
+
+                this.parent.Severity = percentage;
+
+
+
+            }
+
+
+        }
+
+
+
+    }
+}
---
+++ Source/VFECore/AnimalBehaviours/Hediffs/Properties/HediffCompProperties_StageByTemperature.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Verse;
+using System.Collections.Generic;
+
+namespace AnimalBehaviours
+{
+    public class HediffCompProperties_StageByTemperature : HediffCompProperties
+    {
+
+        public int minTemp = 0;
+        public int maxTemp = 0;
+      
+
+        public HediffCompProperties_StageByTemperature()
+        {
+            this.compClass = typeof(HediffComp_StageByTemperature);
+        }
+    }
+}
