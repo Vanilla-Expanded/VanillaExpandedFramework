99c955ebd81565b1991c8ecf04d01ac9b6f588fc
Taranchuk <nfhfyxer@gmail.com>
2022-11-01 20:39:46 +0200

    removed xml error reports

--- Source/VFECore/VFECore/Startup.cs
+++ Source/VFECore/VFECore/Startup.cs
@@ -15,50 +15,6 @@ namespace VFECore
             // Cache setters
             PawnShieldGenerator.Reset();
             ScenPartUtility.SetCache();
-            CheckXmlErrors();
         }
-
-        public static void CheckXmlErrors()
-        {
-            foreach (var def in DefDatabase<WorkGiverDef>.AllDefs)
-            {
-                if (def.giverClass is null)
-                {
-                    Log.Error(def.defName + " is missing worker class and will not work properly. Report about it to " + def.modContentPack?.Name + " devs.");
-                }
-            }
-            foreach (var def in DefDatabase<ThingDef>.AllDefs)
-            {
-                if (def.thingClass is null)
-                {
-                    Log.Error(def.defName + " is missing thing class and will not work properly. Report about it to " + def.modContentPack?.Name + " devs.");
-                }
-                if (def.comps != null)
-                {
-                    foreach (var compProps in def.comps)
-                    {
-                        if (compProps.compClass is null)
-                        {
-                            Log.Error(def.defName + " one of comps is missing comp class and will not work properly. Report about it to " + def.modContentPack?.Name + " devs.");
-                        }
-                    }
-                }
-            }
-
-            foreach (var def in DefDatabase<SoundDef>.AllDefs)
-            {
-                foreach (var subSound in def.subSounds)
-                {
-                    foreach (var grain in subSound.grains)
-                    {
-                        if (grain.GetResolvedGrains().Any() is false)
-                        {
-                            Log.Error(def.defName + " sound is missing resolved grains and will not work properly. Report about it to " + def.modContentPack?.Name + " devs.");
-                        }
-                    }
-                }
-            }
-        }
-
     }
 }
