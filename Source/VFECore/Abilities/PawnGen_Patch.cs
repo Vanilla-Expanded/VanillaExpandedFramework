c74ee604656fb173a04906ef6f3b5858bd03905c
Taranchuk <nfhfyxer@gmail.com>
2022-11-05 16:49:38 +0200

    null checks

--- Source/VFECore/Abilities/PawnGen_Patch.cs
+++ Source/VFECore/Abilities/PawnGen_Patch.cs
@@ -16,27 +16,32 @@ namespace VFECore.Abilities
         [HarmonyPostfix]
         public static void Postfix(Pawn __result)
         {
-            PawnKindAbilityExtension abilityExtension = __result.kindDef?.GetModExtension<PawnKindAbilityExtension>();
-            if (abilityExtension == null)
-                return;
-
-            if (abilityExtension.implantDef != null)
+            if (__result != null)
             {
-                Hediff_Abilities implant = __result.health?.hediffSet?.GetFirstHediffOfDef(abilityExtension.implantDef) as Hediff_Abilities ??
-                                           HediffMaker.MakeHediff(abilityExtension.implantDef, __result,
-                                                                  __result.RaceProps?.body?.GetPartsWithDef(BodyPartDefOf.Brain).FirstOrFallback()) as Hediff_Abilities;
-                if(implant != null)
+                PawnKindAbilityExtension abilityExtension = __result.kindDef?.GetModExtension<PawnKindAbilityExtension>();
+                if (abilityExtension == null)
+                    return;
+
+                if (abilityExtension.implantDef != null)
                 {
-                    implant.giveRandomAbilities = abilityExtension.giveRandomAbilities;
-                    __result.health?.AddHediff(implant);
-                    implant.SetLevelTo(abilityExtension.initialLevel);
+                    Hediff_Abilities implant = __result.health?.hediffSet?.GetFirstHediffOfDef(abilityExtension.implantDef) as Hediff_Abilities ??
+                                               HediffMaker.MakeHediff(abilityExtension.implantDef, __result,
+                                                                      __result.RaceProps?.body?.GetPartsWithDef(BodyPartDefOf.Brain).FirstOrFallback()) as Hediff_Abilities;
+                    if (implant != null)
+                    {
+                        implant.giveRandomAbilities = abilityExtension.giveRandomAbilities;
+                        __result.health?.AddHediff(implant);
+                        implant.SetLevelTo(abilityExtension.initialLevel);
+                    }
+                }
+
+                CompAbilities comp = __result.GetComp<CompAbilities>();
+                if (comp != null)
+                {
+                    foreach (AbilityDef abilityDef in abilityExtension.giveAbilities)
+                        comp.GiveAbility(abilityDef);
                 }
             }
-
-            CompAbilities comp = __result.GetComp<CompAbilities>();
-
-            foreach (AbilityDef abilityDef in abilityExtension.giveAbilities)
-                comp.GiveAbility(abilityDef);
         }
     }
 }
