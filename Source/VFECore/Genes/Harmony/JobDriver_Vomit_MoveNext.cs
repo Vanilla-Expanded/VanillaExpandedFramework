ad0f1b0a48b860ac8ca224ffb787fdba61963372
juanosarg <31105173+juanosarg@users.noreply.github.com>
2022-11-12 16:35:47 +0100

    Without errors now

--- Source/VFECore/Genes/Harmony/JobDriver_Vomit_MoveNext.cs
+++ Source/VFECore/Genes/Harmony/JobDriver_Vomit_MoveNext.cs
@@ -21,6 +21,13 @@ namespace VanillaGenesExpanded
     public static class VanillaGenesExpanded_JobDriver_Vomit_MoveNext_Patch
     {
 
+
+
+
+
+
+
+
         static MethodBase TargetMethod()
         {
             MethodBase method = typeof(JobDriver_Vomit).GetNestedType("<MakeNewToils>d__4", BindingFlags.Instance | BindingFlags.NonPublic).GetMethod("MoveNext", BindingFlags.Instance | BindingFlags.NonPublic);
@@ -33,27 +40,27 @@ namespace VanillaGenesExpanded
         {
             var codes = codeInstructions.ToList();
             var field = AccessTools.Field(typeof(EffecterDefOf), "Vomit");
-            var pawnField = AccessTools.Field(typeof(JobDriver_Vomit), "pawn");
+            
             foreach (var code in codes)
             {
 
                 if (code.opcode == OpCodes.Ldsfld && code.LoadsField(field))
                 {
 
-                    yield return new CodeInstruction(OpCodes.Ldfld, pawnField);
+                   
                     yield return new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(VanillaGenesExpanded_JobDriver_Vomit_MoveNext_Patch), nameof(GetVomitEffecter)));
                 }
                 else yield return code;
             }
         }
 
-        public static EffecterDef GetVomitEffecter(Pawn pawn)
+        public static EffecterDef GetVomitEffecter()
         {
             
-            if (pawn!=null&&StaticCollectionsClass.vomitEffect_gene_pawns.ContainsKey(pawn))
+           /* if (StaticCollectionsClass.vomitEffect_gene_pawns.ContainsKey(pawn))
             {
                 return StaticCollectionsClass.vomitEffect_gene_pawns[pawn];
-            }
+            }*/
             return EffecterDefOf.Vomit;
 
         }
