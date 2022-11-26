1f124dfbab1bc029f6fa6614e26534705510084a
Máté Szabó <mszabo-oss@protonmail.com>
2022-11-26 15:50:12 +0100

    MVCF: Avoid caching stale verbs in VerbManager
    MVCF currently patches OrderForceAttack() and TryGetAttackVerb() if any
    of its features were activated by a mod in the modlist; these will then
    affect verb choice for any pawn. However, this can pose a problem if the
    current modlist does not activate Feature_Humanoid, or if Combat
    Extended is active: it can lead to a situation where VerbManager caches
    a stale CurrentVerb forever, and use it even after the pawn has changed
    weapons.
    
    A simple reproduction follows:
    (1) Have a mod in the modlist that uses some MVCF features - e.g. vanilla animals caves
    (2) Start quicktest map
    (3) Equip starting rifle, draft colonist, shoot at some crap
    (4) Equip starting knife
    (5) Spawn manhunter
    (6) Colonist will shoot with the knife
    
    As a fix, move EquipmentTracker-related patches from Feature_Humanoid to
    Feature, so that pawns' equipment changes are correctly tracked if MVCF
    is active. For this to work correctly, also patch
    EquipmentTracker.TryTransferEquipmentToContainer, which is called in
    some vanilla edge cases (e.g. when a pawn is downed while onboard a
    transport pod or shuttle), but is also used to implement Combat
    Extended's inventory system. Finally, update VerbManager.RemoveVerb() to
    null out its cached CurrentVerb if it was the verb that just got
    removed.
    
    On existing saves, it's possible for error spam to occur if a pawn
    managed to cache a ranged verb inside its VerbManager, then later
    switched to a melee weapon - when this pawn is drafted, it'll attempt to
    use its cached, discarded ranged verb to "scan" for targets via
    CheckForAutoAttack() as if they were wielding a ranged weapon, and cause
    errors as the `caster` for this verb will be `null` if it was discarded.
    Avoid this by adding a basic logic check to VerbManager to null out the
    cached CurrentVerb value on init if it's no longer wielded by this pawn.

--- Source/MVCF/Features/Feature.cs
+++ Source/MVCF/Features/Feature.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using HarmonyLib;
 using MVCF.Comps;
 using MVCF.Features.PatchSets;
+using MVCF.ModCompat;
 using MVCF.ModCompat.PatchSets;
 using MVCF.Utilities;
 using MVCF.VerbComps;
@@ -24,6 +25,16 @@ public abstract class Feature
         yield return Patch.Postfix(AccessTools.Method(typeof(VerbTracker), "InitVerb"), AccessTools.Method(GetType(), nameof(PostInitVerb)));
         yield return Patch.Postfix(AccessTools.Method(typeof(VerbTracker), "InitVerb"), AccessTools.Method(GetType(), nameof(PostInitVerb)));
         foreach (var patch in TargetFinder.GetPatches()) yield return patch;
+
+        // Track pawn equipment changes if any feature is enabled to ensure that verbs selected by OrderForceTarget()
+        // correctly reflect changes to equipment.
+        var equipmentTrackerType = typeof(Pawn_EquipmentTracker);
+        yield return Patch.Postfix(AccessTools.Method(equipmentTrackerType, "Notify_EquipmentAdded"),
+            AccessTools.Method(GetType(), nameof(EquipmentAdded_Postfix)));
+        yield return Patch.Prefix(AccessTools.Method(equipmentTrackerType, "Notify_EquipmentRemoved"),
+            AccessTools.Method(GetType(), nameof(EquipmentRemoved_Prefix)));
+        yield return Patch.Postfix(AccessTools.Method(equipmentTrackerType, "TryTransferEquipmentToContainer"),
+            AccessTools.Method(GetType(), nameof(TryTransferEquipmentToContainer_Postfix)));
     }
 
     public virtual IEnumerable<PatchSet> GetPatchSets()
@@ -78,5 +89,38 @@ public abstract class Feature
         var comps = additionalComps.ToList();
         var mv = verb.Managed(false) ?? props.CreateManaged(!((props is null || props.comps.NullOrEmpty()) && comps.NullOrEmpty()));
         mv.Initialize(verb, props, comps);
+    }
+
+    public static void EquipmentAdded_Postfix(ThingWithComps eq, Pawn_EquipmentTracker __instance)
+    {
+        __instance.pawn.Manager(false)?.AddVerbs(eq);
+    }
+
+    public static void EquipmentRemoved_Prefix(ThingWithComps eq, Pawn_EquipmentTracker __instance) => MaybeRemoveEquipmentVerb(eq, __instance);
+
+    /// <summary>
+    /// Called when a piece of equipment is transferred from a pawn's <see cref="Pawn_EquipmentTracker">EquipmentTracker</see> to another container.
+    /// </summary>
+    /// <remarks>
+    /// This can occur in some edge cases in vanilla (e.g. a pawn dying or getting downed inside a shuttle / transport pod), but may also be used by mods.
+    /// </remarks>
+    public static void TryTransferEquipmentToContainer_Postfix(ThingWithComps eq, Pawn_EquipmentTracker __instance) => MaybeRemoveEquipmentVerb(eq, __instance);
+
+    /// <summary>
+    /// Attempt to remove the verb(s) of a piece of equipment from the <see cref="VerbManager"/> of the owning pawn.
+    /// </summary>
+    private static void MaybeRemoveEquipmentVerb(ThingWithComps eq, Pawn_EquipmentTracker __instance)
+    {
+        if (MVCF.IsIgnoredMod(eq?.def?.modContentPack?.Name)) return;
+        if (MVCF.ShouldIgnore(eq)) return;
+        if (DualWieldCompat.Active && eq.IsOffHand()) return;
+        var comp = eq.TryGetComp<CompEquippable>();
+        if (comp?.VerbTracker?.AllVerbs == null) return;
+        var manager = __instance?.pawn?.Manager(false);
+        if (manager == null) return;
+        if (MVCF.GetFeature<Feature_ExtraEquipmentVerbs>().Enabled)
+            foreach (var verb in comp.VerbTracker.AllVerbs.Concat(manager.ExtraVerbsFor(eq)))
+                manager.RemoveVerb(verb);
+        else manager.RemoveVerb(comp.PrimaryVerb);
     }
 }
