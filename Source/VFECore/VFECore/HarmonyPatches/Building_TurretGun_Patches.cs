def420d62408f0964c4df925be3b3f6344236e5d
Taranchuk <nfhfyxer@gmail.com>
2023-02-02 15:41:56 +0200

    Add Verb_ShootCone from VFE Pirates

---
+++ Source/VFECore/VFECore/HarmonyPatches/Building_TurretGun_Patches.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Text;
+using UnityEngine;
+using Verse;
+using RimWorld;
+using HarmonyLib;
+using VFEPirates;
+
+namespace VFECore
+{
+
+    [HarmonyPatch(typeof(Building_TurretGun), "IsValidTarget")]
+    class Building_TurretGun_IsValidTarget_Patch
+    {
+        public static void Postfix(Thing t, Building_TurretGun __instance, ref bool __result)
+        {
+            if (__instance.AttackVerb is Verb_ShootCone verbShootCone)
+            {
+                __result &= verbShootCone.InCone(t.Position, verbShootCone.caster.Position, verbShootCone.Caster.Rotation, verbShootCone.VerbProps.coneAngle);
+            }
+        }
+    }
+
+    [HarmonyPatch(typeof(Building_TurretGun), "DrawExtraSelectionOverlays")]
+    public static class Building_TurretGun_DrawExtraSelectionOverlays_Transpiler
+    {
+        public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            var codes = instructions.ToList();
+            bool flag = false; //only apply patch to first occurence of DrawRadiusRing
+            for (int i = 0; i < codes.Count; i++)
+            {
+                if (codes[i].operand as MethodInfo == typeof(GenDraw).GetMethod("DrawRadiusRing", new Type[] { typeof(IntVec3), typeof(float) }) && !flag)
+                {
+                    flag = true;
+                    yield return new CodeInstruction(OpCodes.Ldarg_0);
+                    yield return new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(Building_TurretGun_DrawExtraSelectionOverlays_Transpiler), nameof(DrawConeForDirectionalTurret)));
+                }
+                else
+                {
+                    yield return codes[i];
+                }
+            }
+        }
+        public static void DrawConeForDirectionalTurret(IntVec3 center, float radius, Building_TurretGun instance)
+        {
+            //Don't draw radius ring for directional turrets
+            if (!(instance.AttackVerb is Verb_ShootCone))
+            {
+                GenDraw.DrawRadiusRing(instance.Position, radius);
+            }
+        }
+    }
+
+    [HarmonyPatch(typeof(Building_TurretGun), "DrawExtraSelectionOverlays")]
+    public static class Building_TurretGun_DrawExtraSelectionOverlays_Patch
+    {
+        public static void Postfix(Building_TurretGun __instance)
+        {
+            //Draw cone instead of radius ring for directional turret
+            if ((__instance.AttackVerb is Verb_ShootCone verb_ShootCone))
+            {
+                verb_ShootCone.DrawHighlight(verb_ShootCone.CurrentTarget);
+            }
+        }
+    }
+
+    //Don't rotate directional turrets. 
+    [HarmonyPatch(typeof(TurretTop), "get_CurRotation")]
+    class TurretTop_get_CurRotation_Patch
+    {
+        public static bool Prefix(ref Building_Turret ___parentTurret, ref int ___ticksUntilIdleTurn, ref float __result)
+        {
+            if (___parentTurret.AttackVerb is Verb_ShootCone)
+            {
+                var currentTarget = ___parentTurret.CurrentTarget;
+                if (!currentTarget.IsValid)
+                {
+                    __result = ___parentTurret.Rotation.AsAngle;
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ Source/VFECore/VFECore/Verbs/Verb_ShootCone.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using UnityEngine;
+using Verse;
+
+namespace VFEPirates
+{
+    public class VerbProps_ShootCone : VerbProperties
+    {
+        public int coneAngle;
+    }
+    public class Verb_ShootCone : Verb_Shoot
+    {
+        public VerbProps_ShootCone VerbProps => this.verbProps as VerbProps_ShootCone;
+        private Material lineMat = null;
+        private Material LineMat
+        {
+            get
+            {
+                if (lineMat == null)
+                {
+                    lineMat = MaterialPool.MatFrom(GenDraw.LineTexPath, ShaderDatabase.Transparent, Color.white);
+                }
+                return lineMat;
+            }
+        }
+
+        public override void DrawHighlight(LocalTargetInfo target)
+        {
+            if (VerbProps.range <= GenRadial.MaxRadialPatternRadius)
+            {
+                DrawConeRounded(VerbProps.coneAngle);
+            }
+            else
+            {
+                DrawLines();
+            }
+
+            if (target.IsValid)
+            {
+                GenDraw.DrawTargetHighlight(target);
+                DrawHighlightFieldRadiusAroundTarget(target);
+            }
+        }
+
+
+        private void DrawLines()
+        {
+            var startPos = this.Caster.Position.ToVector3Shifted();
+            var quatLeft = Quaternion.Euler(0f, -VerbProps.coneAngle / 2f, 0f);
+            var quatRight = Quaternion.Euler(0f, VerbProps.coneAngle / 2f, 0f);
+            var targetLeft = startPos + (this.Caster.Rotation.AsQuat * quatLeft * new Vector3(0f, 0f, verbProps.range));
+            var targetRight = startPos + (this.Caster.Rotation.AsQuat * quatRight * new Vector3(0f, 0f, verbProps.range));
+            GenDraw.DrawLineBetween(startPos, targetLeft, AltitudeLayer.MetaOverlays.AltitudeFor(), LineMat, 0.5f);
+            GenDraw.DrawLineBetween(startPos, targetRight, AltitudeLayer.MetaOverlays.AltitudeFor(), LineMat, 0.5f);
+
+        }
+        private void DrawConeRounded(float angle)
+        {
+            IntVec3 pos = this.Caster.Position;
+            Rot4 rotation = this.caster.Rotation;
+
+            Func<IntVec3, bool> predicate = ((IntVec3 c) => InCone(c, pos, rotation, angle));
+            GenDraw.DrawRadiusRing(pos, this.verbProps.range, Color.white, predicate);
+        }
+        public override bool CanHitTarget(LocalTargetInfo targ)
+        {
+            return base.CanHitTarget(targ) && InCone(targ.Cell, this.caster.Position, this.caster.Rotation, VerbProps.coneAngle);
+        }
+
+        public bool InCone(IntVec3 evaluatedCell, IntVec3 from, Rot4 rotation, float degrees)
+        {
+            Vector3 dif = evaluatedCell.ToVector3() - from.ToVector3();
+            Vector3 lookRotation = Quaternion.LookRotation(dif, Vector3.up).eulerAngles;
+            if (GenGeo.AngleDifferenceBetween(lookRotation.y, rotation.AsAngle) <= degrees / 2f)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
