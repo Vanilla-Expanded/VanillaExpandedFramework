10bf95b24f3e8c11995786e6503ae051058cb878
Taranchuk <nfhfyxer@gmail.com>
2023-02-08 00:43:58 +0200

    Disable showing mod source in backstory tips when the option is enabled

--- Source/VFECore/VFECore/HarmonyPatches/DisableModSourceReport_Patches.cs
+++ Source/VFECore/VFECore/HarmonyPatches/DisableModSourceReport_Patches.cs
@@ -63,4 +63,29 @@ namespace VFECore
             return VFEGlobal.settings.disableModSourceReport is false;
         }
     }
+
+    [HarmonyPatch(typeof(BackstoryDef), "FullDescriptionFor")]
+    public static class BackstoryDef_FullDescriptionFor_Patch
+    {
+        public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> codeInstructions)
+        {
+            var codes = codeInstructions.ToList();
+            var get_IsCoreModInfo = AccessTools.Method(typeof(ModContentPack), "get_IsOfficialMod");
+            for (int i = 0; i < codes.Count; i++)
+            {
+                var code = codes[i];
+                yield return code;
+                if (codes[i].opcode == OpCodes.Brtrue_S && codes[i - 1].Calls(get_IsCoreModInfo))
+                {
+                    yield return new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(BackstoryDef_FullDescriptionFor_Patch), nameof(ShouldShow)));
+                    yield return new CodeInstruction(OpCodes.Brfalse_S, codes[i].operand);
+                }
+            }
+        }
+
+        public static bool ShouldShow()
+        {
+            return VFEGlobal.settings.disableModSourceReport is false;
+        }
+    }
 }
