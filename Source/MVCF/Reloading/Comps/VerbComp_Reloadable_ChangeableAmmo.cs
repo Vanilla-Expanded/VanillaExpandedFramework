09b76d216c4bfd95941a94ed4bef1fc9fd90b72b
Joshua <legodude17@users.noreply.github.com>
2023-05-07 22:14:27 -0700

    Add AmmoProjectile mapping

--- Source/MVCF/Reloading/Comps/VerbComp_Reloadable_ChangeableAmmo.cs
+++ Source/MVCF/Reloading/Comps/VerbComp_Reloadable_ChangeableAmmo.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Xml;
+using MVCF.Comps;
 using MVCF.VerbComps;
 using Verse;
 
@@ -21,7 +23,16 @@ public class VerbComp_Reloadable_ChangeableAmmo : VerbComp_Reloadable, IThingHol
 
     public ThingOwner GetDirectlyHeldThings() => LoadedAmmo;
     public IThingHolder ParentHolder => null;
-    public override ThingDef ProjectileOverride(ThingDef oldProjectile) => NextAmmoItem?.def.projectileWhenLoaded;
+
+    public override ThingDef ProjectileOverride(ThingDef oldProjectile)
+    {
+        if (NextAmmoItem == null) return null;
+        if (props is VerbCompProperties_Reloadable_ChangeableAmmo { AmmoProjectileDictionary: { } ammoProjectiles }
+         && ammoProjectiles.TryGetValue(NextAmmoItem.def, out var newProjectile))
+            return newProjectile;
+
+        return NextAmmoItem.def.projectileWhenLoaded;
+    }
 
     public override void ExposeData()
     {
@@ -61,5 +72,29 @@ public class VerbComp_Reloadable_ChangeableAmmo : VerbComp_Reloadable, IThingHol
         LoadedAmmo.TryDropAll(parent.Verb.caster.Position, parent.Verb.caster.Map, ThingPlaceMode.Near);
         NextAmmoItem = null;
         ShotsRemaining = 0;
+    }
+}
+
+public class VerbCompProperties_Reloadable_ChangeableAmmo : VerbCompProperties_Reloadable
+{
+    public Dictionary<ThingDef, ThingDef> AmmoProjectileDictionary;
+    public List<AmmoProjectileData> AmmoProjectiles;
+
+    public override void PostLoadSpecial(VerbProperties verbProps, AdditionalVerbProps additionalProps, Def parentDef)
+    {
+        base.PostLoadSpecial(verbProps, additionalProps, parentDef);
+        AmmoProjectileDictionary = AmmoProjectiles?.ToDictionary(ap => ap.Ammo, ap => ap.Projectile);
+    }
+}
+
+public class AmmoProjectileData
+{
+    public ThingDef Ammo;
+    public ThingDef Projectile;
+
+    public void LoadDataFromXmlCustom(XmlNode xmlRoot)
+    {
+        DirectXmlCrossRefLoader.RegisterObjectWantsCrossRef(this, "Ammo", xmlRoot);
+        DirectXmlCrossRefLoader.RegisterObjectWantsCrossRef(this, "Projectile", xmlRoot.FirstChild.Value);
     }
 }
