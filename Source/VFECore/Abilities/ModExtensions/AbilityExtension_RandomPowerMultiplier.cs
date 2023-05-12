2cf908884a7c8544743fe23683ba262294b9a6a6
Kolton Noreen <noreenko@users.noreply.github.com>
2023-05-11 19:05:24 -0500

    Add AbilityExtension_RandomPowerMultiplier.cs

---
+++ Source/VFECore/Abilities/ModExtensions/AbilityExtension_RandomPowerMultiplier.cs
@@ -0,0 +1,12 @@
+﻿using Verse;
+
+namespace VFECore.Abilities;
+
+/// power (after stat offsets) becomes a random number within a range.
+///
+/// The formula used is Rand.Range(power * min, power * max)
+public class AbilityExtension_RandomPowerMultiplier : DefModExtension
+{
+    public float min = 1f;  // Must be <= max
+    public float max = 1f;  // Must be >= min
+}
