b5625043835d9d2c01d832ff80e7c570fa680a23
Kolton Noreen <noreenko@users.noreply.github.com>
2023-05-29 19:11:55 -0500

    Make xml example more concise, make Ability.GetPowerForPawn more concise.

--- Source/VFECore/Abilities/ModExtensions/AbilityExtension_RandomPowerMultiplier.cs
+++ Source/VFECore/Abilities/ModExtensions/AbilityExtension_RandomPowerMultiplier.cs
@@ -7,10 +7,7 @@ namespace VFECore.Abilities;
 /// The formula used is power * Rand.Range(range.min, range.max)
 /// Example XML:
 /// <li Class="VFECore.Abilities.AbilityExtension_RandomPowerMultiplier">
-///   <range>
-///     <min>0.5 </min>
-///     <!-- Because max is not provided, it defaults to 1f -->
-///   </range>
+///   <range>0.5~1.0</range>
 /// </li>
 public class AbilityExtension_RandomPowerMultiplier : DefModExtension
 {
