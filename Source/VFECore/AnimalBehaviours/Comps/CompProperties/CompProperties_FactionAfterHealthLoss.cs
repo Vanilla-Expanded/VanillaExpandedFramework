8270bdf165878a3baec4188b7f20367918b6a797
FantasyFanGitHub <60392272+FantasyFanGitHub@users.noreply.github.com>
2023-06-15 22:25:57 +0200

    Add files via upload

---
+++ Source/VFECore/AnimalBehaviours/Comps/CompProperties/CompProperties_FactionAfterHealthLoss.cs
@@ -0,0 +1,30 @@
+﻿using Verse;
+using UnityEngine;
+
+namespace AnimalBehaviours
+{
+    public class CompProperties_FactionAfterHealthLoss : CompProperties
+    {
+
+        //A comp class that changes the faction of the pawn after losing a precent of it health
+
+        public int healthPercent = 50;
+        public int tickInterval = 1000;
+
+        public string factionToReturnTo = "";
+
+        //If true  and factionToReturnTo not set, their faction will be set to a random non hostile faction
+        //If false or factionToReturnTo not set, their faction will be set to a random enemy faction
+
+        public bool nonHostileFaction = false;
+
+        //If true they will attack the colony
+
+        public bool attackColony = false;
+
+        public CompProperties_FactionAfterHealthLoss()
+        {
+            this.compClass = typeof(Comp_FactionAfterHealthLoss);
+        }
+    }
+}
