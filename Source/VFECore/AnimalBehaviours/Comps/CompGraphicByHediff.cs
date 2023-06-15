5290df7fedb02d0c4e50178dadad707f1f86c380
FantasyFanGitHub <60392272+FantasyFanGitHub@users.noreply.github.com>
2023-06-15 22:25:18 +0200

    Add files via upload

---
+++ Source/VFECore/AnimalBehaviours/Comps/CompGraphicByHediff.cs
@@ -0,0 +1,130 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+using System.Runtime.CompilerServices;
+using System.Xml;
+using UnityEngine;
+using Verse;
+
+namespace AnimalBehaviours
+{
+    [StaticConstructorOnStartup]
+    public class CompGraphicByHediff : ThingComp
+    {
+        private PawnRenderer pawn_renderer;
+        public Graphic dessicatedGraphic;
+        public int changeGraphicsCounter = 0;
+
+        public CompProperties_GraphicByHediff Props
+        {
+            get
+            {
+                return (CompProperties_GraphicByHediff) this.props;
+            }
+        }
+
+        public override void CompTick()
+        {
+            changeGraphicsCounter++;
+            if (changeGraphicsCounter > Props.changeGraphicsInterval)
+            {
+                this.ChangeTheGraphics();
+                changeGraphicsCounter = 0;
+            }
+            base.CompTick();
+        }
+
+        public override void PostSpawnSetup(bool respawningAfterLoad)
+        {
+            base.PostSpawnSetup(respawningAfterLoad);
+            Pawn pawn = this.parent as Pawn;
+            this.pawn_renderer = pawn.Drawer.renderer;
+
+            if (Props.changeDesiccatedGraphic)
+            {
+                GraphicData dessicatedgraphicdata = new GraphicData();
+                dessicatedgraphicdata.texPath = Props.dessicatedTxt;
+                dessicatedGraphic = dessicatedgraphicdata.Graphic;
+            }
+
+            this.ChangeTheGraphics();
+        }
+
+        public void ChangeTheGraphics()
+        {
+            if (this.parent.Map != null && AnimalBehaviours_Settings.flagGraphicChanging)
+            {
+                Pawn pawn = this.parent as Pawn;
+                if (this.pawn_renderer == null)
+                {
+                    this.pawn_renderer = pawn.Drawer.renderer;
+                }
+
+                bool hediffFound = false;
+                Hediff hediff = pawn.health.hediffSet.GetFirstHediffOfDef(Props.hediffDef, false);
+                if (hediff != null)
+                {
+                    hediffFound = true;
+                }
+
+                //Uses drawsize, color, colorTwo and ShadowData of the original bodyGraphicData
+                Vector2 vector = pawn.ageTracker.CurKindLifeStage.bodyGraphicData.drawSize;
+                Color color = pawn.ageTracker.CurKindLifeStage.bodyGraphicData.color;
+                Color color2 = pawn.ageTracker.CurKindLifeStage.bodyGraphicData.color;
+                var shader = ShaderDatabase.Cutout;
+                if (Props.useCutoutComplex)
+                {
+                    shader = ShaderDatabase.CutoutComplex;
+                }
+                var data = new GraphicData();
+                data.shadowData = pawn.ageTracker.CurKindLifeStage.bodyGraphicData.shadowData;
+
+
+
+                if (hediffFound)
+                {
+                    LongEventHandler.ExecuteWhenFinished(delegate
+                    {
+                        if (this.pawn_renderer != null)
+                        {
+                            try
+                            {
+                                Graphic_Multi nakedGraphic = (Graphic_Multi)GraphicDatabase.Get<Graphic_Multi>(Props.newImagePath, shader, vector, color, color2, data, maskPath: Props.maskPath);
+                                if (Props.changeDesiccatedGraphic)
+                                {
+                                    this.pawn_renderer.graphics.dessicatedGraphic = dessicatedGraphic;
+                                }
+                                this.pawn_renderer.graphics.ResolveAllGraphics();
+                                this.pawn_renderer.graphics.nakedGraphic = nakedGraphic;
+                            }
+                            catch (NullReferenceException) { }
+                        }
+                    });
+                }
+                else
+                {
+                    LongEventHandler.ExecuteWhenFinished(delegate
+                    {
+                        if (this.pawn_renderer != null)
+                        {
+                            try
+                            {
+                                Graphic_Multi nakedGraphic = (Graphic_Multi)GraphicDatabase.Get<Graphic_Multi>(pawn.ageTracker.CurKindLifeStage.bodyGraphicData.texPath, shader, vector, color, color2, data, maskPath: pawn.ageTracker.CurKindLifeStage.bodyGraphicData.maskPath);
+                                if (Props.changeDesiccatedGraphic)
+                                {
+                                    this.pawn_renderer.graphics.dessicatedGraphic = pawn.ageTracker.CurKindLifeStage.dessicatedBodyGraphicData.Graphic;
+                                }
+                                this.pawn_renderer.graphics.ResolveAllGraphics();
+                                this.pawn_renderer.graphics.nakedGraphic = nakedGraphic;
+                            }
+                            catch (NullReferenceException) { }
+                        }
+                    });
+                }
+            }
+        }
+    }
+}
---
+++ Source/VFECore/AnimalBehaviours/Comps/Comp_FactionAfterHealthLoss.cs
@@ -0,0 +1,66 @@
+﻿using Verse;
+using RimWorld;
+using System.Collections.Generic;
+using Verse.AI.Group;
+
+namespace AnimalBehaviours
+{
+    public class Comp_FactionAfterHealthLoss : ThingComp
+    {
+        public int tickCounter = 0;
+        bool SetFactionOnce = true;
+
+        public CompProperties_FactionAfterHealthLoss Props
+        {
+            get
+            {
+                return (CompProperties_FactionAfterHealthLoss)this.props;
+            }
+        }
+
+        public override void CompTick()
+        {
+            base.CompTick();
+            tickCounter++;
+            //Only check things every tickInterval
+            if (tickCounter > Props.tickInterval && SetFactionOnce)
+            {
+                Pawn pawn = this.parent as Pawn;
+                if (pawn != null && pawn.Map != null && !pawn.Dead && !pawn.Downed)
+                {
+                    //If pawn's health reaches a threshold
+                    if (pawn.health.summaryHealth.SummaryHealthPercent < ((float)(Props.healthPercent) / 100))
+                    {
+                        //if nonHostileFaction is false and factionToReturnTo is not set, the pawn is set to a random enemy faction
+                        if (Props.factionToReturnTo == "" && !Props.nonHostileFaction)
+                        {
+                            var faction = Find.FactionManager.RandomEnemyFaction(allowNonHumanlike: false);
+                            pawn.SetFaction(faction);
+                            SetFactionOnce = false;
+                        }
+                        //if nonHostileFaction is true and factionToReturnTo is not set, the pawn is set to a random non hostile faction
+                        else if (Props.factionToReturnTo == "")
+                        {
+                            var faction = Find.FactionManager.RandomNonHostileFaction(allowNonHumanlike: false);
+                            pawn.SetFaction(faction);
+                            SetFactionOnce = false;
+                        }
+                        //if factionToReturnTo is set, the pawn is set to the faction it should belong to
+                        else
+                        {
+                            parent.SetFaction(Find.FactionManager.FirstFactionOfDef(FactionDef.Named(Props.factionToReturnTo)), null);
+                            SetFactionOnce = false;
+                        };
+                        //if attackColony is true the pawn will attack the colony
+                        if (Props.attackColony)
+                        {
+                            var map = pawn.Map;
+                            LordMaker.MakeNewLord(pawn.Faction, new LordJob_AssaultColony(pawn.Faction, true, false, false, true, true, false, true), map, new List<Pawn> { pawn });
+                        }
+                    }
+                }
+                tickCounter = 0;
+            }
+        }
+    }
+}
