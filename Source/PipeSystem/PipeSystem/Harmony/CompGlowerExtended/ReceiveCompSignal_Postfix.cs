6617fe31e6adc620518cf32491c4e8b412162b50
KylianB <kylian.balde@gmail.com>
2023-07-08 14:29:34 +0200

    PipeSystem: CompGlowerExtended compatibility

---
+++ Source/PipeSystem/PipeSystem/Harmony/CompGlowerExtended/ReceiveCompSignal_Postfix.cs
@@ -0,0 +1,20 @@
+﻿using HarmonyLib;
+using VanillaFurnitureExpanded;
+using Verse;
+
+namespace PipeSystem.GlowerExtended
+{
+    /// <summary>
+    /// Make sure CompGlowerExtended update the light.
+    /// </summary>
+    [HarmonyPatch(typeof(CompGlowerExtended))]
+    [HarmonyPatch("ReceiveCompSignal", MethodType.Normal)]
+    public static class ReceiveCompSignal_Postfix
+    {
+        public static void Postfix(string signal, ThingWithComps ___parent, CompGlowerExtended __instance)
+        {
+            if (CachedSignals.IsResourceSignal(signal))
+                __instance.UpdateLit();
+        }
+    }
+}
---
+++ Source/PipeSystem/PipeSystem/Harmony/CompGlowerExtended/ShouldBeLitNow_Postfix.cs
@@ -0,0 +1,26 @@
+﻿using HarmonyLib;
+using VanillaFurnitureExpanded;
+using Verse;
+
+namespace PipeSystem.GlowerExtended
+{
+    /// <summary>
+    /// Make sure CompGlowerExtended is off if any of the required resources are off.
+    /// </summary>
+    [HarmonyPatch(typeof(CompGlowerExtended))]
+    [HarmonyPatch("ShouldBeLitNow", MethodType.Getter)]
+    public static class ShouldBeLitNow_Postfix
+    {
+        public static void Postfix(ThingWithComps ___parent, ref bool __result)
+        {
+            foreach (var comp in ___parent.GetComps<CompResourceTrader>())
+            {
+                if (comp != null && !comp.ResourceOn)
+                {
+                    __result = false;
+                    return;
+                }
+            }
+        }
+    }
+}
