eb56923d9163d6992e92098ba993e43b95677d25
Taranchuk <nfhfyxer@gmail.com>
2023-11-13 11:54:47 +0200

    fix for "Collection was modified" exception

--- Source/ModSettingsFramework/Startup.cs
+++ Source/ModSettingsFramework/Startup.cs
@@ -8,9 +8,9 @@ namespace ModSettingsFramework
     {
         static Startup()
         {
-            foreach (var mod in LoadedModManager.RunningMods)
+            foreach (var mod in LoadedModManager.RunningMods.ToList())
             {
-                foreach (var patch in mod.Patches.OfType<PatchOperationWorker>())
+                foreach (var patch in mod.Patches.OfType<PatchOperationWorker>().ToList())
                 {
                     var modSettings = ModSettingsFrameworkSettings.GetModSettingsContainer(mod);
                     if (modSettings.patchWorkers.TryGetValue(patch.GetType().FullName, out var worker))
