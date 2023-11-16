d33c901a8515585e286c467040a8639897d6f58a
juanosarg <31105173+juanosarg@users.noreply.github.com>
2023-11-16 16:30:22 +0100

    Added ConditionalStatAffecter_InColonyMap

---
+++ Source/VFECore/Genes/ConditionalStatAffecters/ConditionalStatAffecter_InColonyMap.cs
@@ -0,0 +1,24 @@
+﻿
+using RimWorld;
+using Verse;
+namespace VanillaGenesExpanded
+{
+    public class ConditionalStatAffecter_InColonyMap : ConditionalStatAffecter
+    {
+        public override string Label => "VGE_StatsReport_ColonyMap".Translate();
+
+        public override bool Applies(StatRequest req)
+        {
+            if (!ModsConfig.BiotechActive)
+            {
+                return false;
+            }
+            if (req.HasThing && req.Thing.Spawned)
+            {
+
+                return req.Thing.Map?.IsPlayerHome==true;
+            }
+            return false;
+        }
+    }
+}
