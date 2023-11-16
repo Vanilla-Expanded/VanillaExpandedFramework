829f4015e9a239b51732bc9fe81bf1ee57690fae
juanosarg <31105173+juanosarg@users.noreply.github.com>
2023-11-16 16:32:07 +0100

    Added ConditionalStatAffecter_OutsideColonyMap

---
+++ Source/VFECore/Genes/ConditionalStatAffecters/ConditionalStatAffecter_OutsideColonyMap.cs
@@ -0,0 +1,24 @@
+﻿
+using RimWorld;
+using Verse;
+namespace VanillaGenesExpanded
+{
+    public class ConditionalStatAffecter_OutsideColonyMap : ConditionalStatAffecter
+    {
+        public override string Label => "VGE_StatsReport_OutsideColonyMap".Translate();
+
+        public override bool Applies(StatRequest req)
+        {
+            if (!ModsConfig.BiotechActive)
+            {
+                return false;
+            }
+            if (req.HasThing && req.Thing.Spawned)
+            {
+
+                return req.Thing.Map?.IsPlayerHome!=true;
+            }
+            return false;
+        }
+    }
+}
