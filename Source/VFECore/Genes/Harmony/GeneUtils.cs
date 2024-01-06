470d48f75a1da28da3954f45f78f1cb38b402366
Taranchuk <nfhfyxer@gmail.com>
2024-01-06 10:18:50 +0200

    removed code duplication, made a helper class to handle gene effects

---
+++ Source/VFECore/Genes/Harmony/GeneUtils.cs
@@ -0,0 +1,222 @@
+﻿using RimWorld;
+using Verse;
+
+namespace VanillaGenesExpanded
+{
+    public static class GeneUtils
+    {
+        public static void ApplyGeneEffects(Gene gene)
+        {
+            GeneExtension extension = gene.def.GetModExtension<GeneExtension>();
+            if (extension != null)
+            {
+                if (extension.forceFemale == true)
+                {
+                    gene.pawn.gender = Gender.Female;
+                    if (gene.pawn.story?.bodyType == BodyTypeDefOf.Male)
+                    {
+                        gene.pawn.story.bodyType = BodyTypeDefOf.Female;
+                    }
+                }
+                if (extension.forceMale == true)
+                {
+                    gene.pawn.gender = Gender.Male;
+                    if (gene.pawn.story?.bodyType == BodyTypeDefOf.Female)
+                    {
+                        gene.pawn.story.bodyType = BodyTypeDefOf.Male;
+                    }
+                }
+
+                if (extension.forcedBodyType != null)
+                {
+                    gene.pawn.story.bodyType = extension.forcedBodyType;
+                    gene.pawn.Drawer.renderer.graphics.SetAllGraphicsDirty();
+                }
+
+                if (extension.customBloodThingDef != null)
+                {
+                    StaticCollectionsClass.AddBloodtypeGenePawnToList(gene.pawn, extension.customBloodThingDef);
+                }
+                if (extension.customBloodIcon != "")
+                {
+                    StaticCollectionsClass.AddBloodIconGenePawnToList(gene.pawn, extension.customBloodIcon);
+                }
+                if (extension.customBloodEffect != null)
+                {
+                    StaticCollectionsClass.AddBloodEffectGenePawnToList(gene.pawn, extension.customBloodEffect);
+                }
+                if (extension.customWoundsFromFleshtype != null)
+                {
+                    StaticCollectionsClass.AddWoundsFromFleshtypeGenePawnToList(gene.pawn, extension.customWoundsFromFleshtype);
+                }
+                if (extension.caravanCarryingFactor != 1f)
+                {
+                    StaticCollectionsClass.AddCaravanCarryingFactorGenePawnToList(gene.pawn, extension.caravanCarryingFactor);
+                }
+                if (extension.diseaseProgressionFactor != 1f)
+                {
+                    StaticCollectionsClass.AddDiseaseProgressionFactorGenePawnToList(gene.pawn, extension.diseaseProgressionFactor);
+                }
+                if (extension.hediffToWholeBody != null)
+                {
+                    gene.pawn.health.AddHediff(extension?.hediffToWholeBody);
+
+                }
+                if (extension.hediffsToBodyParts != null)
+                {
+
+                    foreach (HediffToBodyparts hediffToBodypart in extension?.hediffsToBodyParts)
+                    {
+                        int enumerator = 0;
+                        foreach (BodyPartDef bodypart in hediffToBodypart.bodyparts)
+                        {
+                            if (!gene.pawn.RaceProps.body.GetPartsWithDef(bodypart).EnumerableNullOrEmpty())
+                            {
+                                if (enumerator <= gene.pawn.RaceProps.body.GetPartsWithDef(bodypart).Count)
+                                {
+                                    gene.pawn.health.AddHediff(hediffToBodypart.hediff, gene.pawn.RaceProps.body.GetPartsWithDef(bodypart).ToArray()[enumerator]);
+                                    enumerator++;
+                                }
+
+                            }
+
+                        }
+                    }
+                }
+                if (extension.customVomitThingDef != null)
+                {
+                    StaticCollectionsClass.AddVomitTypeGenePawnToList(gene.pawn, extension.customVomitThingDef);
+                }
+
+                if (extension.customVomitEffect != null)
+                {
+                    StaticCollectionsClass.AddVomitEffectGenePawnToList(gene.pawn, extension.customVomitEffect);
+                }
+
+                if (extension.noSkillLoss != null)
+                {
+                    StaticCollectionsClass.AddNoSkillLossGenePawnToList(gene.pawn, extension.noSkillLoss);
+                }
+                if (extension.skillRecreation != null)
+                {
+                    StaticCollectionsClass.AddSkillRecreationGenePawnToList(gene.pawn, extension.skillRecreation);
+                }
+                if (extension.globalSkillLossMultiplier != 1f)
+                {
+                    StaticCollectionsClass.AddSkillLossMultiplierGenePawnToList(gene.pawn, extension.globalSkillLossMultiplier);
+                }
+                if (extension.skillDegradation != false)
+                {
+                    StaticCollectionsClass.AddSkillDegradationGenePawnToList(gene.pawn);
+                }
+                if (extension.pregnancySpeedFactor != 1f)
+                {
+                    StaticCollectionsClass.AddPregnancySpeedFactorGenePawnToList(gene.pawn, extension.pregnancySpeedFactor);
+                }
+            }
+        }
+
+        public static void RemoveGeneEffects(Gene gene)
+        {
+            GeneExtension extension = gene.def.GetModExtension<GeneExtension>();
+            if (extension != null)
+            {
+                if (extension.customBloodThingDef != null)
+                {
+                    StaticCollectionsClass.RemoveBloodtypeGenePawnFromList(gene.pawn);
+
+                }
+                if (extension.customBloodIcon != "")
+                {
+                    StaticCollectionsClass.RemoveBloodIconGenePawnFromList(gene.pawn);
+
+                }
+                if (extension.customBloodEffect != null)
+                {
+                    StaticCollectionsClass.RemoveBloodEffectGenePawnFromList(gene.pawn);
+
+                }
+                if (extension.customWoundsFromFleshtype != null)
+                {
+                    StaticCollectionsClass.RemoveWoundsFromFleshtypeGenePawnFromList(gene.pawn);
+
+                }
+                if (extension.diseaseProgressionFactor != 1f)
+                {
+                    StaticCollectionsClass.RemoveDiseaseProgressionFactorGenePawnFromList(gene.pawn);
+
+                }
+                if (extension.caravanCarryingFactor != 1f)
+                {
+                    StaticCollectionsClass.RemoveCaravanCarryingFactorGenePawnFromList(gene.pawn);
+
+                }
+                if (extension?.hediffToWholeBody != null)
+                {
+                    if (gene.pawn.health.hediffSet?.HasHediff(extension?.hediffToWholeBody) == true)
+                    {
+                        Hediff hediffToRemove = gene.pawn.health.hediffSet.GetFirstHediffOfDef(extension?.hediffToWholeBody);
+                        if (hediffToRemove != null)
+                        {
+                            gene.pawn.health.RemoveHediff(hediffToRemove);
+                        }
+
+                    }
+                }
+                if (extension?.hediffsToBodyParts != null)
+                {
+                    foreach (HediffToBodyparts hediffToBodypart in extension?.hediffsToBodyParts)
+                    {
+                        foreach (BodyPartDef bodypart in hediffToBodypart.bodyparts)
+                        {
+                            if (gene.pawn.health.hediffSet?.HasHediff(hediffToBodypart.hediff) == true)
+                            {
+                                Hediff hediffToRemove = gene.pawn.health.hediffSet.GetFirstHediffOfDef(hediffToBodypart.hediff);
+                                if (hediffToRemove != null)
+                                {
+                                    gene.pawn.health.RemoveHediff(hediffToRemove);
+                                }
+
+                            }
+
+
+                        }
+
+
+                    }
+
+                }
+                if (extension.customVomitThingDef != null)
+                {
+                    StaticCollectionsClass.RemoveVomitTypeGenePawnFromList(gene.pawn);
+                }
+
+                if (extension.customVomitEffect != null)
+                {
+                    StaticCollectionsClass.RemoveVomitEffectGenePawnFromList(gene.pawn);
+                }
+
+                if (extension.noSkillLoss != null)
+                {
+                    StaticCollectionsClass.RemoveNoSkillLossGenePawnFromList(gene.pawn);
+                }
+                if (extension.skillRecreation != null)
+                {
+                    StaticCollectionsClass.RemoveSkillRecreationGenePawnFromList(gene.pawn);
+                }
+                if (extension.globalSkillLossMultiplier != 1f)
+                {
+                    StaticCollectionsClass.RemoveSkillLossMultiplierGenePawnFromList(gene.pawn);
+                }
+                if (extension.skillDegradation != false)
+                {
+                    StaticCollectionsClass.RemoveSkillDegradationGenePawnFromList(gene.pawn);
+                }
+                if (extension.pregnancySpeedFactor != 1f)
+                {
+                    StaticCollectionsClass.RemovePregnancySpeedFactorGenePawnFromList(gene.pawn);
+                }
+            }
+        }
+    }
+}
--- Source/VFECore/Genes/Harmony/Gene_PostAdd.cs
+++ Source/VFECore/Genes/Harmony/Gene_PostAdd.cs
@@ -17,7 +17,7 @@ namespace VanillaGenesExpanded
                 {
                     if (gene.Active)
                     {
-                        VanillaGenesExpanded_Gene_PostAdd_Patch.ApplyGeneEffects(gene);
+                        GeneUtils.ApplyGeneEffects(gene);
                     }
                 }
             }
@@ -30,119 +30,7 @@ namespace VanillaGenesExpanded
         {
             if (PawnGenerator.IsBeingGenerated(__instance.pawn) is false && __instance.Active)
             {
-                ApplyGeneEffects(__instance);
-            }
-        }
-        public static void ApplyGeneEffects(Gene gene)
-        {
-            GeneExtension extension = gene.def.GetModExtension<GeneExtension>();
-            if (extension != null)
-            {
-                if (extension.forceFemale == true)
-                {
-                    gene.pawn.gender = Gender.Female;
-                    if (gene.pawn.story?.bodyType == BodyTypeDefOf.Male)
-                    {
-                        gene.pawn.story.bodyType = BodyTypeDefOf.Female;
-                    }
-                }
-                if (extension.forceMale == true)
-                {
-                    gene.pawn.gender = Gender.Male;
-                    if (gene.pawn.story?.bodyType == BodyTypeDefOf.Female)
-                    {
-                        gene.pawn.story.bodyType = BodyTypeDefOf.Male;
-                    }
-                }
-
-                if (extension.forcedBodyType != null)
-                {
-                    gene.pawn.story.bodyType = extension.forcedBodyType;
-                    gene.pawn.Drawer.renderer.graphics.SetAllGraphicsDirty();
-                }
-
-                if (extension.customBloodThingDef != null)
-                {
-                    StaticCollectionsClass.AddBloodtypeGenePawnToList(gene.pawn, extension.customBloodThingDef);
-                }
-                if (extension.customBloodIcon != "")
-                {
-                    StaticCollectionsClass.AddBloodIconGenePawnToList(gene.pawn, extension.customBloodIcon);
-                }
-                if (extension.customBloodEffect != null)
-                {
-                    StaticCollectionsClass.AddBloodEffectGenePawnToList(gene.pawn, extension.customBloodEffect);
-                }
-                if (extension.customWoundsFromFleshtype != null)
-                {
-                    StaticCollectionsClass.AddWoundsFromFleshtypeGenePawnToList(gene.pawn, extension.customWoundsFromFleshtype);
-                }
-                if (extension.caravanCarryingFactor != 1f)
-                {
-                    StaticCollectionsClass.AddCaravanCarryingFactorGenePawnToList(gene.pawn, extension.caravanCarryingFactor);
-                }
-                if (extension.diseaseProgressionFactor != 1f)
-                {
-                    StaticCollectionsClass.AddDiseaseProgressionFactorGenePawnToList(gene.pawn, extension.diseaseProgressionFactor);
-                }
-                if (extension.hediffToWholeBody != null)
-                {
-                    gene.pawn.health.AddHediff(extension?.hediffToWholeBody);
-
-                }
-                if (extension.hediffsToBodyParts != null)
-                {
-
-                    foreach (HediffToBodyparts hediffToBodypart in extension?.hediffsToBodyParts)
-                    {
-                        int enumerator = 0;
-                        foreach (BodyPartDef bodypart in hediffToBodypart.bodyparts)
-                        {
-                            if (!gene.pawn.RaceProps.body.GetPartsWithDef(bodypart).EnumerableNullOrEmpty())
-                            {
-                                if (enumerator <= gene.pawn.RaceProps.body.GetPartsWithDef(bodypart).Count)
-                                {
-                                    gene.pawn.health.AddHediff(hediffToBodypart.hediff, gene.pawn.RaceProps.body.GetPartsWithDef(bodypart).ToArray()[enumerator]);
-                                    enumerator++;
-                                }
-
-                            }
-
-                        }
-                    }
-                }
-                if (extension.customVomitThingDef != null)
-                {
-                    StaticCollectionsClass.AddVomitTypeGenePawnToList(gene.pawn, extension.customVomitThingDef);
-                }
-               
-                if (extension.customVomitEffect != null)
-                {
-                    StaticCollectionsClass.AddVomitEffectGenePawnToList(gene.pawn, extension.customVomitEffect);
-                }
-
-                if (extension.noSkillLoss != null)
-                {
-                    StaticCollectionsClass.AddNoSkillLossGenePawnToList(gene.pawn, extension.noSkillLoss);
-                }
-                if (extension.skillRecreation != null)
-                {
-                    StaticCollectionsClass.AddSkillRecreationGenePawnToList(gene.pawn, extension.skillRecreation);
-                }
-                if (extension.globalSkillLossMultiplier != 1f)
-                {
-                    StaticCollectionsClass.AddSkillLossMultiplierGenePawnToList(gene.pawn, extension.globalSkillLossMultiplier);
-                }
-                if (extension.skillDegradation != false)
-                {
-                    StaticCollectionsClass.AddSkillDegradationGenePawnToList(gene.pawn);
-                }
-                if (extension.pregnancySpeedFactor != 1f)
-                {
-                    StaticCollectionsClass.AddPregnancySpeedFactorGenePawnToList(gene.pawn, extension.pregnancySpeedFactor);
-                }
-
-
+                GeneUtils.ApplyGeneEffects(__instance);
             }
         }
     }
