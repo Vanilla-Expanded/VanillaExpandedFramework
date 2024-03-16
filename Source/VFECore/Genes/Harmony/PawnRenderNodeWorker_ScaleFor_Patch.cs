75854438a68d610394a751e060a13dc96dd5dce7
Taranchuk <nfhfyxer@gmail.com>
2024-03-16 17:54:56 +0200

    moved pawn render patches

---
+++ Source/VFECore/Genes/Harmony/PawnRenderNodeWorker_ScaleFor_Patch.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+using Verse;
+using HarmonyLib;
+using System.Reflection;
+using System.Reflection.Emit;
+using UnityEngine;
+
+namespace VanillaGenesExpanded
+{
+    [HarmonyPatch(typeof(PawnRenderNodeWorker), "ScaleFor")]
+    public static class PawnRenderNodeWorker_ScaleFor_Patch
+    {
+        public static void Postfix(ref Vector3 __result, PawnRenderNode node, PawnDrawParms parms)
+        {
+            var pawn = parms.pawn;
+            if (node is PawnRenderNode_Body)
+            {
+                __result = GeneUtils.SetBodyScale(pawn, __result);
+            }
+            else if (node is PawnRenderNode_Head)
+            {
+                __result = GeneUtils.SetHeadScale(pawn, __result);
+            }
+
+            if (node.gene != null)
+            {
+                __result = GeneUtils.SetGeneScale(pawn, __result, node.gene);
+            }
+        }
+    }
+}
---
+++ Source/VFECore/Genes/Harmony/PawnRenderNode_ColorFor_Patch.cs
@@ -0,0 +1,29 @@
+﻿using HarmonyLib;
+using UnityEngine;
+using Verse;
+
+namespace VanillaGenesExpanded
+{
+    [HarmonyPatch(typeof(PawnRenderNode), "ColorFor")]
+    public static class PawnRenderNode_ColorFor_Patch
+    {
+        public static void Postfix(PawnRenderNode __instance, Pawn pawn, ref Color __result)
+        {
+            if (__instance.gene != null)
+            {
+                var extension = __instance.gene.def.GetModExtension<GeneExtension>();
+                if (extension?.applySkinColorWithGenes != null)
+                {
+                    foreach (var otherGene in extension.applySkinColorWithGenes)
+                    {
+                        if (pawn.genes.GetGene(otherGene)?.Active ?? false)
+                        {
+                            __result = pawn.story.SkinColor;
+                            return;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
