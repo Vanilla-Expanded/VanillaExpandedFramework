58c002fb83a2a22ab1a29c560d69f949af1be84c
juanosarg <31105173+juanosarg@users.noreply.github.com>
2024-05-27 19:37:14 +0200

    Added CompHediffAfterHealthLoss

---
+++ Source/VFECore/AnimalBehaviours/Comps/CompHediffAfterHealthLoss.cs
@@ -0,0 +1,61 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using Verse;
+using UnityEngine;
+using System.Collections;
+
+namespace AnimalBehaviours
+{
+    public class CompHediffAfterHealthLoss : ThingComp
+    {
+        public bool addHediffOnce = false;
+
+        public CompProperties_HediffAfterHealthLoss Props
+        {
+            get
+            {
+                return (CompProperties_HediffAfterHealthLoss)this.props;
+            }
+        }
+
+        public override void PostExposeData()
+        {
+            base.PostExposeData();
+            Scribe_Values.Look<bool>(ref this.addHediffOnce, "addHediffOnce", false, false);
+
+        }
+
+        public override void CompTick()
+        {
+            base.CompTick();
+          
+            if(!addHediffOnce)
+            {
+                //Only check things every tickInterval
+                if (this.parent.IsHashIntervalTick(Props.tickInterval))
+                {
+                    Pawn thisPawn = this.parent as Pawn;
+                    if (thisPawn != null && thisPawn.Map != null && !thisPawn.Dead && !thisPawn.Downed)
+                    {
+                        //If pawn's health reaches a threshold
+                        if (thisPawn.health.summaryHealth.SummaryHealthPercent < ((float)(Props.healthPercent) / 100))
+                        {
+                            //apply hediff
+                            thisPawn.health.AddHediff(Props.hediff, thisPawn.health.hediffSet.GetBodyPartRecord(Props.bodyPart));
+                            thisPawn.health.hediffSet.GetFirstHediffOfDef(Props.hediff).Severity = Props.severity;
+                            //this line resets the PawnRenderNodes to apply a graphic change
+                            thisPawn.Drawer.renderer.SetAllGraphicsDirty();
+                            addHediffOnce = true;
+                        }
+                    }
+
+                }
+            }
+
+            
+        }
+    }
+}
+
---
+++ Source/VFECore/AnimalBehaviours/Comps/CompProperties/CompProperties_HediffAfterHealthLoss.cs
@@ -0,0 +1,25 @@
+﻿
+using Verse;
+
+namespace AnimalBehaviours
+{
+    public class CompProperties_HediffAfterHealthLoss : CompProperties
+    {
+
+        //A comp class that applies a hediff to the pawn after losing a precent of it health
+
+        public int healthPercent = 50;
+        public int tickInterval = 1000;
+        public HediffDef hediff;
+        public float severity = 1;
+        public BodyPartDef bodyPart = null;
+
+
+        public CompProperties_HediffAfterHealthLoss()
+        {
+            this.compClass = typeof(CompHediffAfterHealthLoss);
+        }
+
+
+    }
+}
