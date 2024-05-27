d90c5363269ac9f16bee41c3c6a2b765aea030e6
juanosarg <31105173+juanosarg@users.noreply.github.com>
2024-05-27 22:28:45 +0200

    Added DeathActionWorker_ConfigurableExplosion

---
+++ Source/VFECore/AnimalBehaviours/DeathActionWorkers/DeathActionWorker_ConfigurableExplosion.cs
@@ -0,0 +1,23 @@
+﻿using RimWorld;
+using Verse;
+using Verse.AI.Group;
+
+namespace AnimalBehaviours
+{
+    public class DeathActionWorker_ConfigurableExplosion : DeathActionWorker
+    {
+
+        public DeathActionProperties_ConfigurableExplosion Props => (DeathActionProperties_ConfigurableExplosion)props;
+
+        public override RulePackDef DeathRules => RulePackDefOf.Transition_DiedExplosive;
+
+        public override bool DangerousInMelee => true;
+
+        public override void PawnDied(Corpse corpse, Lord prevLord)
+        {
+            GenExplosion.DoExplosion(radius: (corpse.InnerPawn.ageTracker.CurLifeStageIndex == 0) ? Props.babyExplosionRadius : ((corpse.InnerPawn.ageTracker.CurLifeStageIndex != 1) 
+                ? Props.adultExplosionRadius : Props.juvenileExplosionRadius), center: corpse.Position, map: corpse.Map, damType: Props.damageDef, instigator: corpse.InnerPawn, damAmount: Props.damAmount,
+                armorPenetration: Props.armorPenetration,explosionSound: Props.explosionSound);
+        }
+    }
+}
---
+++ Source/VFECore/AnimalBehaviours/DeathActionWorkers/Properties/DeathActionProperties_ConfigurableExplosion.cs
@@ -0,0 +1,23 @@
+﻿using RimWorld;
+using Verse;
+
+namespace AnimalBehaviours
+{
+    public class DeathActionProperties_ConfigurableExplosion : DeathActionProperties
+    {
+        public float babyExplosionRadius = 1.9f;
+        public float juvenileExplosionRadius = 2.9f;
+        public float adultExplosionRadius = 4.9f;
+
+        public DamageDef damageDef;
+
+        public int damAmount = -1;
+        public int armorPenetration = 0;
+        public SoundDef explosionSound = null;
+
+        public DeathActionProperties_ConfigurableExplosion()
+        {
+            workerClass = typeof(DeathActionWorker_ConfigurableExplosion);
+        }
+    }
+}
