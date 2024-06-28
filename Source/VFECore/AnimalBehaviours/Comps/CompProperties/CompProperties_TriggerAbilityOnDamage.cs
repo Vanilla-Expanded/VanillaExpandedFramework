86857264ba86ca1194ff8d52210f7167c462a61c
juanosarg <31105173+juanosarg@users.noreply.github.com>
2024-06-28 11:41:10 +0200

    Adding CompProperties_TriggerAbilityOnDamage

---
+++ Source/VFECore/AnimalBehaviours/Comps/CompProperties/CompProperties_TriggerAbilityOnDamage.cs
@@ -0,0 +1,22 @@
+﻿
+using RimWorld;
+using Verse;
+
+namespace AnimalBehaviours
+{
+    public class CompProperties_TriggerAbilityOnDamage : CompProperties
+    {
+
+        public AbilityDef ability;
+
+        public float minDamageToTrigger = 0;
+
+
+        public CompProperties_TriggerAbilityOnDamage()
+        {
+            this.compClass = typeof(CompTriggerAbilityOnDamage);
+        }
+
+
+    }
+}
---
+++ Source/VFECore/AnimalBehaviours/Comps/CompTriggerAbilityOnDamage.cs
@@ -0,0 +1,39 @@
+﻿using RimWorld;
+using Verse;
+
+namespace AnimalBehaviours
+{
+    class CompTriggerAbilityOnDamage : ThingComp
+    {
+
+
+        public CompProperties_TriggerAbilityOnDamage Props
+        {
+            get
+            {
+                return (CompProperties_TriggerAbilityOnDamage)this.props;
+            }
+        }
+
+
+        public override void PostPostApplyDamage(DamageInfo dinfo, float totalDamageDealt)
+        {
+           
+            Pawn pawn = this.parent as Pawn;
+          
+            if (pawn != null && totalDamageDealt >= Props.minDamageToTrigger)
+            {
+            
+                Ability ability = pawn.abilities?.GetAbility(Props.ability);
+                if (ability != null && !ability.OnCooldown)
+                {
+                    ability.QueueCastingJob(dinfo.Instigator, dinfo.Instigator);
+                  
+
+                }
+            }
+
+        }
+
+    }
+}
