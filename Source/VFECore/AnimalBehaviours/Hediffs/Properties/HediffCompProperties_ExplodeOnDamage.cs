1700f732a3dba2294cc41c249ffc525f02d5c00e
juanosarg <31105173+juanosarg@users.noreply.github.com>
2024-07-04 13:40:10 +0200

    HediffComp_ExplodeOnDamage

---
+++ Source/VFECore/AnimalBehaviours/Hediffs/Properties/HediffCompProperties_ExplodeOnDamage.cs
@@ -0,0 +1,25 @@
+﻿
+using Verse;
+using RimWorld;
+
+
+namespace AnimalBehaviours
+{
+    public class HediffCompProperties_ExplodeOnDamage : HediffCompProperties
+    {
+        public int minDamageToExplode;
+
+        public DamageDef damageType;
+        public int damageAmount = -1;
+        public float radius;
+        public SoundDef sound = null;
+        public ThingDef spawnThingDef = null;
+        public float spawnThingChance = 0f;
+      
+
+        public HediffCompProperties_ExplodeOnDamage()
+        {
+            this.compClass = typeof(HediffComp_ExplodeOnDamage);
+        }
+    }
+}
