c54e1a53d15a9cb9d2c3329f27a71366a13aae90
juanosarg <31105173+juanosarg@users.noreply.github.com>
2024-07-04 11:53:55 +0200

    HediffComp_ExplodeOnFire

---
+++ Source/VFECore/AnimalBehaviours/Hediffs/HediffComp_ExplodeOnFire.cs
@@ -0,0 +1,64 @@
+﻿
+using Verse;
+using RimWorld;
+
+
+namespace AnimalBehaviours
+{
+    public class HediffComp_ExplodeOnFire : HediffComp
+    {
+        public bool onCoolDown = false;
+        public int coolDownCounter = 0;
+
+        public HediffCompProperties_ExplodeOnFire Props
+        {
+            get
+            {
+                return (HediffCompProperties_ExplodeOnFire)this.props;
+            }
+        }
+
+        public override void CompExposeData()
+        {
+            base.CompExposeData();
+            Scribe_Values.Look<bool>(ref this.onCoolDown, "onCoolDown", false, true);
+            Scribe_Values.Look<int>(ref this.coolDownCounter, "coolDownCounter", 0, false);
+
+        }
+
+        public override void CompPostTick(ref float severityAdjustment)
+        {
+            base.CompPostTick(ref severityAdjustment);
+
+            if (!onCoolDown && this.parent.pawn.IsHashIntervalTick(Props.checkInterval) && this.parent.pawn.Map!=null && this.parent.pawn.IsBurning()) {
+
+                Fire fire = (Fire)this.parent.pawn.GetAttachment(ThingDefOf.Fire);
+
+                if (fire.fireSize >= Props.minFireToExplode)
+                {
+                    GenExplosion.DoExplosion(this.parent.pawn.Position, this.parent.pawn.Map, Props.radius, Props.damageType, this.parent.pawn,Props.damageAmount, -1, null, null, null, null, null, 0f, 1, null, false, null, 0f, 1);
+                    onCoolDown = true;
+
+                }
+
+            }
+
+            if (onCoolDown)
+            {
+                coolDownCounter++;
+                if (coolDownCounter > Props.ticksToRecheck)
+                {
+                    onCoolDown = false;
+                }
+
+
+            }
+
+
+        }
+
+
+
+
+    }
+}
---
+++ Source/VFECore/AnimalBehaviours/Hediffs/Properties/HediffCompProperties_ExplodeOnFire.cs
@@ -0,0 +1,24 @@
+﻿
+using Verse;
+using RimWorld;
+
+
+namespace AnimalBehaviours
+{
+    public class HediffCompProperties_ExplodeOnFire : HediffCompProperties
+    {
+        public int minFireToExplode;
+
+        public DamageDef damageType;
+        public int damageAmount = -1;
+        public float radius;
+        public int ticksToRecheck;
+
+        public int checkInterval = 300;
+
+        public HediffCompProperties_ExplodeOnFire()
+        {
+            this.compClass = typeof(HediffComp_ExplodeOnFire);
+        }
+    }
+}
