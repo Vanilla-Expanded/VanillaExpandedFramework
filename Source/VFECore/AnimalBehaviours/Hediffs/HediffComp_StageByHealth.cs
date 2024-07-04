6508f36c49434f0cdae37741017860f2a35ccff3
juanosarg <31105173+juanosarg@users.noreply.github.com>
2024-07-04 14:09:59 +0200

    HediffComp_StageByHealth

---
+++ Source/VFECore/AnimalBehaviours/Hediffs/HediffComp_StageByHealth.cs
@@ -0,0 +1,44 @@
+﻿
+
+using RimWorld;
+using System.Collections.Generic;
+using Verse;
+using System.Linq;
+using Verse.Sound;
+using UnityEngine;
+
+namespace AnimalBehaviours
+{
+    class HediffComp_StageByHealth : HediffComp
+    {
+
+
+        public HediffCompProperties_StageByHealth Props
+        {
+            get
+            {
+                return (HediffCompProperties_StageByHealth)this.props;
+            }
+        }
+
+
+        public override void CompPostTick(ref float severityAdjustment)
+        {
+            base.CompPostTick(ref severityAdjustment);
+
+            if (this.parent.pawn.IsHashIntervalTick(500))
+            {
+                if (this.parent.pawn.Map != null && this.parent.pawn.health.summaryHealth.SummaryHealthPercent>=Props.healthThreshold)
+                {
+                    this.parent.Severity = Props.highHealthStageIndex;
+                }
+                else this.parent.Severity = Props.lowHealthStageIndex;
+            }
+
+
+        }
+
+
+
+    }
+}
---
+++ Source/VFECore/AnimalBehaviours/Hediffs/Properties/HediffCompProperties_StageByHealth.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Verse;
+using System.Collections.Generic;
+
+
+namespace AnimalBehaviours
+{
+    public class HediffCompProperties_StageByHealth : HediffCompProperties
+    {
+
+        public float lowHealthStageIndex = 0.1f;
+        public float highHealthStageIndex = 1f;
+        public float healthThreshold = 0.5f;
+
+        public HediffCompProperties_StageByHealth()
+        {
+            this.compClass = typeof(HediffComp_StageByHealth);
+        }
+    }
+}
