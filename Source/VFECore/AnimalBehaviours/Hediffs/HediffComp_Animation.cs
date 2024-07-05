5d6f94d6e1123cb06c916e34bd9de38a16bdf6b0
juanosarg <31105173+juanosarg@users.noreply.github.com>
2024-07-05 09:46:30 +0200

    HediffComp_Animation

---
+++ Source/VFECore/AnimalBehaviours/Hediffs/HediffComp_Animation.cs
@@ -0,0 +1,61 @@
+﻿
+using Verse;
+using RimWorld;
+
+namespace AnimalBehaviours
+{
+    class HediffComp_Animation : HediffComp
+    {
+
+
+        public HediffCompProperties_Animation Props
+        {
+            get
+            {
+                return (HediffCompProperties_Animation)this.props;
+            }
+        }
+
+
+
+        public override void CompPostPostAdd(DamageInfo? dinfo)
+        {
+
+            this.parent.pawn.Drawer.renderer.SetAnimation(Props.animation);
+
+        }
+
+        public override void CompPostTick(ref float severityAdjustment)
+        {
+            base.CompPostTick(ref severityAdjustment);
+            if (!this.parent.pawn.Drawer.renderer.HasAnimation)
+            {
+                this.parent.pawn.Drawer.renderer.SetAnimation(Props.animation);
+            }
+            if (Props.shamblerParticles && Rand.MTBEventOccurs(1f, 60f, 1f))
+            {
+                FleckMaker.ThrowShamblerParticles(this.parent.pawn);
+            }
+        }
+
+        public override void CompPostPostRemoved()
+        {
+            this.parent.pawn.Drawer.renderer.SetAnimation(null);
+
+        }
+
+        public override void Notify_PawnDied(DamageInfo? dinfo, Hediff culprit = null)
+        {
+            this.parent.pawn.Drawer.renderer.SetAnimation(null);
+
+        }
+
+        public override void Notify_PawnKilled()
+        {
+            this.parent.pawn.Drawer.renderer.SetAnimation(null);
+
+        }
+
+
+    }
+}
---
+++ Source/VFECore/AnimalBehaviours/Hediffs/Properties/HediffCompProperties_Animation.cs
@@ -0,0 +1,17 @@
+﻿using Verse;
+
+namespace AnimalBehaviours
+{
+    public class HediffCompProperties_Animation : HediffCompProperties
+    {
+
+        public AnimationDef animation;
+        public bool shamblerParticles = false;
+
+        public HediffCompProperties_Animation()
+        {
+            this.compClass = typeof(HediffComp_Animation);
+        }
+    }
+}
+
