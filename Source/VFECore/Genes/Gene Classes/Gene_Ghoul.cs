3fe440dddfaf32d04e53ec418c92033c5fa0a26f
juanosarg <31105173+juanosarg@users.noreply.github.com>
2024-07-09 08:27:25 +0200

    Gene_Ghoul and Gene_Shambler

---
+++ Source/VFECore/Genes/Gene Classes/Gene_Ghoul.cs
@@ -0,0 +1,22 @@
+﻿using HarmonyLib;
+using System.Collections.Generic;
+using Verse;
+
+namespace VanillaGenesExpanded
+{
+    public class Gene_Ghoul : Gene
+    {
+       
+        public override bool Active
+        {
+            get
+            {
+                if (!pawn.IsGhoul)
+                {
+                    return false;
+                }
+                return base.Active;
+            }
+        }
+    }
+}
---
+++ Source/VFECore/Genes/Gene Classes/Gene_Shambler.cs
@@ -0,0 +1,21 @@
+﻿using HarmonyLib;
+using System.Collections.Generic;
+using Verse;
+
+namespace VanillaGenesExpanded
+{
+    public class Gene_Shambler : Gene
+    {
+        public override bool Active
+        {
+            get
+            {
+                if (!pawn.IsShambler)
+                {
+                    return false;
+                }
+                return base.Active;
+            }
+        }
+    }
+}
