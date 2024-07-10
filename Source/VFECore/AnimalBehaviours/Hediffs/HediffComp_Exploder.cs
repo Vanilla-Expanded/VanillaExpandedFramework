1b995a6d88a9274d840aecd986983e4c5b853fdc
juanosarg <31105173+juanosarg@users.noreply.github.com>
2024-07-10 15:53:16 +0200

    Null check for HediffComp_Exploder

--- Source/VFECore/AnimalBehaviours/Hediffs/HediffComp_Exploder.cs
+++ Source/VFECore/AnimalBehaviours/Hediffs/HediffComp_Exploder.cs
@@ -21,8 +21,10 @@ namespace AnimalBehaviours
 
         public override void Notify_PawnDied(DamageInfo? dinfo, Hediff culprit = null)
         {
-
-            GenExplosion.DoExplosion(this.parent.pawn.Corpse.Position, this.parent.pawn.Corpse.Map, Props.explosionForce, Props.damageDef, this.parent.pawn.Corpse, -1, -1, null, null, null, null, null, 0f, 1, null, false, null, 0f, 1);
+            Corpse corpse = this.parent.pawn?.Corpse;
+            if (corpse != null && corpse.Map != null) {
+                GenExplosion.DoExplosion(this.parent.pawn.Corpse.Position, this.parent.pawn.Corpse.Map, Props.explosionForce, Props.damageDef, this.parent.pawn.Corpse, -1, -1, null, null, null, null, null, 0f, 1, null, false, null, 0f, 1);
+            }
 
 
         }
