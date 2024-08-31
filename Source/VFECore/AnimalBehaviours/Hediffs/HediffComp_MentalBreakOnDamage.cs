29b992c93a6dca0e38a21b4363043d66401bb7ac
juanosarg <31105173+juanosarg@users.noreply.github.com>
2024-08-31 11:21:01 +0200

    Added HediffCompProperties_MentalBreakOnDamage

---
+++ Source/VFECore/AnimalBehaviours/Hediffs/HediffComp_MentalBreakOnDamage.cs
@@ -0,0 +1,39 @@
+﻿
+using Verse;
+using RimWorld;
+
+
+namespace AnimalBehaviours
+{
+    public class HediffComp_MentalBreakOnDamage : HediffComp
+    {
+
+
+        public HediffCompProperties_MentalBreakOnDamage Props
+        {
+            get
+            {
+                return (HediffCompProperties_MentalBreakOnDamage)this.props;
+            }
+        }
+
+        public override void Notify_PawnPostApplyDamage(DamageInfo dinfo, float totalDamageDealt)
+        {
+            base.Notify_PawnPostApplyDamage(dinfo, totalDamageDealt);
+
+            if (dinfo.Def == Props.damageTypeReceived && this.parent.pawn.Map != null)
+            {
+                this.parent.pawn.mindState.mentalBreaker.TryDoMentalBreak(Props.reason.Translate(),Props.mentalBreak);
+
+            }
+
+        }
+
+
+
+
+
+
+
+    }
+}
---
+++ Source/VFECore/AnimalBehaviours/Hediffs/Properties/HediffCompProperties_MentalBreakOnDamage.cs
@@ -0,0 +1,22 @@
+﻿
+using Verse;
+using RimWorld;
+
+
+namespace AnimalBehaviours
+{
+    public class HediffCompProperties_MentalBreakOnDamage : HediffCompProperties
+    {
+       
+        public DamageDef damageTypeReceived;
+        public MentalBreakDef mentalBreak;
+        public string reason;
+     
+      
+
+        public HediffCompProperties_MentalBreakOnDamage()
+        {
+            this.compClass = typeof(HediffComp_MentalBreakOnDamage);
+        }
+    }
+}
