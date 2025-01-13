2998dbf608df6d57eaa9c2ff276af995d88aa12d
Taranchuk <nfhfyxer@gmail.com>
2025-01-13 14:03:11 +0200

    bouncing arrows position and altitude fixes

--- Source/VFECore/VFECore/Comps/ThingComps/CompBouncingArrow.cs
+++ Source/VFECore/VFECore/Comps/ThingComps/CompBouncingArrow.cs
@@ -23,14 +23,14 @@ namespace VFECore
                 var progress = Find.TickManager.TicksGame % 1000;
                 if (progress < 500) progress = 1000 - progress;
 
-                var pos = Vector3.forward * (1f + progress / 1000f);
-                pos.y = AltitudeLayer.MetaOverlays.AltitudeFor();
+                var pos = parent.DrawPos + (Vector3.forward * (1f + progress / 1000f));
+                pos.y = AltitudeLayer.FogOfWar.AltitudeFor() + 1;
                 var opacity = 1f - progress / 2000f;
                 var rotation = Quaternion.AngleAxis(180, Vector3.up);
 
                 ArrowMatWhite.color = new(1f, 1f, 1f, opacity);
 
-                Graphics.DrawMesh(MeshPool.plane10, this.parent.DrawPos + pos, rotation, ArrowMatWhite, 0);
+                Graphics.DrawMesh(MeshPool.plane10, pos, rotation, ArrowMatWhite, 0);
             }
         }
     }
