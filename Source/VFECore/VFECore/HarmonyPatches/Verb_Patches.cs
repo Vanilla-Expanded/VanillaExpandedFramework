e69a99a2d0f4cbc810e85eb126a774c7129fcf3e
Taranchuk <nfhfyxer@gmail.com>
2025-01-18 10:06:25 +0200

    melee auto attack fix

--- Source/VFECore/VFECore/HarmonyPatches/Verb_Patches.cs
+++ Source/VFECore/VFECore/HarmonyPatches/Verb_Patches.cs
@@ -241,43 +241,17 @@ namespace VFECore
         }
     }
 
-    [HarmonyPatch]
+    [HarmonyPatch(typeof(Verb), "TryFindShootLineFromTo")]
     public static class Verb_TryFindShootLineFromTo_Patch
     {
-        public static MethodBase TargetMethod()
+        public static void Prefix(Verb __instance)
         {
-            var tryFindShootLineFromToMethod = AccessTools.Method(typeof(Verb), "TryFindShootLineFromTo");
-            return tryFindShootLineFromToMethod;
+            ReachabilityImmediate_CanReachImmediate_Patch.curPawn = __instance.CasterPawn;
         }
 
-        public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
+        public static void Postfix()
         {
-            var codes = instructions.ToList();
-            var label = generator.DefineLabel();
-            for (var i = 0; i < codes.Count; i++)
-            {
-                if (codes[i].opcode == OpCodes.Brtrue_S && codes[i - 1].Calls(AccessTools.Method(typeof(VerbProperties), "get_IsMeleeAttack")))
-                {
-                    codes[i + 1].labels.Add(label);
-                    yield return new CodeInstruction(OpCodes.Brfalse_S, label);
-                    yield return new CodeInstruction(OpCodes.Ldarg_0);
-                    yield return new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(Verb_TryFindShootLineFromTo_Patch), nameof(IsVanillaMeleeAttack)));
-                    yield return new CodeInstruction(OpCodes.Brtrue_S, codes[i].operand);
-                }
-                else
-                {
-                    yield return codes[i];
-                }
-            }
-        }
-
-        public static bool IsVanillaMeleeAttack(Verb verb)
-        {
-            if (verb.Caster is Pawn pawn && pawn.GetMeleeReachRange(verb) > ShootTuning.MeleeRange)
-            {
-                return false;
-            }
-            return true;
+            ReachabilityImmediate_CanReachImmediate_Patch.curPawn = null;
         }
     }
 
@@ -321,21 +295,47 @@ namespace VFECore
         }
     }
 
+    [HarmonyPatch(typeof(AttackTargetFinder), "FindBestReachableMeleeTarget")]
+    public static class AttackTargetFinder_FindBestReachableMeleeTarget_Patch
+    {
+        public static void Prefix(Pawn searcherPawn)
+        {
+            ReachabilityImmediate_CanReachImmediate_Patch.curPawn = searcherPawn;
+        }
+
+        public static void Postfix()
+        {
+            ReachabilityImmediate_CanReachImmediate_Patch.curPawn = null;
+        }
+    }
+
+    [HarmonyPatch(typeof(JobGiver_ConfigurableHostilityResponse), "TryGetAttackNearbyEnemyJob")]
+    public static class JobGiver_ConfigurableHostilityResponse_TryGetAttackNearbyEnemyJob_Patch
+    {
+        public static void Prefix(Pawn pawn)
+        {
+            ReachabilityImmediate_CanReachImmediate_Patch.curPawn = pawn;
+        }
+
+        public static void Postfix()
+        {
+            ReachabilityImmediate_CanReachImmediate_Patch.curPawn = null;
+        }
+    }
+
     [HarmonyPatch]
     public static class Toils_Combat_FollowAndMeleeAttack_Patch
     {
         public static FieldInfo targetInd;
 
-        public static Pawn curPawn;
-
         public static void Prefix(Toil ___followAndAttack)
         {
-            curPawn = ___followAndAttack.actor;
+            ReachabilityImmediate_CanReachImmediate_Patch.curPawn = ___followAndAttack.actor;
         }
 
         public static void Postfix()
         {
-            curPawn = null;
+            ReachabilityImmediate_CanReachImmediate_Patch.curPawn = null;
         }
 
         public static MethodBase TargetMethod()
@@ -405,13 +405,14 @@ namespace VFECore
     [HarmonyPatch(typeof(ReachabilityImmediate), nameof(ReachabilityImmediate.CanReachImmediate), new Type[] { typeof(IntVec3), typeof(LocalTargetInfo), typeof(Map), typeof(PathEndMode), typeof(Pawn) })]
     public static class ReachabilityImmediate_CanReachImmediate_Patch
     {
+        public static Pawn curPawn;
+
         public static void Postfix(ref bool __result, IntVec3 start, LocalTargetInfo target, Map map, PathEndMode peMode, Pawn pawn)
         {
-            if (__result is false && Toils_Combat_FollowAndMeleeAttack_Patch.curPawn != null)
+            if (__result is false && curPawn != null)
             {
-                var actor = Toils_Combat_FollowAndMeleeAttack_Patch.curPawn;
-                var verbToUse = actor.GetMeleeVerb();
-                var meleeReachRange = actor.GetMeleeReachRange(verbToUse);
+                var verbToUse = curPawn.GetMeleeVerb();
+                var meleeReachRange = curPawn.GetMeleeReachRange(verbToUse);
                 var distance = target.Cell.DistanceTo(start);
                 __result = distance <= meleeReachRange && GenSight.LineOfSight(start, target.Cell, map);
             }
@@ -422,4 +423,57 @@ namespace VFECore
             return pawn.jobs.curJob?.verbToUse ?? pawn.equipment?.PrimaryEq?.PrimaryVerb;
         }
     }
+
+    [HarmonyPatch(typeof(JobDriver_Wait), "CheckForAutoAttack")]
+    public static class JobDriver_Wait_CheckForAutoAttack_Patch
+    {
+        public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
+        {
+            var codes = instructions.ToList();
+            var label = generator.DefineLabel();
+            for (var i = 0; i < codes.Count; i++)
+            {
+                if (codes[i].opcode == OpCodes.Brtrue_S && codes[i - 1].Calls(AccessTools.Method(typeof(VerbProperties), "get_IsMeleeAttack")))
+                {
+                    codes[i + 1].labels.Add(label);
+                    yield return new CodeInstruction(OpCodes.Brfalse_S, label);
+                    yield return new CodeInstruction(OpCodes.Ldloc_S, 9);
+                    yield return new CodeInstruction(OpCodes.Call, 
+                        AccessTools.Method(typeof(JobDriver_Wait_CheckForAutoAttack_Patch), nameof(IsVanillaMeleeAttack)));
+                    yield return new CodeInstruction(OpCodes.Brtrue_S, codes[i].operand);
+                }
+                else
+                {
+                    yield return codes[i];
+                }
+            }
+        }
+
+        public static bool IsVanillaMeleeAttack(Verb verb)
+        {
+            if (verb.Caster is Pawn pawn && pawn.GetMeleeReachRange(verb) > ShootTuning.MeleeRange)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+
+    [HarmonyPatch(typeof(AttackTargetFinder), "BestAttackTarget")]
+    public static class AttackTargetFinder_BestAttackTarget_Patch
+    {
+        public static void Prefix(IAttackTargetSearcher searcher, ref float maxDist)
+        {
+            if (searcher is Pawn pawn)
+            {
+                var verbToUse = pawn.GetMeleeVerb();
+                var meleeReachRange = pawn.GetMeleeReachRange(verbToUse);
+                if (meleeReachRange > ShootTuning.MeleeRange)
+                {
+                    maxDist = Mathf.Max(maxDist, meleeReachRange);
+                }
+            }
+        }
+    }
 }
+
