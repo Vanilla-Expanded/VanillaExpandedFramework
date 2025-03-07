12ed8bfb25c8d8cf4d6a4a54720a6b134cfd3de2
Simon Latusek <Simon.Latusek@gmail.com>
2025-03-07 16:01:41 +0100

    adjusting VCE ingredients no-mix patch

--- Source/VFECore/Cuisine/Harmony/WorkGiver_DoBill_TryFindBestIngredientsInSet_NoMixHelper_Patch.cs
+++ Source/VFECore/Cuisine/Harmony/WorkGiver_DoBill_TryFindBestIngredientsInSet_NoMixHelper_Patch.cs
@@ -9,15 +9,18 @@ namespace VanillaCookingExpanded
     using RimWorld;
     using Verse;
 
+    [HarmonyDebug]
     [HarmonyPatch(typeof(WorkGiver_DoBill), "TryFindBestIngredientsInSet_NoMixHelper")]
     public static class WorkGiver_DoBill_TryFindBestIngredientsInSet_NoMixHelper_Patch
     {
-        private static bool adjust;
+        private static          bool              adjust;
+        private static readonly HashSet<ThingDef> alreadyUsed = [];
 
         public static void Prefix(Bill bill)
         {
             Recipe_Extension extension = bill?.recipe?.GetModExtension<Recipe_Extension>();
             adjust = extension?.individualIngredients ?? false;
+            alreadyUsed.Clear();
         }
 
         public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator ilg)
@@ -34,13 +37,28 @@ namespace VanillaCookingExpanded
                 yield return instruction;
                 if (instruction.Calls(addToListInfo))
                 {
+                    yield return CodeInstruction.LoadField(typeof(WorkGiver_DoBill_TryFindBestIngredientsInSet_NoMixHelper_Patch), nameof(alreadyUsed));
+                    yield return CodeInstruction.LoadArgument(0);
+                    yield return CodeInstruction.LoadLocal(7);
+                    yield return CodeInstruction.Call(typeof(List<Thing>), "get_Item");
+                    yield return CodeInstruction.LoadField(typeof(Thing), nameof(Thing.def));
+                    yield return CodeInstruction.Call(typeof(HashSet<ThingDef>), nameof(HashSet<ThingDef>.Add));
+                    yield return new CodeInstruction(OpCodes.Pop);
+
+                }
+                if(instruction.opcode == OpCodes.Bne_Un)
+                {
                     yield return CodeInstruction.LoadField(typeof(WorkGiver_DoBill_TryFindBestIngredientsInSet_NoMixHelper_Patch), nameof(adjust));
                     yield return new CodeInstruction(OpCodes.Brfalse, skip);
-                    yield return CodeInstruction.LoadField(typeof(WorkGiver_DoBill), "availableCounts");
-                    yield return CodeInstruction.LoadLocal(5);
-                    yield return new CodeInstruction(OpCodes.Ldc_R4, 0f);
-                    yield return CodeInstruction.Call(AccessTools.Inner(typeof(WorkGiver_DoBill), "DefCountList"), "SetCount");
 
+                    yield return CodeInstruction.LoadField(typeof(WorkGiver_DoBill_TryFindBestIngredientsInSet_NoMixHelper_Patch), nameof(alreadyUsed));
+                    yield return CodeInstruction.LoadArgument(0);
+                    yield return CodeInstruction.LoadLocal(7);
+                    yield return CodeInstruction.Call(typeof(List<Thing>), "get_Item");
+                    
+                    yield return CodeInstruction.LoadField(typeof(Thing), nameof(Thing.def));
+                    yield return CodeInstruction.Call(typeof(HashSet<ThingDef>), nameof(HashSet<ThingDef>.Contains));
+                    yield return new CodeInstruction(OpCodes.Brtrue, instruction.operand);
                     yield return new CodeInstruction(OpCodes.Nop) { labels = [skip] };
                 }
             }
