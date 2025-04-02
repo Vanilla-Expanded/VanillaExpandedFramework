9c0ba44c73d4e56414b43aa2666bc20b06ae96d2
juanosarg <31105173+juanosarg@users.noreply.github.com>
2025-04-02 15:54:03 +0200

    Moved HiddenDesignatorsDef  to the VE Framework

---
+++ Source/VFECore/Furniture/DefOfs/HiddenDesignatorsDef.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using RimWorld;
+using Verse;
+
+namespace VanillaFurnitureExpanded
+{
+    public class HiddenDesignatorsDef : Def
+    {
+
+        public List<BuildableDef> hiddenDesignators;
+
+
+    }
+}
---
+++ Source/VFECore/Furniture/Harmony/DesignationCategoryDef_ResolvedAllowedDesignators.cs
@@ -0,0 +1,48 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System;
+using System.Collections.Generic;
+using Verse;
+using RimWorld.BaseGen;
+
+
+
+namespace VanillaFurnitureExpanded
+{
+
+
+    [HarmonyPatch(typeof(DesignationCategoryDef))]
+    [HarmonyPatch("ResolvedAllowedDesignators", MethodType.Getter)]
+
+
+    public static class VanillaFurnitureExpanded_DesignationCategoryDef_ResolvedAllowedDesignators_Patch
+    {
+
+
+        [HarmonyPostfix]
+        public static IEnumerable<Designator> AllowBuild(IEnumerable<Designator> values)
+
+        {
+            IEnumerable<Designator> designators = values;
+
+            foreach (Designator designator in designators)
+            {
+                Designator_Build designator_build = designator as Designator_Build;
+
+                if (designator_build == null || !StaticCollections.hidden_designators.Contains(designator_build.PlacingDef))
+                {
+                    yield return designator;
+                }
+
+
+            }
+
+
+
+
+        }
+
+    }
+
+
+}
