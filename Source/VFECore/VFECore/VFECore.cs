6cf72e60370751fdd46e9dd74b30e3e710e89445
SokyranTheDragon <exploder1531@gmail.com>
2025-04-23 18:20:46 +0200

    Use Harmony categories for patching
    - `PatchAll` call was replaced with `PatchAllUncategorized`
      - Basically, there's not going to be any change at all as it is (only if we add any harmony patches with `HarmonyPatchCategory` attribute)
    - Added a string constant `LateHarmonyPatchCategory`
    - Use `PatchCategory(LateHarmonyPatchCategory)` to execute all patches using that category
      - The patching is delayed by using `LongEventHandler.ExecuteWhenFinished` to ensure that methods such as `Prepare` will be called when defs are loaded and can be accessed
      - This basically allows us to disable specific patches if there's no defs in game using a specific comp/mod extension/Thing subtype/etc.

--- Source/VFECore/VFECore/VFECore.cs
+++ Source/VFECore/VFECore/VFECore.cs
@@ -12,10 +12,15 @@ namespace VFECore
 
     public class VFECore : Mod
     {
+        public const string LateHarmonyPatchCategory = "LateHarmonyPatch";
+
         public VFECore(ModContentPack content) : base(content)
         {
             harmonyInstance = new Harmony("OskarPotocki.VFECore");
-            harmonyInstance.PatchAll();
+            harmonyInstance.PatchAllUncategorized();
+            // Delay running patches that still need to wait, for example ones that rely on
+            // some condition to run (like defs having a comp or def mod extension).
+            LongEventHandler.ExecuteWhenFinished(() => harmonyInstance.PatchCategory(LateHarmonyPatchCategory));
         }
 
         public static Harmony harmonyInstance;
