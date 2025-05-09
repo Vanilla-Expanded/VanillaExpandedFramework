42169d7c768944c342be892aba8c29a05b9ec655
SokyranTheDragon <exploder1531@gmail.com>
2025-05-09 19:45:40 +0200

    Added CompScheduleActiveAtSunlightRange
    It extends the vanilla CompSchedule (and should be compatible with stuff that checks for it, like `CompAmbientSound`).
    
    On top of that, it should work with either of ticker types (but normal ticker will still re-check every 250 ticks like rare ticker).
    
    The method `ShouldBeAllowed` is virtual, so other types extending this one can add extra checks if needed for anything.

---
+++ Source/VFECore/VFECore/Comps/ThingComps/CompProperties_ScheduleActiveAtSunlightRange.cs
@@ -0,0 +1,11 @@
+﻿using RimWorld;
+
+namespace VFECore;
+
+public class CompProperties_ScheduleActiveAtSunlightRange : CompProperties_Schedule
+{
+    public float minLight;
+    public float maxLight = 1f;
+
+    public CompProperties_ScheduleActiveAtSunlightRange() => compClass = typeof(CompScheduleActiveAtSunlightRange);
+}
