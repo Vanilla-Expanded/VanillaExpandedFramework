470f2f9d9c925873d2f228379af37a591cbe3723
juanosarg <31105173+juanosarg@users.noreply.github.com>
2025-05-15 16:42:32 +0200

    LootableBuildings

---
+++ Source/VFECore/Furniture/Buildings/LootableBuilding.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using Verse;
+using RimWorld;
+using VanillaFurnitureExpanded;
+
+
+namespace VanillaFurnitureExpanded
+{
+    public class LootableBuilding : Building, IOpenable
+    {
+
+        public int OpenTicks => 300;
+        LootableBuildingDetails contentDetails;
+
+        public LootableBuildingDetails GetDetails()
+        {
+            if (contentDetails == null)
+            {
+                contentDetails = this.def.GetModExtension<LootableBuildingDetails>();
+            }
+            return contentDetails;
+        }
+
+        public bool CanOpen
+        {
+            get
+            {
+                GetDetails();
+                if (contentDetails?.requiredMod != "")
+                {
+                    return ModLister.HasActiveModWithName(contentDetails?.requiredMod);
+                }
+                else return true;
+            }
+        }
+
+        public void Open()
+        {
+            GetDetails();
+            if (contentDetails != null)
+            {
+                if (contentDetails.useBaseGameOpen)
+                {
+                    if (contentDetails.randomFromContents)
+                    {
+                        for(int i=0;i< contentDetails.totalRandomLoops; i++)
+                        {
+                            ThingAndCount thingDefCount = contentDetails.contents.RandomElement();
+                            Thing thingToMake = ThingMaker.MakeThing(thingDefCount.thing, null);
+                            thingToMake.stackCount = thingDefCount.count;
+                            thingToMake.TryGetComp<CompQuality>()?.SetQuality(QualityUtility.GenerateQualityRandomEqualChance(), ArtGenerationContext.Colony);
+                            GenPlace.TryPlaceThing(thingToMake, Position, Map, ThingPlaceMode.Near);
+                        }
+                        
+                    }
+                    else {
+
+                        foreach (ThingAndCount thingDefCount in contentDetails.contents)
+                        {
+                            Thing thingToMake = ThingMaker.MakeThing(thingDefCount.thing, null);
+                            thingToMake.stackCount = thingDefCount.count;
+                            thingToMake.TryGetComp<CompQuality>()?.SetQuality(QualityUtility.GenerateQualityRandomEqualChance(), ArtGenerationContext.Colony);
+                            GenPlace.TryPlaceThing(thingToMake, Position, Map, ThingPlaceMode.Near);
+                        }
+                    }
+
+                    
+                    if (contentDetails.buildingLeft != null)
+                    {
+                        Thing buildingToMake = GenSpawn.Spawn(ThingMaker.MakeThing(contentDetails.buildingLeft), Position, Map);
+
+                        if (buildingToMake.def.CanHaveFaction)
+                        {
+                            buildingToMake.SetFaction(this.Faction);
+                        }
+                    }
+                    if (this.Spawned)
+                    {
+                        this.Destroy();
+                    }
+                }
+
+
+            }
+
+
+        }
+
+    }
+}
---
+++ Source/VFECore/Furniture/DefModExtensions/LootableBuildingDetails.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using Verse;
+using RimWorld;
+
+namespace VanillaFurnitureExpanded
+{
+
+    public class LootableBuildingDetails : DefModExtension
+    {
+        public bool useBaseGameOpen = true;
+        public bool randomFromContents = false;
+        public int totalRandomLoops = 1;
+        public List<ThingAndCount> contents = null;
+        public ThingDef buildingLeft = null;
+        public SoundDef deconstructSound = null;
+        public string gizmoTexture;
+        public string gizmoText;
+        public string gizmoDesc;
+        public string requiredMod = "";
+      
+
+    }
+
+    public class ThingAndCount
+    {
+        public ThingDef thing;
+        public int count = 1;
+
+    }
+
+
+}
