b3023130036e737bacb9bf3d4735f508881b3e67
Taranchuk <nfhfyxer@gmail.com>
2025-06-02 20:13:12 +0300

    quests: character system fixes, custom description tweak

--- Source/VFECore/VFECore/Quests/QuestChainState.cs
+++ Source/VFECore/VFECore/Quests/QuestChainState.cs
@@ -6,12 +6,39 @@ namespace VFECore
 {
     public class QuestChainState : IExposable
     {
-        public Dictionary<string, Pawn> uniquePawnsByTag = new Dictionary<string, Pawn>();
+        private List<Pawn> deepSavedPawns = new List<Pawn>();
+        private Dictionary<string, Pawn> uniquePawnsByTag = new Dictionary<string, Pawn>();
 
         public void ExposeData()
         {
+            Scribe_Collections.Look(ref deepSavedPawns, "deepSavedPawns", LookMode.Deep);
             Scribe_Collections.Look(ref uniquePawnsByTag, "uniquePawnsByTag", LookMode.Value, LookMode.Reference);
+            if (Scribe.mode == LoadSaveMode.PostLoadInit)
+            {
+                deepSavedPawns ??= new List<Pawn>();
+                uniquePawnsByTag ??= new Dictionary<string, Pawn>();
+                deepSavedPawns.RemoveAll(p => p == null);
+                uniquePawnsByTag.RemoveAll(pair => pair.Value == null);
+            }
         }
+
+        public void StoreUniquePawn(string tag, Pawn pawn, bool deepSave)
+        {
+            if (!uniquePawnsByTag.ContainsKey(tag))
+            {
+                uniquePawnsByTag[tag] = pawn;
+            }
+            if (deepSave)
+            {
+                deepSavedPawns.Add(pawn);
+            }
+        }
+
+        public void RemoveFromDeepSave(Pawn pawn)
+        {
+            deepSavedPawns.Remove(pawn);
+        }
+
         public Pawn GetUniquePawn(string tag)
         {
             uniquePawnsByTag.TryGetValue(tag, out var pawn);
--- Source/VFECore/VFECore/Quests/QuestChainWorker.cs
+++ Source/VFECore/VFECore/Quests/QuestChainWorker.cs
@@ -1,12 +1,15 @@
 ﻿using System.Collections.Generic;
 using Verse;
 using RimWorld;
+using System.Text.RegularExpressions;
 
 namespace VFECore
 {
     public class QuestChainWorker
     {
         public QuestChainDef def;
+
+        private string _cachedDescription;
 
         public QuestChainState State =>
             GameComponent_QuestChains.Instance.GetStateFor(def);
@@ -26,21 +29,61 @@ namespace VFECore
             }
         }
 
-        public virtual Pawn CreateAndStoreUniquePawn(PawnKindDef kind, UniqueCharacterExtension ext, Faction faction = null)
+        public virtual Pawn CreateAndStoreUniquePawn(PawnKindDef kind, UniqueCharacterExtension ext)
         {
+            Faction faction = null;
+            if (kind.defaultFactionType != null)
+            {
+                faction = Find.FactionManager.FirstFactionOfDef(kind.defaultFactionType);
+            }
             Pawn pawn = PawnGenerator.GeneratePawn(kind, faction);
-            State.uniquePawnsByTag[ext.tag] = pawn;
+            Log.Message($"Creating unique pawn {pawn.Name} with faction {pawn.Faction?.def} for quest chain {def.defName} with tag {ext.tag}");
+            State.StoreUniquePawn(ext.tag, pawn, deepSave: true);
+            InvalidateDescriptionCache();
             return pawn;
         }
 
         public virtual Pawn GetUniquePawn(string tag)
         {
+            var pawn = State.GetUniquePawn(tag);
+            if (pawn != null)
+            {
+                return pawn;
+            }
+            EnsureAllUniquePawnsCreated();
             return State.GetUniquePawn(tag);
         }
 
         public virtual string GetDescription()
         {
-            return def.description;
+            if (_cachedDescription == null)
+            {
+                string desc = def.description;
+                _cachedDescription = Regex.Replace(desc, @"\[(\w+?)_(\w+?)\]", match =>
+                {
+                    string tag = match.Groups[1].Value;
+                    string property = match.Groups[2].Value;
+                    Pawn pawn = GetUniquePawn(tag);
+                    if (pawn == null) return match.Value;
+
+                    string value = property switch
+                    {
+                        "FullName" => pawn.Name?.ToStringFull ?? pawn.LabelCap,
+                        "ShortName" => pawn.Name?.ToStringShort ?? pawn.LabelShortCap,
+                        "Label" => pawn.LabelCap,
+                        _ => ""
+                    };
+
+                    if (string.IsNullOrEmpty(value)) return match.Value;
+                    return value.Colorize(PawnNameColorUtility.PawnNameColorOf(pawn));
+                });
+            }
+            return _cachedDescription;
+        }
+
+        public void InvalidateDescriptionCache()
+        {
+            _cachedDescription = null;
         }
     }
 }
