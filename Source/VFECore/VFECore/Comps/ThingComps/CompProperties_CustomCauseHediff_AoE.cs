3dc691bc54edc26e7ddbdf699f9cbdb6dff69d2e
SokyranTheDragon <exploder1531@gmail.com>
2025-06-07 00:38:41 +0200

    Add CompCustomCauseHediff_AoE and related types
    This is based off of the comp used for radios in VE Furniture - Core. It was significantly expanded and made more customizable (along with some VE Furniture specific components stripped away). VE Furniture - Core will need an update to use this comp specifically in the near future to avoid having 2 comps that do the same thing in the same way. This comp will is also used by VE Furniture - Spacer.
    
    CompCustomCauseHediff_AoE is basically a customized (and more customizable) version of vanilla's CompCauseHediff_AoE.
    
    PlaceWorker_CompCustomCauseHediff_AoE is used to display this comp's range, including options like same room only, inside/outside only, as well as limited and unlimited range (although unlimited range is still limited by GenRadial.MaxRadialPatternRadius).

---
+++ Source/VFECore/VFECore/Comps/ThingComps/CompProperties_CustomCauseHediff_AoE.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using Verse;
+
+namespace VFECore;
+
+public class CompProperties_CustomCauseHediff_AoE : CompProperties
+{
+    public List<PawnCapacityDef> requiredCapacities;
+    public HediffDef hediff;
+    public bool mustBeAwake = false;
+
+    public bool sameRoomOnly = true;
+    public float range;
+
+    public bool allowHumanlike = true;
+    public bool allowInsects = false;
+    public bool allowDryads = false;
+    public bool allowAnimals = false;
+    public bool allowMechanoids = false;
+    public bool allowEntities = false;
+
+    public bool worksInside = true;
+    public bool worksOutside = true;
+
+    public float startingSeverity = 1f;
+    // Used in combination with HediffComp_Disappears
+    public int checkInterval = 10;
+    // To be constant it should be longer by a few ticks than checkInterval.
+    public int hediffDuration = 15;
+
+    // In case someone wants to do weird stuff.
+    protected virtual bool LogWorksBothInsideAndOutsideFieldAreFalse => true;
+
+    public CompProperties_CustomCauseHediff_AoE() => compClass = typeof(CompCustomCauseHediff_AoE);
+
+    public override IEnumerable<string> ConfigErrors(ThingDef parentDef)
+    {
+        foreach (var error in base.ConfigErrors(parentDef))
+            yield return error;
+
+        if (LogWorksBothInsideAndOutsideFieldAreFalse && !worksInside && !worksOutside)
+            yield return parentDef.defName + $" has {nameof(CompCustomCauseHediff_AoE)} with both {nameof(worksInside)} and {nameof(worksOutside)} set to false. The comp won't do anything at all.";
+    }
+}
