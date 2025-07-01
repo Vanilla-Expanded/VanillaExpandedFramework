a8375f2b0c41fc54bb79f97bfdb96a14740967ba
juanosarg <31105173+juanosarg@users.noreply.github.com>
2025-07-01 08:06:52 +0200

    Removing unused patches

---
+++ Source/VEF/Weapons/StaticCollectionsClass/StaticCollectionsClass.cs
@@ -0,0 +1,33 @@
+﻿
+using Verse;
+using System;
+using RimWorld;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace VEF.Weapons
+{
+    [StaticConstructorOnStartup]
+    public static class StaticCollectionsClass
+    {
+
+        static StaticCollectionsClass()
+        {
+            List<ThingDef> uniqueWeapons = DefDatabase<ThingDef>.AllDefsListForReading.Where(x => x.GetCompProperties<CompProperties_UniqueWeapon>() != null).ToList();
+           
+            if(uniqueWeapons.Count > 0)
+            {
+                foreach(ThingDef thingDef in uniqueWeapons)
+                {
+                    thingDef.comps.Add(new CompProperties_OverrideWeaponGraphic());
+                }
+
+            }
+        }
+
+
+
+
+    }
+}
