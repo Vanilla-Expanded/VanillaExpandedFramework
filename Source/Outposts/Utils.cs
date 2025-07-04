e39aa05166df375aac13d765a9cc801c8a1c57de
juanosarg <31105173+juanosarg@users.noreply.github.com>
2025-07-04 20:09:59 +0200

    Partial outposts fixes

--- Source/Outposts/Utils.cs
+++ Source/Outposts/Utils.cs
@@ -15,7 +15,7 @@ public static class Utils
     public delegate void ImmunityTick(ImmunityHandler immunity);
 
     public static readonly ImmunityTick immunityTick =
-        AccessTools.MethodDelegate<ImmunityTick>(AccessTools.Method(typeof(ImmunityHandler), "ImmunityHandlerTick"));
+        AccessTools.MethodDelegate<ImmunityTick>(AccessTools.Method(typeof(ImmunityHandler), "ImmunityHandlerTickInterval"));
 
     public static bool SatisfiedBy(this List<AmountBySkill> minSkills, IEnumerable<Pawn> pawns) =>
         minSkills.All(abs => pawns.Sum(p => p.skills.GetSkill(abs.Skill).Level) >= abs.Count);
