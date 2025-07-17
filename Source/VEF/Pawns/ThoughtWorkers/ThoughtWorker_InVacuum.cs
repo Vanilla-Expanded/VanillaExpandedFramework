56ebec55ab079c75b630132722332b357286719c
juanosarg <31105173+juanosarg@users.noreply.github.com>
2025-07-17 16:42:32 +0200

    ThoughtWorker_InVacuum and ThoughtWorker_NotInVacuum

---
+++ Source/VEF/Pawns/ThoughtWorkers/ThoughtWorker_InVacuum.cs
@@ -0,0 +1,17 @@
+﻿using RimWorld;
+using Verse;
+namespace VEF.Pawns
+{
+    public class ThoughtWorker_InVacuum : ThoughtWorker
+    {
+        protected override ThoughtState CurrentStateInternal(Pawn p)
+        {
+
+            if (p.Map?.BiomeAt(p.Position)?.inVacuum == true)
+            {
+                return true;
+            }
+            return ThoughtState.Inactive;
+        }
+    }
+}
---
+++ Source/VEF/Pawns/ThoughtWorkers/ThoughtWorker_NotInVacuum.cs
@@ -0,0 +1,17 @@
+﻿using RimWorld;
+using Verse;
+namespace VEF.Pawns
+{
+    public class ThoughtWorker_NotInVacuum : ThoughtWorker
+    {
+        protected override ThoughtState CurrentStateInternal(Pawn p)
+        {
+           
+            if (p.Map?.BiomeAt(p.Position)?.inVacuum == false)
+            {
+                return true;
+            }
+            return ThoughtState.Inactive;
+        }
+    }
+}
