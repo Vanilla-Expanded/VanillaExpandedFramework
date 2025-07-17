407f14232a0c469d9ffac51e96fb143f914cca00
juanosarg <31105173+juanosarg@users.noreply.github.com>
2025-07-17 16:10:17 +0200

    HediffCompProperties_StageByVacuum

---
+++ Source/VEF/AnimalBehaviours/HediffComps/HediffComp_StageByVacuum.cs
@@ -0,0 +1,44 @@
+﻿
+
+using RimWorld;
+using System.Collections.Generic;
+using Verse;
+using System.Linq;
+using Verse.Sound;
+using UnityEngine;
+
+namespace VEF.AnimalBehaviours
+{
+    public class HediffComp_StageByVacuum : HediffComp
+    {
+
+
+        public HediffCompProperties_StageByVacuum Props
+        {
+            get
+            {
+                return (HediffCompProperties_StageByVacuum)this.props;
+            }
+        }
+
+
+        public override void CompPostTickInterval(ref float severityAdjustment, int delta)
+        {
+            base.CompPostTickInterval(ref severityAdjustment, delta);
+
+            if (this.parent.pawn.IsHashIntervalTick(500, delta))
+            {
+                if (parent.pawn.Map?.BiomeAt(parent.pawn.Position)?.inVacuum == true)
+                {
+                    this.parent.Severity = Props.vacuumStageIndex;
+                }
+                else this.parent.Severity = Props.notVacuumStageIndex;
+            }
+
+
+        }
+
+
+
+    }
+}
---
+++ Source/VEF/AnimalBehaviours/HediffComps/Properties/HediffCompProperties_StageByVacuum.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Verse;
+using System.Collections.Generic;
+
+
+namespace VEF.AnimalBehaviours
+{
+    public class HediffCompProperties_StageByVacuum : HediffCompProperties
+    {
+
+        public float notVacuumStageIndex = 0.1f;
+        public float vacuumStageIndex = 1f;
+
+        public HediffCompProperties_StageByVacuum()
+        {
+            this.compClass = typeof(HediffComp_StageByVacuum);
+        }
+    }
+}
