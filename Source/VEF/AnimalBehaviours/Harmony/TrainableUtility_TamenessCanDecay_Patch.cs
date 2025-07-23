b2ebc232e46debbf25e98a2d57bcfdc9a90b3800
juanosarg <31105173+juanosarg@users.noreply.github.com>
2025-07-23 18:56:34 +0200

    Changes to TamingCanDecay in unstable

--- Source/VEF/AnimalBehaviours/Harmony/TrainableUtility_TamenessCanDecay_Patch.cs
+++ Source/VEF/AnimalBehaviours/Harmony/TrainableUtility_TamenessCanDecay_Patch.cs
@@ -34,5 +34,44 @@ namespace VEF.AnimalBehaviours
         }
     }
 
+    /*
+     * 
+    [HarmonyPatch(typeof(TrainableUtility))]
+    [HarmonyPatch("TamenessCanDecay")]
+    [HarmonyPatch(new Type[] { typeof(Pawn) })]
+    public static class VanillaExpandedFramework_TrainableUtility_TamenessCanDecay_Patch
+    {
+        [HarmonyPrefix]
+        public static bool RemoveTamenessDecayCheck(Pawn pawn)
+
+        {
+            if (StaticCollectionsClass.IsNoTamingDecayAnimal(pawn.def))
+            {
+                return false;
+
+            }
+            else return true;
+        }
+    }
+
+    [HarmonyPatch(typeof(TrainableUtility))]
+    [HarmonyPatch("TamenessCanDecay")]
+    [HarmonyPatch(new Type[] { typeof(ThingDef) })]
+    public static class VanillaExpandedFramework_TrainableUtility_TamenessCanDecay_ForThingDef_Patch
+    {
+        [HarmonyPrefix]
+        public static bool RemoveTamenessDecayCheck(ThingDef def)
+
+        {
+            if (StaticCollectionsClass.IsNoTamingDecayAnimal(def))
+            {
+                return false;
+
+            }
+            else return true;
+        }
+    }
+*/
+
 
 }
