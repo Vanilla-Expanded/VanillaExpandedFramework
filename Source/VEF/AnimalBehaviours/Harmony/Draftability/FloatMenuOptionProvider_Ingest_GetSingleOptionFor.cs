58f0eca4789ce8b995e08d339a63e60edc71aaaf
juanosarg <31105173+juanosarg@users.noreply.github.com>
2025-08-15 15:12:03 +0200

    Improvements for animal draftability

---
+++ Source/VEF/AnimalBehaviours/Harmony/Draftability/FloatMenuOptionProvider_Ingest_GetSingleOptionFor.cs
@@ -0,0 +1,36 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System.Reflection;
+using Verse;
+using UnityEngine;
+using System.Collections.Generic;
+using System.Linq;
+using Verse.AI;
+using RimWorld.Planet;
+
+namespace VEF.AnimalBehaviours
+{
+
+
+    [HarmonyPatch(typeof(FloatMenuOptionProvider_Ingest))]
+    [HarmonyPatch("GetSingleOptionFor")]
+    public static class VanillaExpandedFramework_FloatMenuOptionProvider_Ingest_GetSingleOptionFor_Patch
+    {
+        [HarmonyPostfix]
+        static void RemoveErrorForNonForbiddables(FloatMenuContext context, Thing clickedThing, ref FloatMenuOption __result)
+        {
+
+            if (StaticCollectionsClass.draftable_animals.Contains(context.FirstSelectedPawn))
+            {
+
+                CompForbiddable comp = clickedThing.TryGetComp<CompForbiddable>();
+                if (comp == null)
+                {
+                    __result = null;
+                }
+                
+            }
+
+        }
+    }
+}
---
+++ Source/VEF/AnimalBehaviours/Harmony/Draftability/FloatMenuOptionProvider_WorkGivers_GetWorkGiverOption.cs
@@ -0,0 +1,29 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System.Reflection;
+using Verse;
+using UnityEngine;
+using System.Collections.Generic;
+using System.Linq;
+using Verse.AI;
+using RimWorld.Planet;
+
+namespace VEF.AnimalBehaviours
+{
+
+
+    [HarmonyPatch(typeof(FloatMenuOptionProvider_WorkGivers))]
+    [HarmonyPatch("GetWorkGiverOption")]
+    public static class VanillaExpandedFramework_FloatMenuOptionProvider_WorkGivers_GetWorkGiverOption_Patch
+    {
+        [HarmonyPostfix]
+        static void NoWorkBesidesAttacks(Pawn pawn, ref FloatMenuOption __result)
+        {
+            if (StaticCollectionsClass.draftable_animals.Contains(pawn))
+            {
+                __result = null;
+            }
+
+        }
+    }
+}
