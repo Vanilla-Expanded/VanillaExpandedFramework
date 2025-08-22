3687a88725dcf2b0efe357f4a9cb66de99a524bc
SokyranTheDragon <exploder1531@gmail.com>
2025-08-22 17:05:58 +0200

    Add custom graphic type for unique empty/full refuelable
    `CompDirtyMapMeshOnRefuelableChange` comp will dirty the map graphic whenever the refuelable is refueled or runs out of fuel, so the graphic can update properly.

---
+++ Source/VEF/Graphics/Comps/CompDirtyMapMeshOnRefuelableChange.cs
@@ -0,0 +1,19 @@
+﻿using RimWorld;
+using Verse;
+
+namespace VEF.Graphics;
+
+public class CompDirtyMapMeshOnRefuelableChange : ThingComp
+{
+    public override void ReceiveCompSignal(string signal)
+    {
+        base.ReceiveCompSignal(signal);
+
+        // Required by Graphic_RefuelableMulti and Graphic_RefuelableSingle
+        if (signal is CompRefuelable.RanOutOfFuelSignal or CompRefuelable.RefueledSignal && parent.Spawned)
+            parent.DirtyMapMesh(parent.Map);
+
+        // We technically could make a patch when the refuelable is refueled/runs out of fuel
+        // (and check if Graphic requires dirtying), but I'd rather avoid that due to performance concerns.
+    }
+}
---
+++ Source/VEF/Graphics/Graphic/Graphic_RefuelableMulti.cs
@@ -0,0 +1,45 @@
+﻿using RimWorld;
+using UnityEngine;
+using Verse;
+
+namespace VEF.Graphics;
+
+public class Graphic_RefuelableMulti : Graphic_Multi
+{
+    public Graphic emptyGraphic;
+
+    public override Material MatAt(Rot4 rot, Thing thing = null)
+    {
+        if (thing.TryGetComp<CompRefuelable>()?.Fuel <= 0)
+            return emptyGraphic.MatAt(rot, thing);
+        return base.MatAt(rot, thing);
+    }
+
+    public override Material MatSingleFor(Thing thing)
+    {
+        if (thing.TryGetComp<CompRefuelable>()?.Fuel <= 0)
+            return emptyGraphic.MatSingleFor(thing);
+        return base.MatSingleFor(thing);
+    }
+
+    public override void TryInsertIntoAtlas(TextureAtlasGroup groupKey)
+    {
+        base.TryInsertIntoAtlas(groupKey);
+        emptyGraphic.TryInsertIntoAtlas(groupKey);
+    }
+
+    public override void Init(GraphicRequest req)
+    {
+        req.maskPath ??= req.path + Graphic_Single.MaskSuffix;
+        base.Init(req);
+        req.path += "_empty";
+
+        emptyGraphic = new Graphic_Multi();
+        emptyGraphic.Init(req);
+    }
+
+    public override Graphic GetColoredVersion(Shader newShader, Color newColor, Color newColorTwo)
+        => GraphicDatabase.Get<Graphic_RefuelableSingle>(path, newShader, drawSize, newColor, newColorTwo, data);
+
+    public override string ToString() => $"{nameof(Graphic_RefuelableSingle)}(base=({base.ToString()}), empty=({emptyGraphic}))";
+}
