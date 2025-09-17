22febe9bb4949870dec80b8f572cdb0d1cb42fb1
juanosarg <31105173+juanosarg@users.noreply.github.com>
2025-09-17 16:32:41 +0200

    HediffComp_PassiveResearch

---
+++ Source/VEF/AnimalBehaviours/HediffComps/Properties/HediffCompProperties_PassiveResearch.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Verse;
+using System.Collections.Generic;
+
+namespace VEF.AnimalBehaviours
+{
+    public class HediffCompProperties_PassiveResearch : HediffCompProperties
+    {
+
+        public int researchPoints = 1;
+        public int tickInterval = 6000;
+      
+
+        public HediffCompProperties_PassiveResearch()
+        {
+            this.compClass = typeof(HediffComp_PassiveResearch);
+        }
+    }
+}
