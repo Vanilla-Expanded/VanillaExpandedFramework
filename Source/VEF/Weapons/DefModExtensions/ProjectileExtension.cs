4ed4d3c45fc887bf7573ec7ce97595d98433aac9
juanosarg <31105173+juanosarg@users.noreply.github.com>
2025-10-27 10:49:44 +0100

    Projectile_Attachments

--- Source/VEF/Weapons/DefModExtensions/ProjectileExtension.cs
+++ Source/VEF/Weapons/DefModExtensions/ProjectileExtension.cs
@@ -1,4 +1,5 @@
-﻿using Verse;
+﻿using UnityEngine;
+using Verse;
 
 namespace VEF.Weapons
 {
@@ -11,5 +12,9 @@ namespace VEF.Weapons
         public float    flashIntensity      = -1f;
         public FleckDef hitFleck;
         public bool excludeFromStaticCollection = false;
+        public EffecterDef attachedEffecter;
+        public FleckDef attachedFleck;
+        public float fleckScale = 1;
+        public int fleckRefreshInterval = 10;
     }
 }
---
+++ Source/VEF/Weapons/Projectiles/Projectile_Attachments.cs
@@ -0,0 +1,96 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Verse;
+
+namespace VEF.Weapons
+{
+    public class Projectile_Attachments : Bullet
+    {
+        public ProjectileExtension cachedProjectileExtension;
+        private Effecter effecter;
+
+        public ProjectileExtension CachedProjectileExtension
+        {
+            get
+            {
+                if(cachedProjectileExtension == null)
+                {
+                    cachedProjectileExtension=this.def.GetModExtension<ProjectileExtension>();
+                }
+                return cachedProjectileExtension;
+
+            }
+
+        }
+
+        protected override void Tick()
+        {
+            base.Tick();
+            if (this.IsHashIntervalTick(CachedProjectileExtension.fleckRefreshInterval) && CachedProjectileExtension.attachedFleck != null)
+            {
+
+                try
+                {
+                    FleckMaker.AttachedOverlay(this, CachedProjectileExtension.attachedFleck, Vector3.zero, CachedProjectileExtension.fleckScale, -1f);
+
+                }
+                catch (Exception) { }
+
+            }
+            if (CachedProjectileExtension.attachedEffecter != null && effecter == null)
+            {
+                effecter = CachedProjectileExtension.attachedEffecter.SpawnAttached(this, this.Map);
+            }
+            effecter?.EffectTick(this, this);
+        }
+
+
+    }
+
+    public class Projectile_Attachments_Explosive : Projectile_Explosive
+    {
+        public ProjectileExtension cachedProjectileExtension;
+        private Effecter effecter;
+
+        public ProjectileExtension CachedProjectileExtension
+        {
+            get
+            {
+                if (cachedProjectileExtension == null)
+                {
+                    cachedProjectileExtension = this.def.GetModExtension<ProjectileExtension>();
+                }
+                return cachedProjectileExtension;
+
+            }
+
+        }
+
+        protected override void Tick()
+        {
+            base.Tick();
+            if (this.IsHashIntervalTick(CachedProjectileExtension.fleckRefreshInterval) && CachedProjectileExtension.attachedFleck != null)
+            {
+                try
+                {
+                    FleckMaker.AttachedOverlay(this, CachedProjectileExtension.attachedFleck, Vector3.zero, CachedProjectileExtension.fleckScale, -1f);
+
+                }
+                catch (Exception) { }
+
+            }
+            if (CachedProjectileExtension.attachedEffecter != null && effecter == null)
+            {
+                effecter = CachedProjectileExtension.attachedEffecter.SpawnAttached(this, this.Map);
+            }
+            effecter?.EffectTick(this, this);
+        }
+
+
+    }
+}
