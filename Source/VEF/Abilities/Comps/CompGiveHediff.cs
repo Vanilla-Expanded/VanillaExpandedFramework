7bc1d2492b9336829634fa1395b46e6aea9939ac
juanosarg <31105173+juanosarg@users.noreply.github.com>
2025-11-21 11:15:52 +0100

    CompProperties_GiveHediff

---
+++ Source/VEF/Abilities/Comps/CompGiveHediff.cs
@@ -0,0 +1,50 @@
+﻿using Verse;
+using RimWorld;
+
+
+namespace VEF.Abilities
+{
+    class CompGiveHediff : CompAbilityEffect
+    {
+
+        new public CompProperties_GiveHediff Props
+        {
+            get
+            {
+                return (CompProperties_GiveHediff)this.props;
+            }
+        }
+
+        public override void Apply(LocalTargetInfo target, LocalTargetInfo dest)
+        {
+            base.Apply(target, dest);
+            if (Props.applyToCaster)
+            {
+                parent.pawn.health.AddHediff(Props.hediffDef);
+            }
+            if (Props.applyToRadius)
+            {
+                foreach (Pawn pawn in parent.pawn.Map.mapPawns.AllPawnsSpawned)
+                {
+
+                    if (pawn.Spawned && pawn.Position.InHorDistOf(target.Cell, parent.def.EffectRadius))
+                    {
+                        pawn.health.AddHediff(Props.hediffDef);
+                    }
+
+
+
+                }
+
+
+            }
+
+        }
+
+
+
+
+
+
+    }
+}
---
+++ Source/VEF/Abilities/Comps/Properties/CompProperties_GiveHediff.cs
@@ -0,0 +1,21 @@
+﻿using RimWorld;
+using Verse;
+
+namespace VEF.Abilities
+{
+
+
+    public class CompProperties_GiveHediff : CompProperties_AbilityEffect
+    {
+
+        public HediffDef hediffDef;
+        public bool applyToCaster = true;
+        public bool applyToRadius = false;
+
+
+        public CompProperties_GiveHediff()
+        {
+            this.compClass = typeof(CompGiveHediff);
+        }
+    }
+}
