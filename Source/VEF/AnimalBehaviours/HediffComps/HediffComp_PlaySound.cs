45574219b96327cade705c21ef410a6bc0e3c0e4
juanosarg <31105173+juanosarg@users.noreply.github.com>
2025-11-24 16:39:11 +0100

    HediffCompProperties_PlaySound

---
+++ Source/VEF/AnimalBehaviours/HediffComps/HediffComp_PlaySound.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse.Sound;
+using Verse;
+
+namespace VEF.AnimalBehaviours
+{
+    public class HediffComp_PlaySound : HediffComp
+    {
+        private Sustainer sustainer;
+        public HediffCompProperties_PlaySound Props => (HediffCompProperties_PlaySound)props;
+        public override void CompPostTick(ref float severityAdjustment)
+        {
+            base.CompPostTick(ref severityAdjustment);
+            if (Props.sustainer != null)
+            {
+                if (sustainer == null || sustainer.Ended)
+                {
+                    sustainer = Props.sustainer.TrySpawnSustainer(SoundInfo.InMap(Pawn, MaintenanceType.PerTick));
+                }
+                sustainer.Maintain();
+            }
+        }
+
+        public override void CompPostPostRemoved()
+        {
+            base.CompPostPostRemoved();
+            if (Props.sustainer != null)
+            {
+                if (!sustainer.Ended)
+                {
+                    sustainer?.End();
+                }
+            }
+            if (Props.endSound != null)
+            {
+                Props.endSound.PlayOneShot(base.Pawn);
+            }
+        }
+    }
+}
---
+++ Source/VEF/AnimalBehaviours/HediffComps/Properties/HediffCompProperties_PlaySound.cs
@@ -0,0 +1,17 @@
+﻿
+ using Verse;
+
+
+namespace VEF.AnimalBehaviours
+{
+   
+    public class HediffCompProperties_PlaySound : HediffCompProperties
+    {
+        public SoundDef sustainer;
+        public SoundDef endSound;
+        public HediffCompProperties_PlaySound()
+        {
+            compClass = typeof(HediffComp_PlaySound);
+        }
+    }
+}
