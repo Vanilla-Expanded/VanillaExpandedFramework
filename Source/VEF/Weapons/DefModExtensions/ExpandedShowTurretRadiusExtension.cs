6437551aa737146ad21019a0716f8be6c3b20077
SokyranTheDragon <exploder1531@gmail.com>
2026-01-15 16:55:34 +0100

    Added PlaceWorker_ExpandedShowTurretRadius
    By default, it supports `Verb_Shoot`, `Verb_Spray`, and `Verb_ShootBeam` (including all their subtypes). This means that, as opposed to the vanilla place worker, this supports subtypes of `Verb_Shoot` as well as `Verb_ShootBeam` and its subbtypes.
    
    On top of that, I've also included `ExpandedShowTurretRadiusExtension` that can be used to support different verb types as well without having to modify this class or add custom ones. It can also be used to select a specific verb type that will be used in case of turrets with multiple verbs.

---
+++ Source/VEF/Weapons/DefModExtensions/ExpandedShowTurretRadiusExtension.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Verse;
+
+namespace VEF.Weapons;
+
+public class ExpandedShowTurretRadiusExtension : DefModExtension
+{
+    /// <summary>
+    /// If specified, replaces the built-in check for supported verb classes with a check for this specific verb class.
+    /// This checks only for this very specific verb class. However, if allowAnyVerb is also true, the check will also
+    /// match any verbs based on that specific one (so for example, allowing for Verb_Shoot will also allow for Verb_ShootWithSmoke).
+    /// This could come in handy for turrets that have multiple different verbs with different ranges, and we want to display a range for a specific verb only.
+    /// </summary>
+    public Type allowedVerbClass = null;
+    /// <summary>
+    /// If true, the place worker will support any verb class (as long as there's any verb present).
+    /// However, if allowedVerbClass is specified, it instead changes how that specific check works.
+    /// </summary>
+    public bool allowAnyVerb = false;
+
+    /// <summary>
+    /// Determines if the maximum range should be drawn for this turret.
+    /// </summary>
+    public bool drawMaxRange = true;
+    /// <summary>
+    /// Determines if the minimum range should be drawn for this turret.
+    /// </summary>
+    public bool drawMinRange = true;
+}
---
+++ Source/VEF/Weapons/PlaceWorkers/PlaceWorker_ExpandedShowTurretRadius.cs
@@ -0,0 +1,51 @@
+﻿using Verse;
+
+namespace VEF.Weapons;
+
+public class PlaceWorker_ExpandedShowTurretRadius : PlaceWorker
+{
+    public override AcceptanceReport AllowsPlacing(BuildableDef checkingDef, IntVec3 loc, Rot4 rot, Map map, Thing thingToIgnore = null, Thing thing = null)
+    {
+        var extension = checkingDef?.GetModExtension<ExpandedShowTurretRadiusExtension>();
+        var props = (checkingDef as ThingDef)?.building?.turretGunDef?.Verbs.Find(v => IsValidVerb(v, extension));
+
+        if (props != null)
+        {
+            if (props.range > 0 && (extension == null || extension.drawMaxRange))
+                GenDraw.DrawRadiusRing(loc, props.range);
+            if (props.minRange > 0 && (extension == null || extension.drawMinRange))
+                GenDraw.DrawRadiusRing(loc, props.minRange);
+        }
+        else
+            Log.ErrorOnce($"Trying to display turret range for {checkingDef} failed, since its turret ({(checkingDef as ThingDef)?.building?.turretGunDef.ToStringSafe()}) " +
+                          $"has no valid verbs to grab the range from. Either make sure the turret has a verb with a supported class, or use {nameof(ExpandedShowTurretRadiusExtension)} " +
+                          $"def mod extension to specify supported verb classes.", Gen.HashCombineInt(checkingDef?.defNameHash ?? "null".GetHashCode(), 422220065));
+
+        return true;
+    }
+
+    private static bool IsValidVerb(VerbProperties v, ExpandedShowTurretRadiusExtension extension)
+    {
+        // Extension allows for some extra configuration
+        if (extension != null)
+        {
+            // We're searching for a specific verb class
+            if (extension.allowedVerbClass != null)
+            {
+                // If allowing any verb, search for subclasses of that verb
+                if (extension.allowAnyVerb)
+                    return extension.allowedVerbClass.IsAssignableFrom(v.verbClass);
+
+                // If not allowing any verb, only allow exact match
+                return extension.allowedVerbClass == v.verbClass;
+            }
+
+            // If we allow any verb, just check that the verb class is not null and is a verb.
+            if (extension.allowAnyVerb)
+                return typeof(Verb).IsAssignableFrom(v.verbClass);
+        }
+
+        // Vanilla check, expanded to allow any Verb_Shoot (rather than only Verb_Shoot) as well as any Verb_ShootBeam
+        return typeof(Verb_Shoot).IsAssignableFrom(v.verbClass) || typeof(Verb_Spray).IsAssignableFrom(v.verbClass) || typeof(Verb_ShootBeam).IsAssignableFrom(v.verbClass);
+    }
+}
