56e8a64056bcb9eaf6e1d8ba602df8fca326f226
SokyranTheDragon <exploder1531@gmail.com>
2026-01-22 01:54:03 +0100

    Modify dodge chance code to support negative dodge chance/extra chance to get hit

---
+++ Source/VEF/Pawns/Harmony/Projectile_CheckForFreeIntercept.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.Reflection.Emit;
+using HarmonyLib;
+using RimWorld;
+using Verse;
+
+namespace VEF.Pawns;
+
+[HarmonyPatch(typeof(Projectile), "CheckForFreeIntercept")]
+public class VanillaExpandedFramework_Projectile_CheckForFreeIntercept_Patch
+{
+    private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instr)
+    {
+        var matcher = new CodeMatcher(instr);
+
+        // Search for "num2 = 0.4f * Mathf.Clamp(pawn.BodySize, 0.1f, 2f);"
+        matcher.MatchEndForward(
+            // Loads the value of 0.4 (base dodge)
+            CodeMatch.LoadsConstant(0.4f),
+            // Loads the pawn
+            CodeMatch.LoadsLocal(),
+            // Calls the BodySize getter
+            new CodeMatch(OpCodes.Callvirt),
+            // Loads 0.1 as minimum dodge chance factor from body size
+            CodeMatch.LoadsConstant(0.1f),
+            // Loads 2 as maximum dodge chance factor from body size
+            CodeMatch.LoadsConstant(2f),
+            // Calls Mathf.Clamp on the last 3 values
+            new CodeMatch(OpCodes.Call),
+            // Multiplies 0.4 with the body size factor
+            new CodeMatch(OpCodes.Mul),
+            // Stores the result in a local variable
+            CodeMatch.StoresLocal()
+        );
+
+        // Move back once, before storing the value
+        matcher.Advance(-1);
+
+        // Wrap the method above with our method
+        matcher.Insert(
+            // Loads the pawn
+            CodeInstruction.LoadLocal(7),
+            // Calls our method
+            CodeInstruction.Call(() => VanillaExpandedFramework_Projectile_ImpactSomething_Patch.GetHitChanceFactor),
+            // Multiply the current value
+            new CodeInstruction(OpCodes.Mul)
+        );
+
+        return matcher.Instructions();
+    }
+}
--- Source/VEF/Pawns/Harmony/Projectile_ImpactSomething.cs
+++ Source/VEF/Pawns/Harmony/Projectile_ImpactSomething.cs
@@ -18,21 +18,37 @@ namespace VEF.Pawns
         public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> codeInstructions, ILGenerator ilg)
         {
             var codes = codeInstructions.ToList();
-          
+
             var addRangedDodge = AccessTools.Method(typeof(VanillaExpandedFramework_Projectile_ImpactSomething_Patch), "AddRangedDodge");
             var impact = AccessTools.Method(typeof(Projectile), "Impact");
 
 
             var label = ilg.DefineLabel();
+            var foundStloc = false;
 
             for (var i = 0; i < codes.Count; i++)
             {
 
-                if (i > 1 && codes[i].opcode == OpCodes.Stloc_2 && codes[i -1].opcode == OpCodes.Isinst && codes[i - 1].OperandIs(typeof(Pawn))  )
+                if (!foundStloc && codes[i].opcode == OpCodes.Stloc_S && codes[i].operand is LocalBuilder { LocalIndex: 5 })
+                {
+                    // Patch only once
+                    foundStloc = true;
+
+                    // Load the "pawn" local
+                    yield return CodeInstruction.LoadLocal(6);
+                    // Call our method
+                    yield return CodeInstruction.Call(() => GetHitChanceFactor);
+                    // Multiply the current value
+                    yield return new CodeInstruction(OpCodes.Mul);
+
+                    // Return the original opcode
+                    yield return codes[i];
+                }
+                else if (i > 1 && codes[i].opcode == OpCodes.Stloc_2 && codes[i -1].opcode == OpCodes.Isinst && codes[i - 1].OperandIs(typeof(Pawn))  )
                 {
                     codes[i + 1].labels.Add(label);
                     yield return new CodeInstruction(OpCodes.Stloc_2);
-                   
+
                     yield return new CodeInstruction(OpCodes.Ldloc_2);
                     yield return new CodeInstruction(OpCodes.Call, addRangedDodge);
                     yield return new CodeInstruction(OpCodes.Brfalse_S, label);
@@ -42,10 +58,10 @@ namespace VEF.Pawns
                     yield return new CodeInstruction(OpCodes.Callvirt,impact);
 
 
-              
+
                     yield return new CodeInstruction(OpCodes.Ret);
                 }
-               
+
                 else yield return codes[i];
             }
         }
@@ -58,13 +74,18 @@ namespace VEF.Pawns
                 if (Rand.Chance(rangedDodgeChance))
                 {
                     MoteMaker.ThrowText(pawn.DrawPos, pawn.Map, "TextMote_Dodge".Translate(), 1.9f);
-                   
+
                     return true;
                 }
             }
-            
+
             return false;
         }
 
+        public static float GetHitChanceFactor(Pawn pawn)
+        {
+            // Get the ranged dodge chance as a factor chance to get hit
+            return 1f - pawn.GetStatValue(InternalDefOf.VEF_RangedDodgeChance);
+        }
     }
 }
