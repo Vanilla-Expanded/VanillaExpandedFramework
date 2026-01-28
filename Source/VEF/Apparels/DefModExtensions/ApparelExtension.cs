3467b4b963e78a12dbbd213149fd4f10ec952101
SokyranTheDragon <exploder1531@gmail.com>
2026-01-28 17:18:09 +0100

    Add IMergeable interface and relevant patches, apply it to ApparelExtension

--- Source/VEF/Apparels/DefModExtensions/ApparelExtension.cs
+++ Source/VEF/Apparels/DefModExtensions/ApparelExtension.cs
@@ -1,14 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
-using HarmonyLib;
 using RimWorld;
 using Verse;
 
 namespace VEF.Apparels
 {
-    public class ApparelExtension : DefModExtension
+    public class ApparelExtension : DefModExtension, IMergeable
     {
-        public float? skillGainModifier;
+        public float skillGainModifier = 1f;
         public WorkTags workDisables = WorkTags.None;
         public List<SkillDef> skillDisables;
 
@@ -26,6 +25,90 @@ namespace VEF.Apparels
         public bool isUnifiedApparel;
         public bool hideHead;
         public bool showBodyInBedAlways;
+
+        // Order doesn't matter.
+        public float Priority => 0f;
+
+        // Always possible to merge.
+        public bool CanMerge(object other) => other != null && other.GetType() == typeof(ApparelExtension);
+
+        public void Merge(object extension)
+        {
+            var other = (ApparelExtension)extension;
+
+            skillGainModifier *= other.skillGainModifier;
+            workDisables |= other.workDisables;
+            CombineLists(ref skillDisables, other.skillDisables);
+
+            CombineStatModifiers(ref equippedStatFactors, other.equippedStatFactors);
+            CombineLists(ref traitsOnEquip, other.traitsOnEquip);
+            CombineLists(ref traitsOnUnequip, other.traitsOnUnequip);
+            CombineStatCapacityMinLevels(ref pawnCapacityMinLevels, other.pawnCapacityMinLevels);
+            preventDowning |= other.preventDowning;
+            preventKilling |= other.preventKilling;
+            preventKillingUntilHealthHPPercentage *= other.preventKillingUntilHealthHPPercentage;
+            preventKillingUntilBrainMissing |= other.preventKillingUntilBrainMissing;
+            preventBleeding |= other.preventBleeding;
+
+            CombineLists(ref secondaryApparelGraphics, other.secondaryApparelGraphics);
+            isUnifiedApparel |= other.isUnifiedApparel;
+            hideHead |= other.hideHead;
+            showBodyInBedAlways |= other.showBodyInBedAlways;
+        }
+
+        private static void CombineLists<T>(ref List<T> original, List<T> other)
+        {
+            if (original == null)
+                original = other;
+            else if (other != null)
+                original.AddRangeUnique(other);
+        }
+
+        private static void CombineStatModifiers(ref List<StatModifier> original, List<StatModifier> other)
+        {
+            if (original == null)
+            {
+                original = other;
+                return;
+            }
+
+            if (other == null)
+                return;
+
+            foreach (var otherFactor in other)
+            {
+                var factor = original.FirstOrDefault(f => f.stat == otherFactor.stat);
+                // The original doesn't include the current one, add it
+                if (factor == null)
+                    original.Add(otherFactor);
+                // The original includes the current one, merge them (multiply)
+                else
+                    factor.value *= otherFactor.value;
+            }
+        }
+
+        private static void CombineStatCapacityMinLevels(ref List<PawnCapacityMinLevel> original, List<PawnCapacityMinLevel> other)
+        {
+            if (original == null)
+            {
+                original = other;
+                return;
+            }
+
+            if (other == null)
+                return;
+
+            foreach (var otherMinLevel in other)
+            {
+                var minLevel = original.FirstOrDefault(f => f.capacity == otherMinLevel.capacity);
+                // The original doesn't include the current one, add it
+                if (minLevel == null)
+                    original.Add(otherMinLevel);
+                // The original includes the current one, merge them (multiply)
+                else
+                    minLevel.minLevel = Math.Max(minLevel.minLevel, otherMinLevel.minLevel);
+            }
+        }
     }
 
     public class PawnCapacityMinLevel
---
+++ Source/VEF/Global/IMergeable.cs
@@ -0,0 +1,31 @@
+﻿namespace VEF;
+
+/// <summary>
+/// An interface that can be applied to comp properties or def mod extensions.
+/// When present, it causes the objects it is applied to will be merged into a single one, somewhat mimicing XML inheritance.
+/// Only applied when both objects are the same exact type, so you should be careful with inheritance.
+/// It may be a smart idea to make all those methods and properties virtual in case you include inheritance for your objects.
+/// Supported by default: Def.modExtensions, ThingDef.comps, HediffDef.comps, WorldObjectDef.comps, StorytellerDef.comps,
+/// AbilityDef.comps, RitualVisualEffectDef.comps, RitualOutcomeEffectDef.comps, SurgeryOutcomeEffectDef.comps
+/// </summary>
+public interface IMergeable
+{
+    /// <summary>
+    /// Determines in which order the objects will be merged. Other objects will be merged into the one with the highest priority.
+    /// </summary>
+    float Priority { get; }
+
+    /// <summary>
+    /// Handles merging other objects into this one.
+    /// </summary>
+    /// <param name="other">The other object that will be removed and should be merged into this one.</param>
+    void Merge(object other);
+
+    /// <summary>
+    /// Determines if the other object can be merged into this one.
+    /// Although not required, it may be smart to include a type check as an extra precaution.
+    /// </summary>
+    /// <param name="other">The other object that could be merged into this one and then removed.</param>
+    /// <returns>True if the other object should be merged and removed, false if it should be kept instead.</returns>
+    bool CanMerge(object other);
+}
