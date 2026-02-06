f242ca0ccb5d13dd1adc7e3a0b4d25383fc98479
juanosarg <31105173+juanosarg@users.noreply.github.com>
2026-02-06 19:37:12 +0100

    Lootbox and LootboxExtension

---
+++ Source/VEF/Buildings/DefModExtensions/LootBoxExtension.cs
@@ -0,0 +1,21 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace VEF.Buildings
+{
+    public class LootBoxExtension : DefModExtension
+    {
+        public ThingSetMakerDef thingSetMakerDef;
+        public FloatRange totalMarketValueRange = new FloatRange(850, 1000);
+        public float? minSingleItemMarketValuePct;
+        public bool allowNonStackableDuplicates = true;
+        public IntRange countRange = new IntRange(1, 1);
+
+
+    }
+}
---
+++ Source/VEF/Buildings/ThingWithComps/LootBox.cs
@@ -0,0 +1,106 @@
+﻿using RimWorld;
+using System.Collections.Generic;
+using Verse;
+
+namespace VEF.Buildings
+{
+    public class LootBox : ThingWithComps, IOpenable
+    {
+
+        public LootBoxExtension cachedLootBoxExtension;
+        public CompQuality cachedCompQuality;
+
+        public LootBoxExtension GetExtension
+        {
+            get
+            {
+                if (cachedLootBoxExtension is null)
+                {
+                    cachedLootBoxExtension = this.def.GetModExtension<LootBoxExtension>();
+                }
+                return cachedLootBoxExtension;
+            }
+        }
+
+        public CompQuality GetQuality
+        {
+            get
+            {
+                if (cachedCompQuality is null)
+                {
+                    cachedCompQuality = this.GetComp<CompQuality>();
+                }
+                return cachedCompQuality;
+            }
+        }
+
+        public float AmountByQuality(QualityCategory quality)
+        {
+            switch (quality)
+            {
+                case QualityCategory.Awful:
+                    return 0.5f;
+                case QualityCategory.Poor:
+                    return 0.75f;
+                case QualityCategory.Normal:
+                    return 1;
+                case QualityCategory.Good:
+                    return 1.25f;
+                case QualityCategory.Excellent:
+                    return 1.5f;
+                case QualityCategory.Masterwork:
+                    return 2.5f;
+                case QualityCategory.Legendary:
+                    return 5f;
+            }
+            return 1;
+        }
+
+        public int OpenTicks => 300;
+
+        public bool CanOpen => true;
+
+        public void Open()
+        {
+
+            ThingSetMakerParams parms = default(ThingSetMakerParams);
+
+            parms.totalMarketValueRange = GetExtension.totalMarketValueRange * AmountByQuality(GetQuality.Quality);
+            parms.minSingleItemMarketValuePct = GetExtension.minSingleItemMarketValuePct;
+            parms.allowNonStackableDuplicates = GetExtension.allowNonStackableDuplicates;
+
+            int fixStupidIntRangeStuff = GetExtension.countRange.RandomInRange;
+            parms.countRange = new IntRange(fixStupidIntRangeStuff, fixStupidIntRangeStuff);
+
+            List<Thing> list2 = GetExtension.thingSetMakerDef.root.Generate(parms);
+
+            if (list2 != null)
+            {
+
+                foreach (Thing thing in list2)
+                {
+                    GenPlace.TryPlaceThing(thing, Position, Map, ThingPlaceMode.Near);
+                }
+
+                if (this.Spawned)
+                {
+                    this.Destroy();
+                }
+            }
+        }
+
+        public override IEnumerable<StatDrawEntry> SpecialDisplayStats()
+        {
+            foreach (StatDrawEntry item in base.SpecialDisplayStats())
+            {
+                yield return item;
+            }
+
+            yield return new StatDrawEntry(StatCategoryDefOf.BasicsImportant, "VEF.TotalMarketValueRange".Translate(), GetExtension.totalMarketValueRange.ToString(), "VEF.TotalMarketValueRange_Desc".Translate(), 2749);
+            yield return new StatDrawEntry(StatCategoryDefOf.BasicsImportant, "VEF.TotalMarketValueRange_Quality".Translate(), (GetExtension.totalMarketValueRange * AmountByQuality(GetQuality.Quality)).ToString(), "VEF.TotalMarketValueRange_Quality_Desc".Translate(AmountByQuality(GetQuality.Quality).ToString()), 2748);
+
+        }
+
+
+    }
+}
