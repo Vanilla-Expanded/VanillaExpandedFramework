ee32f73d34ec6d674ebf9b3aa6c0a43c451bd1ba
SokyranTheDragon <exploder1531@gmail.com>
2026-02-09 16:28:36 +0100

    Added `Verb_ShootOneUse_FlyOverhead`

---
+++ Source/VEF/Weapons/Verbs/Verb_ShootOneUse_FlyOverhead.cs
@@ -0,0 +1,18 @@
+﻿using RimWorld;
+using Verse;
+
+namespace VEF.Weapons;
+
+public class Verb_ShootOneUse_FlyOverhead : Verb_ShootOneUse
+{
+    public override void OrderForceTarget(LocalTargetInfo target)
+    {
+        if (this.ProjectileFliesOverhead() && caster is { Map: { } map, Position: { IsValid: true } pos } && map.roofGrid?.Roofed(pos) == true)
+        {
+            Messages.Message($"{"CannotFire".Translate()}: {"Roofed".Translate().CapitalizeFirst()}", MessageTypeDefOf.RejectInput, false);
+            return;
+        }
+
+        base.OrderForceTarget(target);
+    }
+}
