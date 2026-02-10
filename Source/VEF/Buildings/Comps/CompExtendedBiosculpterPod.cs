a18796e8e52151c59a0ea8080670198fc1cadc4f
SokyranTheDragon <exploder1531@gmail.com>
2026-02-11 00:41:56 +0100

    Added CompExtendedBiosculpterPod
    Its features are:
    - Ability to copy all cycles from a given `ThingDef`
    - Configuring the offset for the pawn inside (for each direction)
    - Forcing a pawn to be facing a specific direction while in the pod
    - Ability to change texture for, completely disable, or move/resize the background material used by the biosculpter pod
    - Option to completely disable drawing the pawns inside of the pod

---
+++ Source/VEF/Buildings/Comps/CompExtendedBiosculpterPod.cs
@@ -0,0 +1,32 @@
+﻿using HarmonyLib;
+using RimWorld;
+using UnityEngine;
+using Verse;
+
+namespace VEF.Buildings;
+
+public class CompExtendedBiosculpterPod : CompBiosculpterPod
+{
+    private static readonly AccessTools.FieldRef<CompBiosculpterPod, int> currentCycleTicksRemainingField = AccessTools.FieldRefAccess<CompBiosculpterPod, int>("currentCycleTicksRemaining");
+    private static readonly AccessTools.FieldRef<CompBiosculpterPod, int> currentCyclePowerCutTicksField = AccessTools.FieldRefAccess<CompBiosculpterPod, int>("currentCyclePowerCutTicks");
+
+    public new CompProperties_ExtendedBiosculpterPod Props => (CompProperties_ExtendedBiosculpterPod)props;
+
+    public override void PostDraw()
+    {
+        if (Props.drawBackground)
+        {
+            var rotation = parent.Rotation;
+            var drawPos = parent.DrawPos;
+            drawPos.y -= 0.07317074f;
+
+            UnityEngine.Graphics.DrawMesh(MeshPool.plane10, Matrix4x4.TRS(drawPos + Props.backgroundOffset, rotation.AsQuat, Props.backgroundSize), Props.backgroundMaterial, 0);
+        }
+
+        if (State == BiosculpterPodState.Occupied && Props.drawPawn)
+        {
+            var pawnDrawPos = parent.DrawPos + FloatingOffset(currentCycleTicksRemainingField(this) + currentCyclePowerCutTicksField(this)) + Props.PawnOffsetFor(parent.Rotation);
+            Occupant.Drawer.renderer.RenderPawnAt(pawnDrawPos, Props.pawnFacingDirectionOverride, true);
+        }
+    }
+}
---
+++ Source/VEF/Buildings/Comps/CompProperties/CompProperties_ExtendedBiosculpterPod.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Linq;
+using HarmonyLib;
+using RimWorld;
+using UnityEngine;
+using Verse;
+
+namespace VEF.Buildings;
+
+public class CompProperties_ExtendedBiosculpterPod : CompProperties_BiosculpterPod
+{
+    public ThingDef copyCyclesFrom = null;
+
+    public bool drawPawn = true;
+    public Rot4? pawnFacingDirectionOverride = null;
+
+    public Vector3 pawnOffsetNorth = Vector3.zero;
+    public Vector3 pawnOffsetSouth = Vector3.zero;
+    public Vector3 pawnOffsetEast = Vector3.zero;
+    public Vector3 pawnOffsetWest = Vector3.zero;
+
+    public bool drawBackground = true;
+    public Vector3 backgroundOffset = Vector3.zero;
+    public Vector3 backgroundSize = Vector3.zero;
+    public string backgroundMaterialPath = null;
+    [Unsaved]
+    public Material backgroundMaterial = null;
+
+    public CompProperties_ExtendedBiosculpterPod() => compClass = typeof(CompExtendedBiosculpterPod);
+
+    public override void ResolveReferences(ThingDef parentDef)
+    {
+        base.ResolveReferences(parentDef);
+
+        try
+        {
+            if (copyCyclesFrom?.comps != null)
+            {
+                foreach (var comp in copyCyclesFrom.comps)
+                {
+                    if (comp is not CompProperties_BiosculpterPod_BaseCycle { key: not null } cycle)
+                        continue;
+                    if (parentDef.comps.OfType<CompProperties_BiosculpterPod_BaseCycle>().Any(x => x.key == cycle.key))
+                        continue;
+
+                    parentDef.comps.Add(Gen.MemberwiseClone(cycle));
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            Log.Error($"Error occured trying to copy vanilla BiosculpterPod cycles to {parentDef.defName}:\n{e}");
+        }
+
+        if (backgroundSize.x <= 0f || backgroundSize.y <= 0f || backgroundSize.z <= 0f)
+            backgroundSize = new Vector3(parentDef.graphicData.drawSize.x * 0.8f, 1f, parentDef.graphicData.drawSize.y * 0.8f);
+
+        if (!backgroundMaterialPath.NullOrEmpty())
+            backgroundMaterial = MaterialPool.MatFrom(backgroundMaterialPath);
+        if (backgroundMaterial.NullOrBad())
+            backgroundMaterial = (Material)typeof(CompBiosculpterPod).DeclaredField("BackgroundMat").GetValue(null);
+    }
+
+    public Vector3 PawnOffsetFor(Rot4 rotation)
+    {
+        var output = rotation.AsInt switch
+        {
+            Rot4.NorthInt => pawnOffsetNorth,
+            Rot4.SouthInt => pawnOffsetSouth,
+            Rot4.EastInt => pawnOffsetEast,
+            Rot4.WestInt => pawnOffsetWest,
+            _ => Vector3.zero,
+        };
+
+        return output;
+    }
+}
