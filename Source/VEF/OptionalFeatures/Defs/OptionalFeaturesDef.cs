2e01fbc6f1bcb1ee3fc07e2a283b48dfac08cff9
SokyranTheDragon <exploder1531@gmail.com>
2026-02-11 00:35:06 +0100

    Expand/improve optional features
    - Optional features now use either an `ApplyFeature` method (like they used to) or apply patches to an entire Harmony category
    - The `ApplyFeature` method is initialized in `OptionalFeaturesDef.ResolveReferences`
    - The def has an (unsaved) bool that determines if it's active or not
    - The applying process is handled in the def (since it can check if it's active or not)
    - The code for applying optional features was significantly sipmplified

--- Source/VEF/OptionalFeatures/Defs/OptionalFeaturesDef.cs
+++ Source/VEF/OptionalFeatures/Defs/OptionalFeaturesDef.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using RimWorld;
+using System.Reflection;
+using HarmonyLib;
 using Verse;
 
 namespace VEF.OptionalFeatures
@@ -11,7 +8,35 @@ namespace VEF.OptionalFeatures
     public class OptionalFeaturesDef : Def
     {
         public string feature;
-        public Type activationClass;     
+        public Type activationClass;
+        [Unsaved] public MethodInfo activationMethod;
+        public string harmonyCategory;
+        [Unsaved] private bool isActive = false;
 
+        public override void ResolveReferences()
+        {
+            base.ResolveReferences();
+
+            // Search for a static method with a single "Harmony" argument only.
+            if (activationClass != null)
+                activationMethod = activationClass.GetMethod("ApplyFeature", BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic, null, [typeof(Harmony)], null);
+        }
+
+        public void Activate()
+        {
+            if (isActive)
+                return;
+            isActive = true;
+
+            if (activationClass != null && harmonyCategory != null)
+                Log.WarningOnce($"Feature {feature} has both {nameof(activationClass)} and {nameof(harmonyCategory)} specified, only {nameof(harmonyCategory)} will be used. Category: {harmonyCategory}, type: {activationClass}.", feature.GetHashCode());
+
+            if (!harmonyCategory.NullOrEmpty())
+                VEF_Mod.harmonyInstance.PatchCategory(harmonyCategory);
+            else if (activationMethod == null)
+                Log.ErrorOnce($"Feature {feature} with type {activationClass.ToStringSafe()} does not have ApplyFeature method or does not specify a harmony category", feature.GetHashCode());
+            else
+                activationMethod.Invoke(null, [VEF_Mod.harmonyInstance]);
+        }
     }
 }
--- Source/VEF/OptionalFeatures/OptionalFeatures.cs
+++ Source/VEF/OptionalFeatures/OptionalFeatures.cs
@@ -1,42 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Reflection;
 using Verse;
 
 namespace VEF.OptionalFeatures
 {
     public class OptionalFeatures : Mod
     {
-        public static Dictionary<string, Type> Features = new Dictionary<string, Type>
-        {
-            
-        };
-
-        public static List<(string, Type, MethodInfo)> ActiveFeatures = new List<(string, Type, MethodInfo)>();
-
         public OptionalFeatures(ModContentPack content) : base(content)
         {
             LongEventHandler.ExecuteWhenFinished(delegate
             {
-                foreach (OptionalFeaturesDef featureToAdd in DefDatabase<OptionalFeaturesDef>.AllDefsListForReading)
+                var features = new Dictionary<string, OptionalFeaturesDef>();
+
+                foreach (var featureToAdd in DefDatabase<OptionalFeaturesDef>.AllDefsListForReading)
                 {
                     if (featureToAdd.feature != null)
                     {
-                        Features[featureToAdd.feature] = featureToAdd.activationClass;
+                        features[featureToAdd.feature] = featureToAdd;
                     }
                 }
 
-                foreach (var feature in DefDatabase<ModDef>.AllDefs.SelectMany(def => def.Activate.Where(feature => !ActiveFeatures.Any(tuple => tuple.Item1 == feature))))
-                    if (Features.ContainsKey(feature))
-                        ActiveFeatures.Add((feature, Features[feature], Features[feature].GetMethod("ApplyFeature", BindingFlags.Static | BindingFlags.Public)));
-                    else Log.ErrorOnce($"Feature not found: {feature}", feature.GetHashCode());
-
-                foreach (var (name, type, method) in ActiveFeatures)
-                    if (method == null)
-                        Log.ErrorOnce($"Feature {name} with type {type} does not have ApplyFeature method", type.GetHashCode());
-                    else
-                        method.Invoke(null, new object[] {VEF_Mod.harmonyInstance});
+                foreach (var feature in DefDatabase<ModDef>.AllDefs.SelectMany(def => def.Activate))
+                    if (feature != null && features.TryGetValue(feature, out var f))
+                        f.Activate();
+                    else Log.ErrorOnce($"Feature not found: {feature}", (feature ?? "null").GetHashCode());
             });
         }
     }
