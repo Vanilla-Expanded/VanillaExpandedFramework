2f457cd5533aa0e1794f6c323c1e5d19a7f0a44d
SokyranTheDragon <exploder1531@gmail.com>
2026-02-13 21:26:11 +0100

    Added Verb_Shoot_FlyOverhead

---
+++ Source/VEF/Weapons/Verbs/Verb_Shoot_FlyOverhead.cs
@@ -0,0 +1,18 @@
+﻿using RimWorld;
+using Verse;
+
+namespace VEF.Weapons;
+
+public class Verb_Shoot_FlyOverhead : Verb_Shoot
+{
+    public override void OrderForceTarget(LocalTargetInfo target)
+    {
+        if (this.ProjectileFliesOverhead() && caster is { Map: { } map, Position: { IsValid: true } pos } && map.roofGrid?.Roofed(pos) == true)
+        {
+            Messages.Message($"{"CannotFire".Translate()}: {"Roofed".Translate().CapitalizeFirst()}", MessageTypeDefOf.RejectInput, false);
+            return;
+        }
+
+        base.OrderForceTarget(target);
+    }
+}
