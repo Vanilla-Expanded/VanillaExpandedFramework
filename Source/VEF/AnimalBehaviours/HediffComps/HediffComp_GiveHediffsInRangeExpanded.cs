1dc5ab366e865f23dcefe1881eea608a479e3e1e
juanosarg <31105173+juanosarg@users.noreply.github.com>
2026-03-26 12:25:50 +0100

    HediffComp_GiveHediffsInRangeExpanded

---
+++ Source/VEF/AnimalBehaviours/HediffComps/HediffComp_GiveHediffsInRangeExpanded.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+using RimWorld;
+using UnityEngine;
+using Verse;
+
+namespace VEF.AnimalBehaviours
+{
+    public class HediffComp_GiveHediffsInRangeExpanded : HediffComp
+    {
+        private Mote mote;
+
+        public HediffCompProperties_GiveHediffsInRangeExpanded Props => (HediffCompProperties_GiveHediffsInRangeExpanded)props;
+
+        public override void CompPostTick(ref float severityAdjustment)
+        {
+            if (!parent.pawn.Awake() || parent.pawn.health == null || parent.pawn.health.InPainShock || !parent.pawn.Spawned)
+            {
+                return;
+            }
+            if (!Props.hideMoteWhenNotDrafted || parent.pawn.Drafted)
+            {
+                if (Props.mote != null && (mote == null || mote.Destroyed))
+                {
+                    mote = MoteMaker.MakeAttachedOverlay(parent.pawn, Props.mote, Vector3.zero);
+                }
+                if (mote != null)
+                {
+                    mote.Maintain();
+                }
+            }
+            List<Pawn> list = null;
+            list = parent.pawn.Map.mapPawns.PawnsInFaction(parent.pawn.Faction);
+            foreach (Pawn item in list)
+            {
+                if (item.Dead || item.health == null || item == parent.pawn || !item.RaceProps.Insect || !(item.Position.DistanceTo(parent.pawn.Position) <= Props.range) || !Props.targetingParameters.CanTarget(item) || ((Props.affectSameDef) && (item.def != parent.pawn.def)))
+                {
+                    continue;
+                }
+                Hediff hediff = item.health.hediffSet.GetFirstHediffOfDef(Props.hediff);
+                if (hediff == null)
+                {
+                    hediff = item.health.AddHediff(Props.hediff, item.health.hediffSet.GetBrain());
+                    hediff.Severity = Props.initialSeverity;
+                    HediffComp_Link hediffComp_Link = hediff.TryGetComp<HediffComp_Link>();
+                    if (hediffComp_Link != null)
+                    {
+                        hediffComp_Link.drawConnection = true;
+                        hediffComp_Link.other = parent.pawn;
+                    }
+                }
+                HediffComp_Disappears hediffComp_Disappears = hediff.TryGetComp<HediffComp_Disappears>();
+                if (hediffComp_Disappears == null)
+                {
+                    Log.Error("HediffComp_GiveHediffsInRange has a hediff in props which does not have a HediffComp_Disappears");
+                }
+                else
+                {
+                    hediffComp_Disappears.ticksToDisappear = 5;
+                }
+            }
+        }
+    }
+}
---
+++ Source/VEF/AnimalBehaviours/HediffComps/Properties/HediffCompProperties_GiveHediffsInRangeExpanded.cs
@@ -0,0 +1,27 @@
+﻿using RimWorld;
+using Verse;
+
+namespace VEF.AnimalBehaviours
+{
+    public class HediffCompProperties_GiveHediffsInRangeExpanded : HediffCompProperties
+    {
+        public float range;
+
+        public TargetingParameters targetingParameters;
+
+        public HediffDef hediff;
+
+        public ThingDef mote;
+
+        public bool hideMoteWhenNotDrafted;
+
+        public float initialSeverity = 1f;
+
+        public bool affectSameDef;
+
+        public HediffCompProperties_GiveHediffsInRangeExpanded()
+        {
+            compClass = typeof(HediffComp_GiveHediffsInRangeExpanded);
+        }
+    }
+}
