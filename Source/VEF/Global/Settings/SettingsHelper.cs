f1c8d1aa802272c024f2ffa088d9e7baec5f0704
juanosarg <31105173+juanosarg@users.noreply.github.com>
2026-04-11 22:57:41 +0200

    SettingsHelper

---
+++ Source/VEF/Global/Settings/SettingsHelper.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Globalization;
+using System.Text;
+using UnityEngine;
+using Verse;
+using RimWorld;
+
+namespace VEF
+{
+    [StaticConstructorOnStartup]
+    public static class SettingsHelper
+    {
+        public static bool Settings_Button(this Listing_Standard ls, string label, Rect rect)
+        {
+            bool result = Widgets.ButtonText(rect, label, true, true, true);
+            ls.Gap((2f));
+            return result;
+        }
+
+        public static Rect LabelPlusButton(this Listing_Standard ls, string label, string tooltip = null)
+        {
+            float num = Text.CalcHeight(label, ls.ColumnWidth);
+            Rect rect = ls.GetRect(num);
+            Widgets.Label(rect, label);
+            if (tooltip != null)
+            {
+                TooltipHandler.TipRegion(rect, tooltip);
+            }
+            ls.Gap(50);
+            return rect;
+        }
+    }
+}
