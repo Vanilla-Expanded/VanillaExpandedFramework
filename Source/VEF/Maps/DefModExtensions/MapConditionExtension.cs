1863209a2be68d00b60b80042a67f682d0a56908
juanosarg <31105173+juanosarg@users.noreply.github.com>
2026-04-27 18:42:33 +0200

    MapConditionExtension

---
+++ Source/VEF/Maps/DefModExtensions/MapConditionExtension.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace VEF.Maps
+{
+    public class MapConditionExtension : DefModExtension
+    {
+        //This value only affects VE Furniture - Power
+        public float tideStrengthMultiplier = 1;
+
+        //This value affects VE Furniture - Power and base game watermill generators
+        public float watermillStrengthMultiplier = 1;
+
+    }
+}
