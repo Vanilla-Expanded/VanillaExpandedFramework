dfe89ee3ab14af89d5d05655936a37f17ef23dec
juanosarg <31105173+juanosarg@users.noreply.github.com>
2026-05-22 11:35:32 +0200

    DeathActionWorker_VanishInsect

---
+++ Source/VEF/AnimalBehaviours/DeathActionWorkers/DeathActionWorker_VanishInsect.cs
@@ -0,0 +1,42 @@
+﻿
+using RimWorld;
+using VEF.AnimalBehaviours;
+using Verse;
+using Verse.AI.Group;
+
+namespace VEF.AnimalBehaviours
+{
+    public class DeathActionWorker_VanishInsect : DeathActionWorker
+    {
+        public DeathActionProperties_VanishInsect Props => (DeathActionProperties_VanishInsect)props;
+
+        public override void PawnDied(Corpse corpse, Lord prevLord)
+        {
+            if (Props.fleck != null)
+            {
+                FleckMaker.Static(corpse.PositionHeld, corpse.MapHeld, Props.fleck);
+            }
+            if (Props.filth != null)
+            {
+                int randomInRange = Props.filthCountRange.RandomInRange;
+                for (int i = 0; i < randomInRange; i++)
+                {
+                    FilthMaker.TryMakeFilth(corpse.PositionHeld, corpse.MapHeld, Props.filth);
+                }
+            }
+
+            CellRect cellRect = new CellRect(corpse.PositionHeld.x, corpse.PositionHeld.z, 3, 3).ClipInsideMap(corpse.MapHeld);
+
+            IntVec3 randomCell = cellRect.RandomCell;
+            ThingDef filthDef = ThingDefOf.Filth_BloodInsect;
+            if (randomCell.InBounds(corpse.MapHeld) && GenSight.LineOfSight(randomCell, corpse.PositionHeld, corpse.MapHeld))
+            {
+                FilthMaker.TryMakeFilth(randomCell, corpse.MapHeld, filthDef);
+            }
+
+
+
+            corpse.Destroy();
+        }
+    }
+}
---
+++ Source/VEF/AnimalBehaviours/DeathActionWorkers/Properties/DeathActionProperties_VanishInsect.cs
@@ -0,0 +1,17 @@
+﻿using Verse;
+namespace VEF.AnimalBehaviours
+{
+    public class DeathActionProperties_VanishInsect : DeathActionProperties
+    {
+        public FleckDef fleck;
+
+        public ThingDef filth;
+
+        public IntRange filthCountRange = IntRange.One;
+
+        public DeathActionProperties_VanishInsect()
+        {
+            workerClass = typeof(DeathActionWorker_VanishInsect);
+        }
+    }
+}
