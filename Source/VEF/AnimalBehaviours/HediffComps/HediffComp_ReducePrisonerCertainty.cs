85da6b9d72b4a4c3cb6a738315767295fb371ed8
juanosarg <31105173+juanosarg@users.noreply.github.com>
2026-05-26 08:19:09 +0200

    HediffComp_ReducePrisonerCertainty

---
+++ Source/VEF/AnimalBehaviours/HediffComps/HediffComp_ReducePrisonerCertainty.cs
@@ -0,0 +1,45 @@
+﻿
+using Verse;
+using RimWorld;
+using System.Collections.Generic;
+using System.Linq;
+using Verse.AI;
+using UnityEngine;
+
+namespace VEF.AnimalBehaviours
+{
+    public class HediffComp_ReducePrisonerCertainty : HediffComp
+    {
+        public HediffCompProperties_ReducePrisonerCertainty Props
+        {
+            get
+            {
+                return (HediffCompProperties_ReducePrisonerCertainty)this.props;
+            }
+        }
+
+        public override void CompPostTickInterval(ref float severityAdjustment, int delta)
+        {
+            base.CompPostTickInterval(ref severityAdjustment, delta);
+
+            if (Pawn.IsHashIntervalTick(Props.checkingInterval, delta))
+            {
+                if (Pawn.RaceProps.Humanlike && Pawn.Ideo !=null && Current.Game.World.factionManager.OfPlayer?.ideos?.PrimaryIdeo!=null)
+                {
+                    if (Pawn.Ideo != Current.Game.World.factionManager.OfPlayer.ideos.PrimaryIdeo)
+                    {
+                        Pawn.ideo.Reassure(-Props.certaintyPerTick * Props.checkingInterval /100);
+                        if (Pawn.ideo.Certainty <= 0)
+                        {
+                            Pawn.ideo.SetIdeo(Current.Game.World.factionManager.OfPlayer.ideos.PrimaryIdeo);
+                        }
+                    }
+                    else
+                    {
+                        Pawn.ideo.Reassure(Props.certaintyPerTick * Props.checkingInterval / 100);
+                    }
+                }            
+            }
+        }
+    }
+}
---
+++ Source/VEF/AnimalBehaviours/HediffComps/Properties/HediffCompProperties_ReducePrisonerCertainty.cs
@@ -0,0 +1,19 @@
+﻿
+using Verse;
+using RimWorld;
+
+
+namespace VEF.AnimalBehaviours
+{
+    public class HediffCompProperties_ReducePrisonerCertainty : HediffCompProperties
+    {
+        public float certaintyPerTick;
+
+        public int checkingInterval = 250;
+
+        public HediffCompProperties_ReducePrisonerCertainty()
+        {
+            this.compClass = typeof(HediffComp_ReducePrisonerCertainty);
+        }
+    }
+}
