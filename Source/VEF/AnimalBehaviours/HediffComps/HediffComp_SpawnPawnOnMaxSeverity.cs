1fe8fee6e2b2b36ffb291f52f9b65a8b259739c5
juanosarg <31105173+juanosarg@users.noreply.github.com>
2026-05-26 10:47:27 +0200

    HediffComp_SpawnPawnOnMaxSeverity

---
+++ Source/VEF/AnimalBehaviours/HediffComps/HediffComp_SpawnPawnOnMaxSeverity.cs
@@ -0,0 +1,51 @@
+﻿using RimWorld;
+using Verse;
+using VEF.Global;
+using Verse.AI.Group;
+using Verse.Sound;
+
+namespace VEF.AnimalBehaviours
+{
+    public class HediffComp_SpawnPawnOnMaxSeverity : HediffComp
+    {
+        public HediffCompProperties_SpawnPawnOnMaxSeverity Props => base.props as HediffCompProperties_SpawnPawnOnMaxSeverity;
+
+        public override void CompPostTick(ref float severityAdjustment)
+        {
+            base.CompPostTick(ref severityAdjustment);
+            if (parent.Severity >= 0.99f)
+            {
+                for (int i = 0; i < Props.pawnKindOptions.Count; i++)
+                {
+                    PawnKindDef kind = Props.pawnKindOptions[i];
+                    Faction faction = Faction.OfInsects;
+                    float? fixedBiologicalAge = 0f;
+                    Pawn child = PawnGenerator.GeneratePawn(new PawnGenerationRequest(kind, faction, PawnGenerationContext.NonPlayer, fixedBiologicalAge: fixedBiologicalAge));
+                    SpawnPawn(child, Pawn, Pawn.PositionHeld, Pawn.MapHeld);
+                }
+
+                for (int i = 0; i < Props.filthCountRange.RandomInRange; i++)
+                {
+                    IntVec3 c;
+                    CellFinder.TryFindRandomReachableNearbyCell(Pawn.PositionHeld, Pawn.MapHeld, 2, TraverseParms.For(TraverseMode.NoPassClosedDoors, Danger.Deadly, false), null, null, out c);
+                    FilthMaker.TryMakeFilth(c, Pawn.MapHeld, Props.filthCreated);
+                }
+                if (Props.sound != null)
+                {
+                    Props.sound.PlayOneShot(new TargetInfo(Pawn.PositionHeld, Pawn.MapHeld, false));
+                }
+                Pawn.TakeDamage(new DamageInfo(Props.damage, Props.damageAmount.RandomInRange, 0f, -1f, null, null, null, DamageInfo.SourceCategory.ThingOrUnknown, null));
+
+                Pawn.health.RemoveHediff(this.parent);
+            }
+        } 
+        
+
+        private void SpawnPawn(Pawn child, Pawn parent, IntVec3 position, Map map)
+        {
+            GenSpawn.Spawn(child, position, map, WipeMode.VanishOrMoveAside);
+            
+            FleshbeastUtility.SpawnPawnAsFlyer(child, map, position);
+        }
+    }
+}
---
+++ Source/VEF/AnimalBehaviours/HediffComps/Properties/HediffCompProperties_SpawnPawnOnMaxSeverity.cs
@@ -0,0 +1,22 @@
+﻿using RimWorld;
+using System.Collections.Generic;
+using Verse;
+using System.Text;
+
+namespace VEF.AnimalBehaviours
+{
+    public class HediffCompProperties_SpawnPawnOnMaxSeverity : HediffCompProperties
+    {
+        public List<PawnKindDef> pawnKindOptions = new List<PawnKindDef>();
+        public ThingDef filthCreated;
+        public IntRange filthCountRange;
+        public SoundDef sound;
+        public DamageDef damage;
+        public FloatRange damageAmount; 
+
+        public HediffCompProperties_SpawnPawnOnMaxSeverity()
+        {
+            this.compClass = typeof(HediffComp_SpawnPawnOnMaxSeverity);
+        }
+    }
+}
