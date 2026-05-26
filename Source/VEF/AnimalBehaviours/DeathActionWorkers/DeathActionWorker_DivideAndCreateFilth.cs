12e31aeba5d66f397c4cf107b9828f874b7ba701
juanosarg <31105173+juanosarg@users.noreply.github.com>
2026-05-26 08:57:55 +0200

    DeathActionWorker_DivideAndCreateFilth

---
+++ Source/VEF/AnimalBehaviours/DeathActionWorkers/DeathActionWorker_DivideAndCreateFilth.cs
@@ -0,0 +1,50 @@
+﻿
+using RimWorld;
+using Verse;
+using Verse.AI.Group;
+using Verse.Noise;
+
+namespace VEF.AnimalBehaviours
+
+{
+    public class DeathActionWorker_DivideAndCreateFilth : DeathActionWorker
+    {
+        public DeathActionProperties_DivideAndCreateFilth Props => (DeathActionProperties_DivideAndCreateFilth)props;
+
+        public override void PawnDied(Corpse corpse, Lord prevLord)
+        {
+
+            Pawn innerPawn = corpse.InnerPawn;
+            if (innerPawn == null)
+            {
+                return;
+            }
+
+            for (int i = 0; i < Props.dividePawnKindOptions.Count; i++)
+            {
+                PawnKindDef kind = Props.dividePawnKindOptions[i];
+                Faction faction = corpse.InnerPawn.Faction;
+                float? fixedBiologicalAge = 0f;
+                Pawn child = PawnGenerator.GeneratePawn(new PawnGenerationRequest(kind, faction, PawnGenerationContext.NonPlayer, fixedBiologicalAge: fixedBiologicalAge));
+                SpawnPawn(child, innerPawn, corpse.PositionHeld, corpse.MapHeld, prevLord);
+            }
+
+            for (int i = 0; i < Props.filthCountRange.RandomInRange; i++)
+            {
+                IntVec3 c;
+                CellFinder.TryFindRandomReachableNearbyCell(corpse.PositionHeld, corpse.MapHeld, 2, TraverseParms.For(TraverseMode.NoPassClosedDoors, Danger.Deadly, false), null, null, out c);
+                FilthMaker.TryMakeFilth(c, corpse.MapHeld, Props.filthCreated);
+            }
+
+            corpse.Destroy();
+        }
+
+        private void SpawnPawn(Pawn child, Pawn parent, IntVec3 position, Map map, Lord lord)
+        {
+            GenSpawn.Spawn(child, position, map, WipeMode.VanishOrMoveAside);
+            lord?.AddPawn(child);
+
+            FleshbeastUtility.SpawnPawnAsFlyer(child, map, position);
+        }
+    }
+}
---
+++ Source/VEF/AnimalBehaviours/DeathActionWorkers/Properties/DeathActionProperties_DivideAndCreateFilth.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using Verse;
+
+namespace VEF.AnimalBehaviours
+
+{
+    public class DeathActionProperties_DivideAndCreateFilth : DeathActionProperties
+    {
+        public List<PawnKindDef> dividePawnKindOptions = new List<PawnKindDef>();
+        public ThingDef filthCreated;
+        public IntRange filthCountRange;
+
+        public DeathActionProperties_DivideAndCreateFilth()
+        {
+            workerClass = typeof(DeathActionWorker_DivideAndCreateFilth);
+        }
+
+
+    }
+}
