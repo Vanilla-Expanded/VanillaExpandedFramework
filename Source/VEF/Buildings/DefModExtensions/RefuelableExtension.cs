efc8c9894f67d1c153150152cd1f771337438db6
SokyranTheDragon <exploder1531@gmail.com>
2026-06-19 18:33:18 +0200

    Added RefuelableExtension with option to make ejected fuel (gizmo/deconstruction) scale with fuel multiplier

---
+++ Source/VEF/Buildings/DefModExtensions/RefuelableExtension.cs
@@ -0,0 +1,16 @@
+﻿using Verse;
+
+namespace VEF.Buildings;
+
+public class RefuelableExtension : DefModExtension
+{
+    public bool ejectingFuelRespectsFuelMultiplier = false;
+
+    public override void ResolveReferences(Def parentDef)
+    {
+        base.ResolveReferences(parentDef);
+
+        if (ejectingFuelRespectsFuelMultiplier)
+            VanillaExpandedFramework_CompRefuelable_EjectFuelPatches.patchActive = true;
+    }
+}
---
+++ Source/VEF/Buildings/Harmony/CompRefuelable_EjectFuelPatches.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+using HarmonyLib;
+using RimWorld;
+
+namespace VEF.Buildings;
+
+[HarmonyPatch]
+[HarmonyPatchCategory(VEF_HarmonyCategories.LateHarmonyPatchCategory)]
+public static class VanillaExpandedFramework_CompRefuelable_EjectFuelPatches
+{
+    public static bool patchActive = false;
+
+    private static bool Prepare() => patchActive;
+
+    private static IEnumerable<MethodBase> TargetMethods()
+    {
+        yield return typeof(CompRefuelable).DeclaredMethod(nameof(CompRefuelable.EjectFuel));
+        yield return typeof(CompRefuelable).DeclaredMethod(nameof(CompRefuelable.PostDestroy));
+    }
+
+    private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instr, MethodBase baseMethod)
+    {
+        var matcher = new CodeMatcher(instr);
+
+        matcher.MatchEndForward(CodeMatch.LoadsField(typeof(CompRefuelable).DeclaredField("fuel")));
+        matcher.InsertAfter(
+            // Load "this"
+            CodeInstruction.LoadArgument(0),
+            // Call our method
+            CodeInstruction.Call(() => ApplyFuelMultiplier)
+        );
+
+        return matcher.Instructions();
+    }
+
+    private static float ApplyFuelMultiplier(float fuel, CompRefuelable __instance)
+    {
+        var extension = __instance.parent.def.GetModExtension<RefuelableExtension>();
+        if (extension is { ejectingFuelRespectsFuelMultiplier: true })
+            return fuel / __instance.Props.FuelMultiplierCurrentDifficulty;
+        return fuel;
+    }
+}
