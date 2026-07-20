63a77d30dacbeb11d6a00f9a0e9a2c8beaf0058c
Taranchuk <nfhfyxer@gmail.com>
2026-07-20 22:01:53 +0300

    vanilla prefabs support + mass export tool

---
+++ Source/VEF/Storyteller/Misc/Quests/PrefabExporter.cs
@@ -0,0 +1,259 @@
+using RimWorld;
+using Verse;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+using UnityEngine;
+using System.Text;
+using System.Reflection;
+using LudeonTK;
+
+namespace VEF.Storyteller
+{
+    public static class PrefabExporter
+    {
+        private static readonly FieldInfo thingsField = typeof(PrefabDef).GetField("things", BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public);
+        private static readonly FieldInfo terrainField = typeof(PrefabDef).GetField("terrain", BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public);
+
+        [DebugAction("KCSG", "Mass export prefabs", false, false, actionType = DebugActionType.Action, allowedGameStates = AllowedGameStates.PlayingOnMap)]
+        public static void MassExportPrefabs()
+        {
+            DebugToolsGeneral.GenericRectTool("Mass export", delegate (CellRect rect)
+            {
+                Find.WindowStack.Add(new Dialog_NameMassExport(rect));
+            }, closeOnComplete: true);
+        }
+
+        public static string GeneratePrefabXml(PrefabDef prefabDef, string defName, IntVec2 size)
+        {
+            var sb = new StringBuilder();
+            var indent = "  ";
+            sb.AppendLine("<PrefabDef>");
+            sb.AppendLine(indent + "<defName>" + defName + "</defName>");
+            sb.AppendLine($"{indent}<size>({size.x},{size.z})</size>");
+
+            var things = (List<PrefabThingData>)thingsField?.GetValue(prefabDef);
+            if (things != null && things.Count > 0)
+            {
+                sb.AppendLine(indent + "<things>");
+                for (var i = 0; i < things.Count; i++)
+                {
+                    var thingData = things[i];
+                    sb.AppendLine(indent + indent + "<" + thingData.def.defName + ">");
+                    if (thingData.rects != null)
+                    {
+                        sb.AppendLine(indent + indent + indent + "<rects>");
+                        foreach (var rect in thingData.rects)
+                        {
+                            sb.AppendLine($"{indent}{indent}{indent}{indent}<li>{rect}</li>");
+                        }
+                        sb.AppendLine(indent + indent + indent + "</rects>");
+                    }
+                    else if (thingData.positions != null)
+                    {
+                        sb.AppendLine(indent + indent + indent + "<positions>");
+                        foreach (var position in thingData.positions)
+                        {
+                            sb.AppendLine($"{indent}{indent}{indent}{indent}<li>{position}</li>");
+                        }
+                        sb.AppendLine(indent + indent + indent + "</positions>");
+                    }
+                    else
+                    {
+                        sb.AppendLine($"{indent}{indent}{indent}<position>{thingData.position}</position>");
+                    }
+                    if (thingData.relativeRotation != RotationDirection.None)
+                    {
+                        sb.AppendLine(indent + indent + indent + "<relativeRotation>" + Enum.GetName(typeof(RotationDirection), thingData.relativeRotation) + "</relativeRotation>");
+                    }
+                    if (thingData.stuff != null)
+                    {
+                        sb.AppendLine(indent + indent + indent + "<stuff>" + thingData.stuff.defName + "</stuff>");
+                    }
+                    if (thingData.quality.HasValue)
+                    {
+                        sb.AppendLine($"{indent}{indent}{indent}<quality>{thingData.quality}</quality>");
+                    }
+                    if (thingData.hp != 0)
+                    {
+                        sb.AppendLine($"{indent}{indent}{indent}<hp>{thingData.hp}</hp>");
+                    }
+                    if (thingData.stackCountRange != IntRange.One)
+                    {
+                        sb.AppendLine($"{indent}{indent}{indent}<stackCountRange>{thingData.stackCountRange.min}~{thingData.stackCountRange.max}</stackCountRange>");
+                    }
+                    if (thingData.colorDef != null)
+                    {
+                        sb.AppendLine($"{indent}{indent}{indent}<colorDef>{thingData.colorDef}</colorDef>");
+                    }
+                    if (thingData.color != default(Color))
+                    {
+                        sb.AppendLine($"{indent}{indent}{indent}<color>{thingData.color}</color>");
+                    }
+                    sb.AppendLine(indent + indent + "</" + thingData.def.defName + ">");
+                }
+                sb.AppendLine(indent + "</things>");
+            }
+
+            var terrain = (List<PrefabTerrainData>)terrainField?.GetValue(prefabDef);
+            if (terrain != null && terrain.Count > 0)
+            {
+                sb.AppendLine(indent + "<terrain>");
+                foreach (var item in terrain)
+                {
+                    sb.AppendLine(indent + indent + "<" + item.def.defName + ">");
+                    if (item.color != null)
+                    {
+                        sb.AppendLine($"{indent}{indent}{indent}<color>{item.color}</color>");
+                    }
+                    sb.AppendLine(indent + indent + indent + "<rects>");
+                    foreach (var rect2 in item.rects)
+                    {
+                        sb.AppendLine($"{indent}{indent}{indent}{indent}<li>{rect2}</li>");
+                    }
+                    sb.AppendLine(indent + indent + indent + "</rects>");
+                    sb.AppendLine(indent + indent + "</" + item.def.defName + ">");
+                }
+                sb.AppendLine(indent + "</terrain>");
+            }
+            sb.AppendLine("</PrefabDef>");
+            return sb.ToString();
+        }
+
+        private class Dialog_NameMassExport : Window
+        {
+            private string prefix = "NewPrefab";
+            private CellRect rect;
+            public override Vector2 InitialSize => new Vector2(300f, 150f);
+
+            public Dialog_NameMassExport(CellRect rect)
+            {
+                this.rect = rect;
+                doCloseX = true;
+                forcePause = true;
+                absorbInputAroundWindow = true;
+            }
+
+            public override void DoWindowContents(Rect inRect)
+            {
+                var listing = new Listing_Standard();
+                listing.Begin(inRect);
+                listing.Label("Prefix:");
+                prefix = listing.TextEntry(prefix);
+                if (listing.ButtonText("Accept"))
+                {
+                    MassExport(rect, prefix);
+                    Close();
+                }
+                listing.End();
+            }
+
+            private void MassExport(CellRect rect, string prefix)
+            {
+                var currentMap = Find.CurrentMap;
+                var list = new List<CellRect>();
+                var width = rect.maxX - rect.minX + 1;
+                var height = rect.maxZ - rect.minZ + 1;
+                var separatorX = new HashSet<int>();
+                for (var x = rect.minX; x <= rect.maxX; x++)
+                {
+                    var goldCount = 0;
+                    for (var z = rect.minZ; z <= rect.maxZ; z++)
+                    {
+                        var terrain = new IntVec3(x, 0, z).GetTerrain(currentMap);
+                        if (terrain != null && terrain.defName == "GoldTile")
+                        {
+                            goldCount++;
+                        }
+                    }
+                    if (goldCount >= Math.Max(5, height * 0.3f))
+                    {
+                        separatorX.Add(x);
+                    }
+                }
+                var separatorZ = new HashSet<int>();
+                for (var z = rect.minZ; z <= rect.maxZ; z++)
+                {
+                    var goldCount = 0;
+                    for (var x = rect.minX; x <= rect.maxX; x++)
+                    {
+                        var terrain = new IntVec3(x, 0, z).GetTerrain(currentMap);
+                        if (terrain != null && terrain.defName == "GoldTile")
+                        {
+                            goldCount++;
+                        }
+                    }
+                    if (goldCount >= Math.Max(5, width * 0.3f))
+                    {
+                        separatorZ.Add(z);
+                    }
+                }
+                var xIntervals = new List<(int min, int max)>();
+                var startX = -1;
+                for (var x = rect.minX; x <= rect.maxX; x++)
+                {
+                    if (separatorX.Contains(x))
+                    {
+                        if (startX != -1)
+                        {
+                            xIntervals.Add((startX, x - 1));
+                            startX = -1;
+                        }
+                    }
+                    else if (startX == -1)
+                    {
+                        startX = x;
+                    }
+                }
+                if (startX != -1)
+                {
+                    xIntervals.Add((startX, rect.maxX));
+                }
+                var zIntervals = new List<(int min, int max)>();
+                var startZ = -1;
+                for (var z = rect.minZ; z <= rect.maxZ; z++)
+                {
+                    if (separatorZ.Contains(z))
+                    {
+                        if (startZ != -1)
+                        {
+                            zIntervals.Add((startZ, z - 1));
+                            startZ = -1;
+                        }
+                    }
+                    else if (startZ == -1)
+                    {
+                        startZ = z;
+                    }
+                }
+                if (startZ != -1)
+                {
+                    zIntervals.Add((startZ, rect.maxZ));
+                }
+                for (var i = 0; i < zIntervals.Count; i++)
+                {
+                    var zInt = zIntervals[i];
+                    for (var k = 0; k < xIntervals.Count; k++)
+                    {
+                        var xInt = xIntervals[k];
+                        var subRect = CellRect.FromLimits(xInt.min, zInt.min, xInt.max, zInt.max);
+                        if (subRect.Width >= 2 && subRect.Height >= 2)
+                        {
+                            list.Add(subRect);
+                        }
+                    }
+                }
+                list = list.OrderByDescending((CellRect r) => r.maxZ).ThenBy((CellRect r) => r.minX).ToList();
+                var sb = new StringBuilder();
+                for (var j = 0; j < list.Count; j++)
+                {
+                    var prefabDef = PrefabUtility.CreatePrefab(list[j], true, true);
+                    var value = GeneratePrefabXml(prefabDef, $"{prefix}_{j + 1}", list[j].Size);
+                    sb.AppendLine(value);
+                }
+                GUIUtility.systemCopyBuffer = sb.ToString();
+                Messages.Message($"Copied {list.Count} prefabs to clipboard.", MessageTypeDefOf.NeutralEvent, historical: false);
+            }
+        }
+    }
+}
--- Source/VEF/Storyteller/Misc/Quests/StructureSetGenerator.cs
+++ Source/VEF/Storyteller/Misc/Quests/StructureSetGenerator.cs
@@ -1,6 +1,6 @@
-using Verse;
 using RimWorld;
 using KCSG;
+using Verse;
 using Verse.AI.Group;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,8 +16,8 @@ namespace VEF.Storyteller
         {
             var generatedRects = new List<CellRect>();
             var mapCenter = map.Center;
-            var usedDefs = new HashSet<KCSG.StructureLayoutDef>();
-            var standardLayouts = new List<(StructurePatternOffset layout, KCSG.StructureLayoutDef def)>();
+            var usedDefs = new HashSet<Def>();
+            var standardLayouts = new List<(StructurePatternOffset layout, Def def)>();
             var specialLayouts = new List<StructurePatternOffset>();
 
             foreach (var layout in structureSetDef.structureLayouts)
@@ -30,9 +30,7 @@ namespace VEF.Storyteller
                     continue;
                 }
 
-                var availableDefs = DefDatabase<KCSG.StructureLayoutDef>.AllDefsListForReading
-                    .Where(def => !usedDefs.Contains(def) && Regex.IsMatch(def.defName, "^" + layout.pattern + "$"))
-                    .ToList();
+                var availableDefs = GetAvailableDefs(layout.pattern, usedDefs);
 
                 if (!availableDefs.Any()) continue;
                 
@@ -45,20 +43,11 @@ namespace VEF.Storyteller
 
             if (standardLayouts.Any())
             {
-                int minX = int.MaxValue, minZ = int.MaxValue, maxX = int.MinValue, maxZ = int.MinValue;
-                foreach (var item in standardLayouts)
-                {
-                    var rect = CellRect.CenteredOn(new IntVec3(item.layout.offset.x * item.def.Sizes.x, 0, item.layout.offset.z * item.def.Sizes.z), item.def.Sizes);
-                    if (rect.minX < minX) minX = rect.minX;
-                    if (rect.minZ < minZ) minZ = rect.minZ;
-                    if (rect.maxX > maxX) maxX = rect.maxX;
-                    if (rect.maxZ > maxZ) maxZ = rect.maxZ;
-                }
-
-                var totalOffset = new IntVec3(-(minX + (maxX - minX) / 2), 0, -(minZ + (maxZ - minZ) / 2));
+                var totalOffset = GetTotalOffset(standardLayouts);
                 var first = standardLayouts[0];
-                var spawnPos = mapCenter + totalOffset + new IntVec3(first.layout.offset.x * first.def.Sizes.x, 0, first.layout.offset.z * first.def.Sizes.z);
-                primaryRect = CellRect.CenteredOn(spawnPos, first.def.Sizes.x, first.def.Sizes.z);
+                var firstSize = GetLayoutSize(first.def);
+                var spawnPos = mapCenter + totalOffset + new IntVec3(first.layout.offset.x * firstSize.x, 0, first.layout.offset.z * firstSize.z);
+                primaryRect = CellRect.CenteredOn(spawnPos, firstSize.x, firstSize.z);
             }
 
             foreach (var layout in specialLayouts)
@@ -69,17 +58,15 @@ namespace VEF.Storyteller
                     int subSpawnCount = layout.radialCount > 0 ? layout.radialCount : 1;
                     for (int j = 0; j < subSpawnCount; j++)
                     {
-                        var availableDefs = DefDatabase<KCSG.StructureLayoutDef>.AllDefsListForReading
-                            .Where(def => !usedDefs.Contains(def) && Regex.IsMatch(def.defName, "^" + layout.pattern + "$"))
-                            .ToList();
+                        var availableDefs = GetAvailableDefs(layout.pattern, usedDefs);
                         if (!availableDefs.Any())
                         {
-                            availableDefs = DefDatabase<KCSG.StructureLayoutDef>.AllDefsListForReading
-                                .Where(def => Regex.IsMatch(def.defName, "^" + layout.pattern + "$")).ToList();
+                            availableDefs = GetAvailableDefs(layout.pattern, null);
                         }
                         if (!availableDefs.Any())
                             continue;
                         var selectedDef = availableDefs.RandomElement();
+                        var defSize = GetLayoutSize(selectedDef);
                         usedDefs.Add(selectedDef);
 
                         IntVec3 spawnPos;
@@ -121,7 +108,7 @@ namespace VEF.Storyteller
                                 {
                                     spawnPos = new IntVec3(Rand.Range(0, map.Size.x), 0, Rand.Range(0, map.Size.z));
                                 }
-                                var checkRect = CellRect.CenteredOn(spawnPos, selectedDef.Sizes.x + 2, selectedDef.Sizes.z + 2);
+                                var checkRect = CellRect.CenteredOn(spawnPos, defSize.x + 2, defSize.z + 2);
                                 if (checkRect.FullyContainedWithin(new CellRect(0, 0, map.Size.x, map.Size.z)) && !generatedRects.Any(r => r.Overlaps(checkRect)))
                                 {
                                     found = true;
@@ -132,14 +119,10 @@ namespace VEF.Storyteller
 
                             if (layout.randomRotated) rot = Rot4.Random;
                         }
-
-                        var rotatedSizes = rot.HasValue && rot.Value.IsHorizontal ? new IntVec2(selectedDef.Sizes.z, selectedDef.Sizes.x) : selectedDef.Sizes;
+                        var rotatedSizes = rot.HasValue && rot.Value.IsHorizontal ? new IntVec2(defSize.z, defSize.x) : defSize;
                         var structureRect = CellRect.CenteredOn(spawnPos, rotatedSizes.x, rotatedSizes.z);
-
-                        GenOption.GetAllMineableIn(structureRect, map);
-                        LayoutUtils.CleanRect(selectedDef, map, structureRect, true, rot ?? Rot4.North);
                         var spawnedThings = new List<Thing>();
-                        selectedDef.Generate(structureRect, map, spawnedThings, faction, false, rot);
+                        GenerateLayout(selectedDef, map, structureRect, faction, rot ?? Rot4.North, spawnedThings);
 
                         generatedRects.Add(structureRect);
                         SpawnPawnsAndThings(map, structureRect, layout, faction);
@@ -148,32 +131,17 @@ namespace VEF.Storyteller
             }
             if (standardLayouts.Any())
             {
-                var minX = int.MaxValue;
-                var minZ = int.MaxValue;
-                var maxX = int.MinValue;
-                var maxZ = int.MinValue;
-                foreach (var item in standardLayouts)
-                {
-                    var rect = CellRect.CenteredOn(new IntVec3(item.layout.offset.x * item.def.Sizes.x, 0, item.layout.offset.z * item.def.Sizes.z), item.def.Sizes);
-                    if (rect.minX < minX) minX = rect.minX;
-                    if (rect.minZ < minZ) minZ = rect.minZ;
-                    if (rect.maxX > maxX) maxX = rect.maxX;
-                    if (rect.maxZ > maxZ) maxZ = rect.maxZ;
-                }
-
-                var totalOffset = new IntVec3(-(minX + (maxX - minX) / 2), 0, -(minZ + (maxZ - minZ) / 2));
+                var totalOffset = GetTotalOffset(standardLayouts);
                 var centerAssigned = false;
                 foreach (var item in standardLayouts)
                 {
-                    var spawnPos = mapCenter + totalOffset + new IntVec3(item.layout.offset.x * item.def.Sizes.x, 0, item.layout.offset.z * item.def.Sizes.z);
+                    var size = GetLayoutSize(item.def);
+                    var spawnPos = mapCenter + totalOffset + new IntVec3(item.layout.offset.x * size.x, 0, item.layout.offset.z * size.z);
                     Rot4 rot = item.layout.randomRotated ? Rot4.Random : Rot4.North;
-                    IntVec2 sizes = item.layout.randomRotated && rot.IsHorizontal ? new IntVec2(item.def.Sizes.z, item.def.Sizes.x) : item.def.Sizes;
+                    IntVec2 sizes = item.layout.randomRotated && rot.IsHorizontal ? new IntVec2(size.z, size.x) : size;
                     var structureRect = CellRect.CenteredOn(spawnPos, sizes.x, sizes.z);
-
-                    GenOption.GetAllMineableIn(structureRect, map);
-                    LayoutUtils.CleanRect(item.def, map, structureRect, true, rot);
                     var spawnedThings = new List<Thing>();
-                    item.def.Generate(structureRect, map, spawnedThings, faction, false, rot);
+                    GenerateLayout(item.def, map, structureRect, faction, rot, spawnedThings);
 
                     generatedRects.Add(structureRect);
                     SpawnPawnsAndThings(map, structureRect, item.layout, faction);
@@ -189,6 +157,67 @@ namespace VEF.Storyteller
             var usedRects = MapGenerator.GetOrGenerateVar<List<CellRect>>("UsedRects");
             usedRects.AddRange(generatedRects);
             return generatedRects;
+        }
+
+        private static List<Def> GetAvailableDefs(string pattern, HashSet<Def> usedDefs)
+        {
+            var list = new List<Def>();
+            var regex = new Regex("^" + pattern + "$");
+            foreach (var def in DefDatabase<KCSG.StructureLayoutDef>.AllDefsListForReading)
+            {
+                if ((usedDefs == null || !usedDefs.Contains(def)) && regex.IsMatch(def.defName))
+                    list.Add(def);
+            }
+            foreach (var def in DefDatabase<PrefabDef>.AllDefsListForReading)
+            {
+                if ((usedDefs == null || !usedDefs.Contains(def)) && regex.IsMatch(def.defName))
+                    list.Add(def);
+            }
+            return list;
+        }
+
+        private static IntVec2 GetLayoutSize(Def def)
+        {
+            if (def is KCSG.StructureLayoutDef kcsgDef) return kcsgDef.Sizes;
+            if (def is PrefabDef prefabDef) return prefabDef.size;
+            return IntVec2.One;
+        }
+
+        private static void GenerateLayout(Def def, Map map, CellRect structureRect, Faction faction, Rot4 rot, List<Thing> spawnedThings)
+        {
+            GenOption.GetAllMineableIn(structureRect, map);
+            if (def is KCSG.StructureLayoutDef kcsgDef)
+            {
+                LayoutUtils.CleanRect(kcsgDef, map, structureRect, true, rot);
+                kcsgDef.Generate(structureRect, map, spawnedThings, faction, false, rot);
+            }
+            else if (def is PrefabDef prefabDef)
+            {
+                foreach (var cell in structureRect.Cells)
+                {
+                    var edifice = cell.GetEdifice(map);
+                    if (edifice != null && edifice.def.destroyable)
+                    {
+                        edifice.Destroy();
+                    }
+                }
+                PrefabUtility.SpawnPrefab(prefabDef, map, structureRect.CenterCell, rot, faction, spawnedThings);
+            }
+        }
+
+        private static IntVec3 GetTotalOffset(List<(StructurePatternOffset layout, Def def)> standardLayouts)
+        {
+            int minX = int.MaxValue, minZ = int.MaxValue, maxX = int.MinValue, maxZ = int.MinValue;
+            foreach (var item in standardLayouts)
+            {
+                var size = GetLayoutSize(item.def);
+                var rect = CellRect.CenteredOn(new IntVec3(item.layout.offset.x * size.x, 0, item.layout.offset.z * size.z), size);
+                if (rect.minX < minX) minX = rect.minX;
+                if (rect.minZ < minZ) minZ = rect.minZ;
+                if (rect.maxX > maxX) maxX = rect.maxX;
+                if (rect.maxZ > maxZ) maxZ = rect.maxZ;
+            }
+            return new IntVec3(-(minX + (maxX - minX) / 2), 0, -(minZ + (maxZ - minZ) / 2));
         }
 
         private static void SpawnPawnsAndThings(Map map, CellRect structureRect, StructurePatternOffset layout, Faction faction)
