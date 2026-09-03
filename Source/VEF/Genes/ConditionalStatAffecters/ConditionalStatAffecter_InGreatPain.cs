76a99ed1514ea3872948016930ca6850b97f3a1c
juanosarg <31105173+juanosarg@users.noreply.github.com>
2026-09-03 16:26:09 +0200

    ConditionalStatAffecter_InGreatPain

---
+++ Source/VEF/Genes/ConditionalStatAffecters/ConditionalStatAffecter_InGreatPain.cs
@@ -0,0 +1,22 @@
+﻿
+using RimWorld;
+using Verse;
+namespace VEF.Genes
+{
+    public class ConditionalStatAffecter_InGreatPain : ConditionalStatAffecter
+    {
+        public override string Label => "VGE_StatsReport_InGreatPain".Translate();
+
+        public override bool Applies(StatRequest req)
+        {
+            
+            if (req.HasThing && req.Thing.Spawned)
+            {
+                Pawn pawn = req.Thing as Pawn;
+               
+                return pawn.health.hediffSet.PainTotal > 0.2f;
+            }
+            return false;
+        }
+    }
+}
