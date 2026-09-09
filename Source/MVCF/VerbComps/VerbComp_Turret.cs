f167a812308740f15c11342ed496944627a522b8
SokyranTheDragon <exploder1531@gmail.com>
2026-09-09 16:56:54 +0200

    Remove unneccessary null checks in VerbComp_Turret
    The method checks if parent.Manager.Pawn is null (using a pattern), along with checking if a pawn is spawned. No need to do it again here.

--- Source/MVCF/VerbComps/VerbComp_Turret.cs
+++ Source/MVCF/VerbComps/VerbComp_Turret.cs
@@ -52,7 +52,7 @@ public class VerbComp_Turret : VerbComp_Draw
             return;
         }
 
-        if (parent.Manager?.Pawn?.IsHashIntervalTick(Props.verbCompTurretIntervalTicks) ==true && !currentTarget.IsValid) currentTarget = TryFindNewTarget();
+        if (parent.Manager.Pawn.IsHashIntervalTick(Props.verbCompTurretIntervalTicks) && !currentTarget.IsValid) currentTarget = TryFindNewTarget();
 
         if (warmUpTicksLeft == 0) TryCast();
         else if (warmUpTicksLeft > 0) warmUpTicksLeft--;
